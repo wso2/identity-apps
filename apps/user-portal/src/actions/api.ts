7cc47e0ac3e0f04b81cf7df47b4715da87179c56
Brion Mario <info@brionmario.com>
2019-08-15 12:44:47 +0530

    Define API action interfaces

--- apps/user-portal/src/actions/api.ts
+++ apps/user-portal/src/actions/api.ts
@@ -19,10 +19,6 @@
 import { HttpRequestConfig } from "../models/api";
 
 /**
- * Redux actions related to API requests and responses.
- */
-
-/**
  * Action type to handle starting state of an API request.
  *
  * @type {string}
@@ -30,13 +26,21 @@ import { HttpRequestConfig } from "../models/api";
 export const API_REQUEST_START = "API_REQUEST_START";
 
 /**
+ * Api request start action interface.
+ */
+interface ApiRequestStartAction {
+    payload: string;
+    type: typeof API_REQUEST_START;
+}
+
+/**
  * Dispatches an action of type `API_REQUEST_START` with the type
  * of dispatcher as the payload.
  *
- * @param actionType - Type of the dispatcher who initiated the request.
+ * @param {string} actionType - Type of the dispatcher who initiated the request.
  * @returns An action of type `API_REQUEST_START`
  */
-export const apiRequestStart = (actionType) => ({
+export const apiRequestStart = (actionType: string): ApiRequestStartAction => ({
     payload: actionType,
     type: API_REQUEST_START
 });
@@ -49,13 +53,21 @@ export const apiRequestStart = (actionType) => ({
 export const API_REQUEST_END = "API_REQUEST_END";
 
 /**
+ * Api request end action interface.
+ */
+interface ApiRequestEndAction {
+    payload: string;
+    type: typeof API_REQUEST_END;
+}
+
+/**
  * Dispatches an action of type `API_REQUEST_END` with the type
  * of dispatcher as the payload.
  *
- * @param actionType - Type of the dispatcher who initiated the request.
+ * @param {string} actionType - Type of the dispatcher who initiated the request.
  * @returns An action of type `API_REQUEST_END`
  */
-export const apiRequestEnd = (actionType) => ({
+export const apiRequestEnd = (actionType: string): ApiRequestEndAction => ({
     payload: actionType,
     type: API_REQUEST_END
 });
@@ -68,13 +80,22 @@ export const apiRequestEnd = (actionType) => ({
 export const API_REQUEST = "API_REQUEST";
 
 /**
+ * Api request action interface.
+ */
+interface ApiRequestAction {
+    meta: object;
+    payload: any;
+    type: typeof API_REQUEST;
+}
+
+/**
  * Takes in an config of type `HttpRequestConfig` and dispatches and action of type
  * `API_REQUEST` with data as payload and other configs as meta.
  *
  * @param {HttpRequestConfig} config - Http request configuration
  * @returns An action of type `API_REQUEST`
  */
-export const apiRequest = (config: HttpRequestConfig) => {
+export const apiRequest = (config: HttpRequestConfig): ApiRequestAction => {
     const { auth, data, dispatcher, headers, method, onSuccess, onError, url  } = config;
     return {
         meta: { auth, dispatcher, headers, method, onSuccess, onError, url },
@@ -82,3 +103,8 @@ export const apiRequest = (config: HttpRequestConfig) => {
         type: API_REQUEST
     };
 };
+
+/**
+ * Action type to specify API actions.
+ */
+export type ApiActionTypes = ApiRequestAction | ApiRequestStartAction | ApiRequestEndAction;
