53b43cc6924dcaea00fc929862568449c0d0e321
Brion Mario <info@brionmario.com>
2019-08-26 11:31:59 +0530

    Implement reusable icon component

---
+++ apps/user-portal/src/components/icon.tsx
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import cx from "classnames";
+import * as React from "react";
+import { base64MimeType } from "../helpers";
+
+/**
+ * Proptypes for the Icon component.
+ */
+interface ComponentProps {
+    icon: any;
+    inline?: boolean;
+    className?: string;
+    transparent?: boolean;
+    relaxed?: boolean | string;
+    bordered?: boolean;
+    rounded?: boolean;
+    defaultIcon?: boolean;
+    twoTone?: boolean;
+    size?: string;
+    style?: object;
+}
+
+/**
+ * Generic component to render icons.
+ *
+ * @param {React.PropsWithChildren<any>} props
+ * @return {any}
+ */
+export const Icon: React.FunctionComponent<ComponentProps> = (props): JSX.Element => {
+    const {
+        icon, inline, className, transparent, relaxed, bordered, rounded, defaultIcon, twoTone, size, style
+    } = props;
+    const relaxLevel = (relaxed && relaxed === true) ? "" : relaxed;
+
+    const classNames = cx({
+        "bordered": bordered,
+        "default": defaultIcon,
+        "inline": inline,
+        "relaxed": relaxed,
+        "rounded": rounded,
+        [`${size}`]: size,
+        "transparent": transparent,
+        "two-tone": twoTone,
+        [`${relaxLevel}`]: relaxLevel,
+    }, className);
+
+    const constructContent = (): HTMLElement | SVGElement | JSX.Element => {
+        if (icon instanceof SVGElement) {
+            return icon;
+        }
+
+        // Check if the icon is a React component.
+        if (icon.ReactComponent && typeof icon.ReactComponent === "function") {
+            return <icon.ReactComponent />;
+        }
+
+        if (typeof icon !== "string") {
+            throw new Error("The provided icon type is not supported.");
+        }
+
+        const mimeType = base64MimeType(icon);
+
+        if (!mimeType) {
+            throw new Error("The provided icon type is not supported.");
+        }
+
+        return <img src={icon} alt="icon" />;
+    };
+
+    return (
+        <div className={ `icon-wrapper ${classNames}` } style={style}>
+            { constructContent() }
+        </div>
+    );
+};
+
+/**
+ * Default proptypes for the Icon component.
+ */
+Icon.defaultProps = {
+    bordered: false,
+    className: "",
+    defaultIcon: true,
+    inline: false,
+    relaxed: false,
+    rounded: false,
+    size: "mini",
+    style: {},
+    transparent: false,
+    twoTone: false,
+};
