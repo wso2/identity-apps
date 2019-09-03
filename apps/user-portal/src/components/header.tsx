f13c93f0f1b88f01dcfc0725b8982cb96b35f436
Brion Mario <info@brionmario.com>
2019-09-04 03:26:00 +0530

    Add a sidebar toggle button to the header component

--- apps/user-portal/src/components/header.tsx
+++ apps/user-portal/src/components/header.tsx
@@ -18,60 +18,91 @@
 
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Dropdown, Item, Menu } from "semantic-ui-react";
-import { Title } from "../components";
+import { Button, Container, Divider, Dropdown, Icon, Item, Menu, Responsive } from "semantic-ui-react";
 import { AuthConsumer } from "./auth-context";
-import { UserImageDummy } from "./ui";
+import { Title, UserImageDummy } from "./ui";
 
-export const Header = () => (
-    <Menu fixed="top" borderless>
-        <Container>
-            <Menu.Item as={Link} to={APP_HOME_PATH} header>
-                <Title style={{marginTop: 0}} />
-            </Menu.Item>
-            <AuthConsumer>
-                {({ displayName, emails, username }) => (
-                    <Menu.Menu position="right">
-                        <Dropdown
-                            item
-                            floating
-                            text={(displayName !== "undefined") ? displayName : username}
-                            className="user-dropdown">
-                            <Dropdown.Menu>
-                                <Item.Group>
-                                    <Item className="header">
-                                        <Item.Image size="tiny" circular src={UserImageDummy} />
+/**
+ * Header component prop types.
+ */
+interface HeaderProps {
+    onSidePanelToggleClick?: () => void;
+    showSidePanelToggle?: boolean;
+}
 
-                                        <Item.Content verticalAlign="middle">
-                                            <Item.Description>
-                                                <div>{username}</div>
-                                                {(emails !== "undefined") &&
-                                                    <div>{emails}</div>
-                                                }
-                                                <Divider hidden />
-                                                <Button as={Link} to="/profile" size="tiny" primary>Profile</Button>
-                                            </Item.Description>
-                                        </Item.Content>
-                                    </Item>
-                                </Item.Group>
-                                <Dropdown.Divider />
-                                <Dropdown.Item
-                                    icon="lock"
-                                    as={Link}
-                                    to="/account-security"
-                                    text="Account Security" />
-                                <Dropdown.Item
-                                    icon="handshake outline"
-                                    as={Link}
-                                    to="/consent"
-                                    text="Consent Management" />
-                                <Dropdown.Divider />
-                                <Dropdown.Item as={Link} to="/logout" text="Logout" />
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Menu.Menu>
-                )}
-            </AuthConsumer>
-        </Container>
-    </Menu>
-);
+/**
+ * Header component.
+ *
+ * @param {HeaderProps} props - Props supplied to the header component.
+ * @return {JSX.Element}
+ */
+export const Header: React.FunctionComponent<HeaderProps> = (props: HeaderProps): JSX.Element => {
+    const { onSidePanelToggleClick, showSidePanelToggle } = props;
+    return (
+        <Menu fixed="top" borderless>
+            <Container>
+                { showSidePanelToggle ?
+                    <Responsive as={ Menu.Item } maxWidth={ 767 }>
+                        <Icon name="bars" size="large" onClick={ onSidePanelToggleClick } link />
+                    </Responsive>
+                    : null
+                }
+                <Menu.Item as={ Link } to={ APP_HOME_PATH } header>
+                    <Title style={ { marginTop: 0 } }/>
+                </Menu.Item>
+                <AuthConsumer>
+                    { ({ displayName, emails, username }) => (
+                        <Menu.Menu position="right">
+                            <Dropdown
+                                item
+                                floating
+                                text={ (displayName !== "undefined") ? displayName : username }
+                                className="user-dropdown">
+                                <Dropdown.Menu>
+                                    <Item.Group>
+                                        <Item className="header">
+                                            <Item.Image size="tiny" circular src={ UserImageDummy }/>
+
+                                            <Item.Content verticalAlign="middle">
+                                                <Item.Description>
+                                                    <div>{ username }</div>
+                                                    { (emails !== "undefined") &&
+                                                    <div>{ emails }</div>
+                                                    }
+                                                    <Divider hidden/>
+                                                    <Button as={ Link } to="/profile" size="tiny"
+                                                            primary>Profile</Button>
+                                                </Item.Description>
+                                            </Item.Content>
+                                        </Item>
+                                    </Item.Group>
+                                    <Dropdown.Divider/>
+                                    <Dropdown.Item
+                                        icon="lock"
+                                        as={ Link }
+                                        to="/account-security"
+                                        text="Account Security"/>
+                                    <Dropdown.Item
+                                        icon="handshake outline"
+                                        as={ Link }
+                                        to="/consent"
+                                        text="Consent Management"/>
+                                    <Dropdown.Divider/>
+                                    <Dropdown.Item as={ Link } to="/logout" text="Logout"/>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Menu.Menu>
+                    ) }
+                </AuthConsumer>
+            </Container>
+        </Menu>
+    );
+};
+
+/**
+ * Default proptypes for the header component.
+ */
+Header.defaultProps = {
+    onSidePanelToggleClick: () => null,
+    showSidePanelToggle: true
+};
