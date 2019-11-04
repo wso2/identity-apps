c5c8e92d9fa8822c9576b2646414dfab917ba918
Nipuni Paaris <se01nipuni@gmail.com>
2019-11-04 17:22:18 +0530

    Add sinhala translation support

---
+++ apps/user-portal/src/locales/sn/common.ts
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// import { Common } from "../../models";
+
+export const common = {
+    access: "ප්\u200Dරවේශය",
+    active: "සක්\u200Dරීයයි",
+    add: "එකතු කරන්න",
+    all: "සියලුම",
+    applicationName: "යෙදුම් නම",
+    applications: "යෙදුම්",
+    approvalStatus: "අනුමත කිරීමේ තත්වය",
+    approve: "අනුමත කරන්න",
+    assignees: "පැවරුම්කරුවන්",
+    browser: "බ්\u200Dරව්සරය",
+    cancel: "අවලංගු කරන්න",
+    challengeQuestionNumber: "අභියෝගාත්මක ප්\u200Dරශ්නය {{number}}",
+    change: "වෙනස් කරන්න",
+    claim: "හිමිකම",
+    completed: "සම්පුර්ණ කරන ලද",
+    configure: "වින්\u200Dයාස කරන්න",
+    continue: "පවත්වාගෙන යන්න",
+    createdOn: "නිර්මාණය කරන ලද්දේ",
+    description: "විස්තරය",
+    deviceModel: "උපාංග ආකෘතිය",
+    edit: "සංස්කරණය කරන්න",
+    filter: "පෙරහන",
+    goBackHome: "නැවත ප්\u200Dරධාන පිටුවට",
+    help: "උපකාර",
+    hidePassword: "මුරපදය සඟවන්න",
+    initiator: "ආරම්භකයා",
+    ipAddress: "ip ලිපිනය",
+    lastAccessed: "අවසන් ප්\u200Dරවේශය",
+    lastSeen: "අවසන් වරට",
+    loginTime: "පිවිසුම් කාලය",
+    logout: "වරන්න",
+    operatingSystem: "මෙහෙයුම් පද්ධතිය",
+    operations: "මෙහෙයුම්",
+    overview: "දළ විශ්ලේෂණය",
+    personalInfo: "පෞද්ගලික තොරතුරු",
+    priority: "ප්\u200Dරමුඛතාවය",
+    privacy: "රහස්\u200Dයතාවය",
+    properties: "ගුණාංග",
+    ready: "සූදානම්",
+    reject: "ප්\u200Dරතික්ෂේප කරන්න",
+    release: "මුදා හැරීම",
+    remove: "ඉවත් කරන්න",
+    reserved: "වෙන් කරන ලද",
+    revoke: "අවලංගු කරන්න",
+    revokeAll: "සියල්ල අවලංගු කරන්න",
+    save: "සුරකින්න",
+    security: "ආරක්ෂාව",
+    showLess: "අඩුවෙන් පෙන්වන්න",
+    showMore: "වැඩිදුර තොරතුරු",
+    showPassword: "මුරපදය පෙන්වන්න",
+    submit: "ඉදිරිපත් කරන්න",
+    switch: "මාරු කරන්න",
+    terminate: "අවසන් කරන්න",
+    terminateAll: "සියල්ල අවසන් කරන්න",
+    terminateSession: "සැසිය අවසන් කරන්න",
+    type: "වර්ගය",
+    update: "යාවත්කාලීන කරන්න",
+    user: "පරිශීලක"
+};
