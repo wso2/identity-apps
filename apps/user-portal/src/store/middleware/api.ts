cfa086f7fdf50d40bb3078aa57d21c7bfd9ffe86
Nipuni Paaris <se01nipuni@gmail.com>
2019-11-29 10:58:51 +0530

    Fix consent issue

--- apps/user-portal/src/store/middleware/api.ts
+++ apps/user-portal/src/store/middleware/api.ts
@@ -16,11 +16,19 @@
  * under the License.
  */
 
+import { AxiosHttpClient } from "@wso2is/http";
 import axios from "axios";
 import log from "log";
 import { HttpRequestConfig } from "../../models";
 import { apiRequestEnd, apiRequestStart } from "../actions";
 import { API_REQUEST } from "../actions/types";
+
+/**
+ * Get an axios instance.
+ *
+ * @type {AxiosHttpClientInstance}
+ */
+const httpClient = AxiosHttpClient.getInstance();
 
 /**
  * Intercepts and handles actions of type `API_REQUEST`.
@@ -47,7 +55,7 @@ export const apiMiddleware = ({ dispatch }) => (next) => (action) => {
         dispatch(apiRequestStart(dispatcher));
     }
 
-    axios
+    httpClient
         .request({
             auth,
             [dataOrParams]: data,
