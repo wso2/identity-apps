fc317d278c1fa869f1889a35a3272fa3162c2f20
Nipuni Paaris <se01nipuni@gmail.com>
2019-12-13 10:52:02 +0530

    Change animation icons

--- apps/user-portal/src/components/shared/modal.tsx
+++ apps/user-portal/src/components/shared/modal.tsx
@@ -44,36 +44,78 @@ export const ModalComponent = (props: ModalComponentProps) => {
     const iconName = () => {
         if (type === "positive") {
             return (
-            <div className="sa">
-                <div className="sa-success">
-                    <div className="sa-success-tip"/>
-                    <div className="sa-success-long"/>
-                    <div className="sa-success-placeholder"/>
-                    <div className="sa-success-fix"/>
+                <div className="svg-box">
+                    <svg className="circular green-stroke">
+                        <circle
+                            className="path"
+                            cx="75"
+                            cy="75"
+                            r="50"
+                            fill="none"
+                            stroke-width="5"
+                            stroke-miterlimit="10"
+                        />
+                    </svg>
+                    <svg className="checkmark green-stroke">
+                        <g transform="matrix(0.79961,8.65821e-32,8.39584e-32,0.79961,-489.57,-205.679)">
+                            <path
+                                className="checkmark__check"
+                                fill="none"
+                                d="M616.306,283.025L634.087,300.805L673.361,261.53"
+                            />
+                        </g>
+                    </svg>
                 </div>
-            </div>
             );
         } else if (type === "negative") {
             return (
-                <div className="sa">
-                <div className="sa-error">
-                    <div className="sa-error-x">
-                        <div className="sa-error-left"/>
-                        <div className="sa-error-right"/>
-                    </div>
-                    <div className="sa-error-placeholder"/>
-                    <div className="sa-error-fix"/>
+                <div className="svg-box">
+                    <svg className="circular red-stroke">
+                        <circle
+                            className="path"
+                            cx="75"
+                            cy="75"
+                            r="50"
+                            fill="none"
+                            stroke-width="5"
+                            stroke-miterlimit="10"
+                        />
+                    </svg>
+                    <svg className="cross red-stroke">
+                        <g transform="matrix(0.79961,8.65821e-32,8.39584e-32,0.79961,-502.652,-204.518)">
+                            <path className="first-line" d="M634.087,300.805L673.361,261.53" fill="none"/>
+                        </g>
+                        <g transform="matrix(-1.28587e-16,-0.79961,0.79961,-1.28587e-16,-204.752,543.031)">
+                            <path className="second-line" d="M634.087,300.805L673.361,261.53"/>
+                        </g>
+                    </svg>
                 </div>
-            </div>
             );
         } else if (type === "warning") {
             return (
-                    <Icon
-                        className="modal-icon"
-                        name="exclamation circle"
-                        size="huge"
-                        color="yellow"
-                    />
+                <div className="svg-box">
+                    <svg className="circular yellow-stroke">
+                        <circle
+                            className="path"
+                            cx="75"
+                            cy="75"
+                            r="50"
+                            fill="none"
+                            stroke-width="5"
+                            stroke-miterlimit="10"
+                        />
+                    </svg>
+                    <svg className="alert-sign yellow-stroke">
+                        <g transform="matrix(1,0,0,1,-615.516,-257.346)">
+                            <g transform="matrix(0.56541,-0.56541,0.56541,0.56541,93.7153,495.69)">
+                                <path className="line" d="M634.087,300.805L673.361,261.53" fill="none"/>
+                            </g>
+                            <g transform="matrix(2.27612,-2.46519e-32,0,2.27612,-792.339,-404.147)">
+                                <circle className="dot" cx="621.52" cy="316.126" r="1.318"/>
+                            </g>
+                        </g>
+                    </svg>
+                </div>
             );
         } else {
             return (
@@ -127,5 +169,5 @@ export const ModalComponent = (props: ModalComponentProps) => {
  */
 ModalComponent.defaultProps = {
     dimmer: "blurring",
-    size: "mini"
+    size: "tiny"
 };
