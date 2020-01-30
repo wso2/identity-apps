a700dbd5ce0aca3ecd38fa99ae05b9d7bc00a530
Brion Mario <info@brionmario.com>
2020-01-30 06:24:46 +0530

    Add route models and update ui constants

--- modules/core/src/constants/ui-constants.ts
+++ modules/core/src/constants/ui-constants.ts
@@ -33,7 +33,20 @@ export class UIConstants {
      * `Gravatar` website URL.
      * @constant
      * @type {string}
-     * @default
      */
     public static readonly GRAVATAR_URL: string = "https://www.gravatar.com";
+
+    /**
+     * Constant to handle the default dashboard layout's desktop content top spacing.
+     * @constant
+     * @type {number}
+     */
+    public static readonly DEFAULT_DASHBOARD_LAYOUT_DESKTOP_CONTENT_TOP_SPACING: number = 0;
+
+    /**
+     * Constant to handle the default side panel item height.
+     * @constant
+     * @type {number}
+     */
+    public static readonly DEFAULT_SIDE_PANEL_ITEM_HEIGHT: number = 75;
 }
