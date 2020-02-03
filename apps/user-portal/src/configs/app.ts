1821a1456c6d05191059a865e392ece34eea5b19
Thivi <theviyanthan@wso2.com>
2020-02-03 12:54:50 +0530

    Rename federatedAssociation to federatedAssociations

--- apps/user-portal/src/configs/app.ts
+++ apps/user-portal/src/configs/app.ts
@@ -25,7 +25,7 @@ interface ServiceResourcesType {
     challenges: string;
     challengeAnswers: string;
     consents: string;
-    federatedAssociation: string;
+    federatedAssociations: string;
     fidoEnd: string;
     fidoMetaData: string;
     fidoStart: string;
@@ -50,7 +50,7 @@ export const ServiceResourcesEndpoint: ServiceResourcesType = {
     challengeAnswers: `${GlobalConfig.serverHost}/api/users/v1/me/challenge-answers`,
     challenges: `${GlobalConfig.serverHost}/api/users/v1/me/challenges`,
     consents: `${GlobalConfig.serverHost}/api/identity/consent-mgt/v1.0/consents`,
-    federatedAssociation: `${GlobalConfig.serverHost}/api/users/v1/me/federated-associations`,
+    federatedAssociations: `${GlobalConfig.serverHost}/api/users/v1/me/federated-associations`,
     fidoEnd: `${GlobalConfig.serverHost}/api/users/v2/me/webauthn/finish-registration`,
     fidoMetaData: `${GlobalConfig.serverHost}/api/users/v2/me/webauthn`,
     fidoStart: `${GlobalConfig.serverHost}/api/users/v2/me/webauthn/start-registration`,
