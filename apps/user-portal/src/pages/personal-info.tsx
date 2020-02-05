f3176f6e5a22796117505df1575fb2ddc0cf6554
Thivi <theviyanthan@wso2.com>
2020-02-05 10:48:57 +0530

    Add filters to the personal info page

--- apps/user-portal/src/pages/personal-info.tsx
+++ apps/user-portal/src/pages/personal-info.tsx
@@ -20,10 +20,13 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { Divider, Grid } from "semantic-ui-react";
+import appConfig from "../../app.config.json";
 import { FederatedAssociations, LinkedAccounts, Profile, ProfileExport } from "../components";
+import { EXPORT_PROFILE, LINKED_ACCOUNTS, PERSONAL_INFO, PROFILE } from "../constants";
 import { InnerPageLayout } from "../layouts";
 import { AlertInterface } from "../models";
 import { addAlert } from "../store/actions";
+import { checkEnabled } from "../utils";
 
 /**
  * Personal Info page.
@@ -33,6 +36,7 @@ import { addAlert } from "../store/actions";
 export const PersonalInfoPage = (): JSX.Element => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
+    const personalInfoConfig = appConfig[PERSONAL_INFO];
 
     /**
      * Dispatches the alert object to the redux store.
@@ -51,22 +55,46 @@ export const PersonalInfoPage = (): JSX.Element => {
             <Grid>
                 <Grid.Row columns={ 1 }>
                     <Grid.Column width={ 16 }>
-                        <Profile onAlertFired={ handleAlerts } />
+                        {
+                            checkEnabled(personalInfoConfig, PROFILE)
+                            ? (
+                                < Profile onAlertFired={ handleAlerts } />
+                            )
+                            : null
+                        }
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row columns={ 1 }>
                     <Grid.Column width={ 16 }>
-                        <LinkedAccounts onAlertFired={ handleAlerts } />
+                        {
+                            checkEnabled(personalInfoConfig, LINKED_ACCOUNTS)
+                                ? (
+                                    <LinkedAccounts onAlertFired={ handleAlerts } />
+                                )
+                                : null
+                        }
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row columns={ 1 }>
                     <Grid.Column width={ 16 }>
-                        <FederatedAssociations onAlertFired={ handleAlerts } />
+                        {
+                            checkEnabled(personalInfoConfig, EXTERNAL_LOGINS)
+                                ? (
+                                    <FederatedAssociations onAlertFired={ handleAlerts } />
+                                )
+                                : null
+                        }
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row columns={ 1 }>
                     <Grid.Column width={ 16 }>
-                        <ProfileExport onAlertFired={ handleAlerts } />
+                        {
+                            checkEnabled(personalInfoConfig, EXPORT_PROFILE)
+                                ? (
+                                    <ProfileExport onAlertFired={ handleAlerts } />
+                                )
+                                : null
+                        }
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
