dd479308043f65f294127f7a5c5ba4574edcdb69
Brion Mario <info@brionmario.com>
2020-02-11 12:35:16 +0530

    Implement selection card variation in reusable card components

---
+++ modules/react-components/src/card/selection-card.tsx
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import classNames from "classnames";
+import React, { FunctionComponent } from "react";
+import { Card, CardProps } from "semantic-ui-react";
+import { GenericIcon, GenericIconSizes } from "../icon";
+
+/**
+ * Proptypes for the selection card component.
+ */
+interface SelectionCardPropsInterface {
+    /**
+     * Additional classes.
+     */
+    className?: string;
+    description?: string;
+    header: string;
+    id?: string;
+    image?: any;
+    imageSize?: GenericIconSizes;
+    onClick?: (event: React.MouseEvent<HTMLAnchorElement>, data: CardProps) => void;
+    selected?: boolean;
+    textAlign?: "center" | "left" | "right";
+    inline?: boolean;
+}
+
+/**
+ * Selection card component.
+ *
+ * @param {SelectionCardPropsInterface} props - Props injected to the components.
+ * @return {JSX.Element}
+ * @constructor
+ */
+export const SelectionCard: FunctionComponent<SelectionCardPropsInterface> = (
+    props: SelectionCardPropsInterface
+): JSX.Element => {
+
+    const {
+        className,
+        description,
+        header,
+        id,
+        inline,
+        image,
+        imageSize,
+        onClick,
+        selected,
+        textAlign
+    } = props;
+
+    const classes = classNames(
+        "selection-card",
+        {
+            ["with-image"]: image,
+            inline,
+            selected
+        },
+        className
+    );
+
+    return (
+        <Card
+            id={ id }
+            className={ classes }
+            onClick={ onClick }
+            link={ false }
+        >
+            {
+                image && (
+                    <Card.Content className="card-image-container">
+                        <GenericIcon
+                            className="card-image"
+                            size={ imageSize }
+                            icon={ image }
+                            square
+                            transparent
+                        />
+                    </Card.Content>
+                )
+            }
+            <Card.Content className="card-text-container" style={ { textAlign } }>
+                <Card.Header>{ header }</Card.Header>
+                { description && <Card.Description>{ description }</Card.Description> }
+            </Card.Content>
+        </Card>
+    );
+};
+
+SelectionCard.defaultProps = {
+    imageSize: "tiny",
+    inline: false,
+    onClick: (event: React.MouseEvent<HTMLAnchorElement>, data: CardProps) => null,
+    textAlign: "center"
+};
--- modules/react-components/stories/components/card/card.stories.meta.ts
+++ modules/react-components/stories/components/card/card.stories.meta.ts
@@ -17,24 +17,21 @@
  *
  */
 
-export * from "./alert";
-export * from "./avatar";
-export * from "./brand";
-export * from "./button";
-export * from "./card";
-export * from "./danger-zone";
-export * from "./error";
-export * from "./footer";
-export * from "./header";
-export * from "./icon";
-export * from "./input";
-export * from "./language-switcher";
-export * from "./list";
-export * from "./loader";
-export * from "./modal";
-export * from "./page-header";
-export * from "./pagination";
-export * from "./placeholder";
-export * from "./route";
-export * from "./section";
-export * from "./side-panel";
+import { StoryCategories } from "../../hierarchy";
+import { StoryMetaInterface } from "../../models";
+
+export const meta: StoryMetaInterface = {
+    components: [ "SelectionCard" ],
+    description: "Component to cards.",
+    stories: [
+        {
+            description: "Default Card.",
+            title: "Card",
+        },
+        {
+            description: "Card to represent selections.",
+            title: "Selection Card",
+        }
+    ],
+    title: `${ StoryCategories.COMPONENTS }/Card`,
+};
