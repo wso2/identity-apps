4b816dc7527f9ca5811fa1b113c55467313154eb
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2020-02-15 14:46:56 +0530

    Add eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 9,
+        sourceType: 'module',
+    },
+    settings: {
+        react: {
+            version: "detect"
+        },
+    },
+    rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    },
+};
