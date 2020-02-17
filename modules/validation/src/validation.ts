8a478ae6b4580bb3a7607f046b36a9429aa9bee2
Thivi <theviyanthan@wso2.com>
2020-02-17 21:29:36 +0530

    Add imageUrl validator

--- modules/validation/src/validation.ts
+++ modules/validation/src/validation.ts
@@ -18,7 +18,9 @@
  */
 
 import Joi from "@hapi/joi";
+import "axios";
 import "./plugins/text-encoder-polyfill";
+import Axios from "axios";
 
 type ValidationFunction = (value: string) => boolean;
 
@@ -56,4 +58,25 @@ export const url: ValidationFunction = (value: string): boolean => {
         return false;
     }
     return true;
+};
+
+/**
+ * Checks if the image url is valid
+ * @param value Url
+ */
+export const imageUrl = async (value: string): Promise<boolean> => {
+        if (
+            Joi.string()
+                .uri()
+                .validate(value).error
+        ) {
+            return Promise.resolve(false);
+        } else {
+            try {
+                const response = await Axios.get(value);
+                return Promise.resolve(response.headers["content-type"].includes("image"));
+            } catch (error) {
+                return Promise.resolve(false);
+            }
+        }
 };
