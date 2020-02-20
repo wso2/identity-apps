9a20502c91d58f7b7fca35d908850151eda01a20
Brion Mario <info@brionmario.com>
2020-02-21 02:57:40 +0530

    Add missing doc comments

--- apps/admin-portal/src/components/applications/forms/general-details-form.tsx
+++ apps/admin-portal/src/components/applications/forms/general-details-form.tsx
@@ -27,12 +27,33 @@ import { FormValidation } from "@wso2is/validation";
  * Proptypes for the applications general details form component.
  */
 interface GeneralDetailsFormPopsInterface {
+    /**
+     * Application access URL.
+     */
     accessUrl?: string;
+    /**
+     * Currently editing application id.
+     */
     appId?: string;
+    /**
+     * Application description.
+     */
     description?: string;
+    /**
+     * Is the application discoverable.
+     */
     discoverability?: boolean;
+    /**
+     * Application logo URL.
+     */
     imageUrl?: string;
+    /**
+     * Name of the application.
+     */
     name: string;
+    /**
+     * On submit callback.
+     */
     onSubmit: (values: any) => void;
 }
 
@@ -53,10 +74,16 @@ export const GeneralDetailsForm: FunctionComponent<GeneralDetailsFormPopsInterfa
         onSubmit
     } = props;
 
-    const [isDiscoverable, setDiscoverability] = useState<boolean>(discoverability);
+    const [ isDiscoverable, setDiscoverability ] = useState<boolean>(discoverability);
 
+    /**
+     * Prepare form values for submitting.
+     *
+     * @param values - Form values.
+     * @return {any} Sanitized form values.
+     */
     const updateConfigurations = (values: Map<string, FormValue>): ApplicationInterface => {
-        const editingApplication: ApplicationInterface = {
+        return  {
             accessUrl: values.get("accessUrl").toString(),
             advancedConfigurations: {
                 discoverableByEndUsers: !!values.get("discoverableByEndUsers").includes("discoverable"),
@@ -66,9 +93,14 @@ export const GeneralDetailsForm: FunctionComponent<GeneralDetailsFormPopsInterfa
             imageUrl: values.get("imageUrl").toString(),
             name: values.get("name").toString(),
         };
-        return editingApplication;
     };
 
+    /**
+     * Handles form value change.
+     *
+     * @param {boolean} isPure - Is the form pure.
+     * @param {Map<string, FormValue>} values - Form values
+     */
     const handleFormValuesOnChange = (isPure: boolean, values: Map<string, FormValue>) => {
         // Set the discoverability based on the checkbox toggle.
         if (values.get("discoverableByEndUsers").includes("discoverable") !== isDiscoverable) {
@@ -77,104 +109,102 @@ export const GeneralDetailsForm: FunctionComponent<GeneralDetailsFormPopsInterfa
     };
 
     return (
-        <>
-            <Forms
-                onSubmit={ (values) => {
-                    onSubmit(updateConfigurations(values))
-                } }
-                onChange={ handleFormValuesOnChange }
-            >
-                <Grid>
-                    <Grid.Row columns={ 1 }>
-                        <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
-                            <Field
-                                name="name"
-                                label="Application Name"
-                                required={ true }
-                                requiredErrorMessage="Application name is required"
-                                placeholder={ name }
-                                type="text"
-                                value={ name }
-                            />
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row columns={ 1 }>
-                        <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
-                            <Field
-                                name="description"
-                                label="Description"
-                                required={ false }
-                                requiredErrorMessage=""
-                                placeholder="Enter a description for the application"
-                                type="textarea"
-                                value={ description }
-                            />
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row columns={ 1 }>
-                        <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
-                            <Field
-                                name="imageUrl"
-                                label="Application Image"
-                                required={ false }
-                                requiredErrorMessage=""
-                                placeholder="Enter a image url for the application"
-                                type="text"
-                                validation={ (value: string, validation: Validation) => {
-                                    if (!FormValidation.url(value)) {
-                                        validation.isValid = false;
-                                        validation.errorMessages.push("This is not a valid URL");
-                                    }
-                                } }
-                                value={ imageUrl }
-                            />
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row columns={ 1 }>
-                        <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
-                            <Field
-                                name="discoverableByEndUsers"
-                                required={ false }
-                                requiredErrorMessage=""
-                                type="checkbox"
-                                children={ [
-                                    {
-                                        label: "Discoverable application",
-                                        value: "discoverable"
-                                    }
-                                ] }
-                                value={ isDiscoverable ? ["discoverable"] : [] }
-                            />
-                            <Field
-                                name="accessUrl"
-                                label="Access URL"
-                                required={ isDiscoverable }
-                                requiredErrorMessage={ "A valid access URL needs to be defined for" +
-                                " an application to be marked as discoverable" }
-                                placeholder="Enter access url for the application login page"
-                                type="text"
-                                validation={ (value: string, validation: Validation) => {
-                                    if (!FormValidation.url(value)) {
-                                        validation.isValid = false;
-                                        validation.errorMessages.push("This is not a valid URL");
-                                    }
-                                } }
-                                value={ accessUrl }
-                            />
-                            <Hint>
-                                Applications flagged as discoverable are visible for end users.
-                            </Hint>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row columns={ 1 }>
-                        <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
-                            <Button primary type="submit" size="small" className="form-button">
-                                Update
-                            </Button>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Forms>
-        </>
+        <Forms
+            onSubmit={ (values) => {
+                onSubmit(updateConfigurations(values))
+            } }
+            onChange={ handleFormValuesOnChange }
+        >
+            <Grid>
+                <Grid.Row columns={ 1 }>
+                    <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
+                        <Field
+                            name="name"
+                            label="Application Name"
+                            required={ true }
+                            requiredErrorMessage="Application name is required"
+                            placeholder={ name }
+                            type="text"
+                            value={ name }
+                        />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={ 1 }>
+                    <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
+                        <Field
+                            name="description"
+                            label="Description"
+                            required={ false }
+                            requiredErrorMessage=""
+                            placeholder="Enter a description for the application"
+                            type="textarea"
+                            value={ description }
+                        />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={ 1 }>
+                    <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
+                        <Field
+                            name="imageUrl"
+                            label="Application Image"
+                            required={ false }
+                            requiredErrorMessage=""
+                            placeholder="Enter a image url for the application"
+                            type="text"
+                            validation={ (value: string, validation: Validation) => {
+                                if (!FormValidation.url(value)) {
+                                    validation.isValid = false;
+                                    validation.errorMessages.push("This is not a valid URL");
+                                }
+                            } }
+                            value={ imageUrl }
+                        />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={ 1 }>
+                    <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
+                        <Field
+                            name="discoverableByEndUsers"
+                            required={ false }
+                            requiredErrorMessage=""
+                            type="checkbox"
+                            children={ [
+                                {
+                                    label: "Discoverable application",
+                                    value: "discoverable"
+                                }
+                            ] }
+                            value={ isDiscoverable ? [ "discoverable" ] : [] }
+                        />
+                        <Field
+                            name="accessUrl"
+                            label="Access URL"
+                            required={ isDiscoverable }
+                            requiredErrorMessage={ "A valid access URL needs to be defined for" +
+                            " an application to be marked as discoverable" }
+                            placeholder="Enter access url for the application login page"
+                            type="text"
+                            validation={ (value: string, validation: Validation) => {
+                                if (!FormValidation.url(value)) {
+                                    validation.isValid = false;
+                                    validation.errorMessages.push("This is not a valid URL");
+                                }
+                            } }
+                            value={ accessUrl }
+                        />
+                        <Hint>
+                            Applications flagged as discoverable are visible for end users.
+                        </Hint>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={ 1 }>
+                    <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 8 }>
+                        <Button primary type="submit" size="small" className="form-button">
+                            Update
+                        </Button>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Forms>
     );
 };
