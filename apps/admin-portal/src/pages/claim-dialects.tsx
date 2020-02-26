8a954e7a53bb6b8f7211e1ee684f1b21cebec5a7
Thivi <theviyanthan@wso2.com>
2020-02-26 15:14:49 +0530

    Add claim dialects page

---
+++ apps/admin-portal/src/pages/claim-dialects.tsx
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, { useEffect, useState } from "react";
+import { PageLayout } from "../layouts";
+import { ListLayout } from "../layouts";
+import { PrimaryButton } from "@wso2is/react-components";
+import { Icon, DropdownProps, PaginationProps } from "semantic-ui-react";
+import { ClaimsList, ListType } from "../components";
+import { ClaimDialect } from "../models";
+import { getClaimDialect } from "../api";
+import { DEFAULT_USER_LIST_ITEM_LIMIT } from "../constants";
+
+export const ClaimDialectsPage = (): React.ReactElement => {
+
+    const [dialects, setDialects] = useState<ClaimDialect[]>(null);
+    const [offset, setOffset] = useState(0);
+    const [listItemLimit, setListItemLimit] = useState<number>(0);
+
+    useEffect(() => {
+
+        setListItemLimit(DEFAULT_USER_LIST_ITEM_LIMIT);
+
+        getClaimDialect().then((response: ClaimDialect[]) => {
+            const filteredResponse = response.filter((claim: ClaimDialect) => {
+                return claim.id !== "local";
+            });
+            setDialects(filteredResponse);
+        }).catch(error => {
+            // TODO: Notify
+        })
+    }, []);
+
+    const paginate = (list: ClaimDialect[], limit: number, offset: number): ClaimDialect[] => {
+        return list?.slice(offset, offset + limit);
+    };
+
+    const handleItemsPerPageDropdownChange = (event: React.MouseEvent<HTMLAnchorElement>, data: DropdownProps) => {
+        setListItemLimit(data.value as number);
+    };
+
+    const handlePaginationChange = (event: React.MouseEvent<HTMLAnchorElement>, data: PaginationProps) => {
+        setOffset((data.activePage as number - 1) * listItemLimit);
+    };
+
+    return (
+        <PageLayout
+            title="Claims"
+            description="View, edit and add claims"
+            showBottomDivider={true}
+        >
+            <ListLayout
+                advancedSearch={null}
+                currentListSize={listItemLimit}
+                listItemLimit={listItemLimit}
+                onItemsPerPageDropdownChange={handleItemsPerPageDropdownChange}
+                onPageChange={handlePaginationChange}
+                onSortStrategyChange={null}
+                rightActionPanel={
+                    (
+                        <PrimaryButton
+                            onClick={() => {
+                            }}
+                        >
+                            <Icon name="add" />Add a dialect
+                        </PrimaryButton>
+                    )
+                }
+                showPagination={true}
+                sortOptions={null}
+                sortStrategy={null}
+                totalPages={Math.ceil(dialects?.length / listItemLimit)}
+                totalListSize={dialects?.length}
+            >
+                <ClaimsList list={paginate(dialects, listItemLimit, offset)} localClaim={ListType.DIALECT} />
+            </ListLayout>
+        </PageLayout>
+    );
+};
