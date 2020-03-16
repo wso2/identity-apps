73b2e8888ae33c22c2ba102a8bf9e3cd8f9492c4
Thivi <theviyanthan@wso2.com>
2020-03-16 16:44:37 +0530

    Add delete functionality to user store management.

--- apps/admin-portal/src/components/user-stores/user-stores-list.tsx
+++ apps/admin-portal/src/components/user-stores/user-stores-list.tsx
@@ -16,9 +16,13 @@
 * under the License.
 */
 
-import React from "react";
-import { ResourceList } from "@wso2is/react-components";
-import { UserStoreListItem } from "../../models";
+import React, { useState } from "react";
+import { ResourceList, LinkButton, PrimaryButton } from "@wso2is/react-components";
+import { UserStoreListItem, AlertLevels } from "../../models";
+import { Modal } from "semantic-ui-react";
+import { deleteUserStore } from "../../api";
+import { useDispatch } from "react-redux";
+import { addAlert } from "../../store/actions";
 
 interface UserStoresListPropsInterface {
     list: UserStoreListItem[];
@@ -28,36 +32,103 @@ interface UserStoresListPropsInterface {
 export const UserStoresList = (props: UserStoresListPropsInterface): React.ReactElement => {
 
     const { list, openEdit, update } = props;
+
+    const [deleteConfirm, setDeleteConfirm] = useState(false);
+    const [deleteID, setDeleteID] = useState<string>(null);
+
+    const dispatch = useDispatch();
+
+    const initDelete = (id: string) => {
+        setDeleteID(id);
+        setDeleteConfirm(true);
+    };
+
+    const closeDeleteConfirm = () => {
+        setDeleteConfirm(false);
+        setDeleteID(null);
+    }
+
+    const showDeleteConfirm = (): React.ReactElement => {
+        return (
+            <Modal
+                open={ deleteConfirm }
+                onClose={ closeDeleteConfirm }
+                size="mini"
+                dimmer="blurring"
+            >
+                <Modal.Header>
+                    Confirm Delete
+                </Modal.Header>
+                <Modal.Content>
+                    This will completely remove the user store and the data in it.
+                    Do you want to continue deleting it?
+                </Modal.Content>
+                <Modal.Actions>
+                    <LinkButton onClick={ closeDeleteConfirm }>
+                        Cancel
+                    </LinkButton>
+                    <PrimaryButton onClick={ () => {
+
+                        deleteUserStore(deleteID)
+                            .then(() => {
+                                dispatch(addAlert({
+                                    message: "User Store deleted successfully!",
+                                    description: "The user store has been deleted successfully!",
+                                    level: AlertLevels.SUCCESS
+                                }));
+                                update();
+                                closeDeleteConfirm();
+                            })
+                            .catch(error => {
+                                dispatch(addAlert({
+                                    message: error?.message ?? "Something went wrong!",
+                                    description: error?.description ?? "There was an error while deleting the user store",
+                                    level: AlertLevels.ERROR
+                                }));
+                                closeDeleteConfirm();
+                            });
+
+                    } }>
+                        Delete
+                    </PrimaryButton>
+                </Modal.Actions>
+            </Modal>
+        )
+    };
+
     return (
-        <ResourceList>
-            {
-                list?.map((userStore: UserStoreListItem, index: number) => {
-                    return (
-                        <ResourceList.Item
-                            key={ index }
-                            actions={ [
-                                {
-                                    icon: "pencil alternate",
-                                    onClick: () => {
-                                        openEdit(userStore.id);
+        <>
+            {showDeleteConfirm()}
+            <ResourceList>
+                {
+                    list?.map((userStore: UserStoreListItem, index: number) => {
+                        return (
+                            <ResourceList.Item
+                                key={ index }
+                                actions={ [
+                                    {
+                                        icon: "pencil alternate",
+                                        onClick: () => {
+                                            openEdit(userStore.id);
+                                        },
+                                        popupText: "edit",
+                                        type: "button"
                                     },
-                                    popupText: "Edit",
-                                    type: "button"
-                                },
-                                {
-                                    icon: "trash alternate",
-                                    onClick: () => {update()},
-                                    popupText: "Delete",
-                                    type: "dropdown"
-                                }
-                            ] }
-                            actionsFloated="right"
-                            itemHeader={ userStore.name }
-                            metaContent={ userStore.description }
-                        />
-                    )
-                })
-            }
-        </ResourceList>
+                                    {
+                                        icon: "trash alternate",
+                                        onClick: () => { initDelete(userStore.id) },
+                                        popupText: "delete",
+                                        type: "dropdown"
+                                    }
+                                ] }
+                                actionsFloated="right"
+                                itemHeader={ userStore.name }
+                                metaContent={ userStore.description }
+                            />
+                        )
+                    })
+                }
+            </ResourceList>
+        </>
     )
 }
