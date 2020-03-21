ffe298ec416e1499661d104829fb08018c1b2328
Brion Mario <info@brionmario.com>
2020-03-21 22:30:57 +0530

    Refactor i18n helpers and add custom backend function

--- modules/i18n/src/helpers.ts
+++ modules/i18n/src/helpers.ts
@@ -21,7 +21,15 @@ import { I18nModuleConstants } from "./constants";
 import * as LOCALES from "./content";
 import { SupportedLanguages } from "./models";
 
-export const generateI18nOptions = (options: InitOptions, override: boolean, debug: boolean) => {
+/**
+ * Generate the i18n options.
+ *
+ * @param {i18next.InitOptions} options - Options override.
+ * @param {boolean} override - Should the passed in options replace the default.
+ * @param {boolean} debug - Debug enabled flag.
+ * @return {InitOptions} Init options config.
+ */
+export const generateI18nOptions = (options: InitOptions, override: boolean, debug: boolean): InitOptions => {
 
     const DEFAULT_INIT_OPTIONS: InitOptions = {
         contextSeparator: "_",
@@ -32,10 +40,10 @@ export const generateI18nOptions = (options: InitOptions, override: boolean, deb
             escapeValue: false
         },
         keySeparator: ".",
-        ns: readNamespaces(),
+        ns: loadDefaultNamespaces(),
         nsSeparator: ":",
         pluralSeparator: "_",
-        resources: loadResources()
+        resources: loadDefaultResources()
     };
 
     if (override) {
@@ -52,7 +60,12 @@ export const generateI18nOptions = (options: InitOptions, override: boolean, deb
     };
 };
 
-export const readNamespaces = (): string[] => {
+/**
+ * Load the namespaces from the default bundle which is inside the module.
+ *
+ * @return {string[]} Namespace array.
+ */
+const loadDefaultNamespaces = (): string[] => {
 
     const namespaces: string[] = [];
 
@@ -67,20 +80,69 @@ export const readNamespaces = (): string[] => {
     return namespaces;
 };
 
-export const loadResources = (): Resource => {
+/**
+ * Load the default resource bundle available inside the module.
+ *
+ * @return {i18next.Resource} Resource bundle.
+ */
+const loadDefaultResources = (): Resource => {
 
     let resources: Resource = {};
 
-    for (const value of Object.values(LOCALES)) {
-        resources = {
-            ...resources,
-            [ value.meta.code ]: value.resources
-        };
+    for (const locale of Object.values(LOCALES)) {
+        // Try to find the namespace resource file based on the namespaces array declared in meta.
+        for(const resource of Object.values(locale.resources)) {
+            resources = {
+                ...resources,
+                [ locale.meta.code ]: {
+                    ...resources[ locale.meta.code ],
+                    ...resource as object
+                }
+            };
+        }
     }
 
     return resources;
 };
 
+/**
+ * Custom backend to fetch resources at runtime.
+ *
+ * @param {SupportedLanguages} meta - Meta information.
+ * @param {string} bundleLocation - Resource bundle path.
+ * @return {Promise<i18next.Resource>} Resources as a promise.
+ */
+export const loadResourcesAtRuntime = async (meta: SupportedLanguages, bundleLocation: string): Promise<Resource> => {
+
+    let resources: Resource = {};
+
+    for (const locale of Object.values(meta)) {
+        for (const [ nsKey, nsPath ] of Object.entries(locale.paths)) {
+            try {
+                const response = await fetch(`${ bundleLocation }/${ nsPath }`);
+                const payload = await response.json();
+
+                resources = {
+                    ...resources,
+                    [ locale.code ]: {
+                        ...resources[ locale.code ],
+                        [ nsKey ]: payload as any
+                    }
+                }
+            } catch (e) {
+                // Handle error.
+            }
+        }
+    }
+
+    return resources;
+};
+
+/**
+ * Get the supported list of languages.
+ *
+ * @return {SupportedLanguages} Supported languages.
+ */
 export const getSupportedLanguages = (): SupportedLanguages => {
 
     let meta = {};
