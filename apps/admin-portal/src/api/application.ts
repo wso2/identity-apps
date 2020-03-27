50e7682fcb1e3c01461f93c46fb91f6bd8c0bf33
inthirakumaaran <inthira.14@cse.mrt.ac.lk>
2020-03-28 00:06:14 +0530

    Application template get api methods added.

--- apps/admin-portal/src/api/application.ts
+++ apps/admin-portal/src/api/application.ts
@@ -26,6 +26,8 @@ import {
     ApplicationBasicInterface,
     ApplicationInterface,
     ApplicationListInterface,
+    ApplicationTemplate,
+    ApplicationTemplateListInterface,
     AuthProtocolMetaListItemInterface,
     HttpMethods,
     OIDCDataInterface,
@@ -578,6 +580,96 @@ export const getAdaptiveAuthTemplates = (): Promise<AdaptiveAuthTemplatesListInt
         }).catch((error: AxiosError) => {
             throw new IdentityAppsApiException(
                 ApplicationManagementConstants.ADAPTIVE_AUTH_TEMPLATES_FETCH_ERROR,
+                error.stack,
+                error.code,
+                error.request,
+                error.response,
+                error.config);
+        });
+};
+
+/**
+ * Get Application Template data.
+ *
+ * @param templateId Template Id of the application.
+ */
+export const getApplicationTemplateData = (templateId: string): Promise<any> => {
+    const requestConfig = {
+        headers: {
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": GlobalConfig.clientHost,
+            "Content-Type": "application/json"
+        },
+        method: HttpMethods.GET,
+        url: ServiceResourcesEndpoint.applications + "/templates/" + templateId
+    };
+
+    return httpClient.request(requestConfig)
+        .then((response: AxiosResponse) => {
+            if (response.status !== 200) {
+                throw new IdentityAppsApiException(
+                    ApplicationManagementConstants.APPLICATION_TEMPLATE_FETCH_INVALID_STATUS_CODE_ERROR,
+                    null,
+                    response.status,
+                    response.request,
+                    response,
+                    response.config);
+            }
+
+            return Promise.resolve(response.data as ApplicationTemplate);
+        }).catch((error: AxiosError) => {
+            throw new IdentityAppsApiException(
+                ApplicationManagementConstants.APPLICATION_TEMPLATE_FETCH_ERROR,
+                error.stack,
+                error.code,
+                error.request,
+                error.response,
+                error.config);
+        });
+};
+
+/**
+ * Gets the application template list with limit and offset.
+ *
+ * @param {number} limit - Maximum Limit of the application template List.
+ * @param {number} offset - Offset for get to start.
+ * @param {string} filter - Search filter.
+ *
+ * @return {Promise<ApplicationListInterface>} A promise containing the response.
+ */
+export const getApplicationTemplateList = (limit?: number, offset?: number,
+                                           filter?: string): Promise<any> => {
+    const requestConfig = {
+        headers: {
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": GlobalConfig.clientHost,
+            "Content-Type": "application/json"
+        },
+        method: HttpMethods.GET,
+        params: {
+            filter,
+            limit,
+            offset
+        },
+        url: ServiceResourcesEndpoint.applications + "/templates"
+    };
+
+    return httpClient.request(requestConfig)
+        .then((response: AxiosResponse) => {
+            if (response.status !== 200) {
+                throw new IdentityAppsApiException(
+                    ApplicationManagementConstants.APPLICATION_TEMPLATES_LIST_FETCH_INVALID_STATUS_CODE_ERROR,
+                    null,
+                    response.status,
+                    response.request,
+                    response,
+                    response.config);
+            }
+
+            return Promise.resolve(response.data as ApplicationTemplateListInterface);
+        }).catch((error: AxiosError) => {
+            throw new IdentityAppsApiException(
+                ApplicationManagementConstants.APPLICATION_TEMPLATES_LIST_FETCH_ERROR,
                 error.stack,
                 error.code,
                 error.request,
