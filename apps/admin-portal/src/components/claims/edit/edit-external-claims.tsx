ad94e7b29bcc2843d508caf497c9d6a67d184d18
Thivi <theviyanthan@wso2.com>
2020-04-04 01:50:50 +0530

    Add edit externals claims tab pane

--- apps/admin-portal/src/components/claims/edit/edit-external-claims.tsx
+++ apps/admin-portal/src/components/claims/edit/edit-external-claims.tsx
@@ -1,183 +1,264 @@
 /**
-* Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-* WSO2 Inc. licenses this file to you under the Apache License,
-* Version 2.0 (the 'License'); you may not use this file except
-* in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-import { AlertLevels, Claim, ExternalClaim } from "../../../models";
-import { Field, Forms, FormValue, useTrigger } from "@wso2is/forms";
-import { getAllLocalClaims, getAnExternalClaim, updateAnExternalClaim } from "../../../api";
-import { Header, Modal } from "semantic-ui-react";
-import { LinkButton, PrimaryButton } from "@wso2is/react-components";
-import React, { useEffect, useState } from "react";
+import { AlertLevels, AppConfigInterface, ClaimDialect, ExternalClaim } from "../../../models";
+import { ClaimsList, ExternalClaimsSearch, ListType } from "../../../components";
+import { DropdownProps, Icon, PaginationProps } from "semantic-ui-react";
+import { filterList, sortList } from "../../../utils";
+import { getADialect, getAllExternalClaims } from "../../../api";
+import React, { useContext, useEffect, useState } from "react";
 
 import { addAlert } from "@wso2is/core/store";
+import { AppConfig } from "../../../helpers";
+import { DEFAULT_USER_LIST_ITEM_LIMIT } from "../../../constants";
+import { EmptyPlaceholder } from "../../../components/shared";
+import { EmptyPlaceholderIllustrations } from "../../../configs";
+import { ListLayout } from "../../../layouts";
+import { PrimaryButton } from "@wso2is/react-components";
 import { useDispatch } from "react-redux";
 
-/**
- * Prop types of `EditExternalClaims` component
- */
-interface EditExternalClaimsPropsInterface {
+interface EditExternalClaimsPropsInterface{
     /**
-     * Opens the modal
-     */
-    open: boolean;
-    /**
-     * Called when the modal is closed
-     */
-    onClose: () => void;
-    /**
-     * The claim ID to be edited
-     */
-    claimID: string;
-    /**
-     * The ID of the dialect the claim belongs to
+     * Dialect ID
      */
     dialectID: string;
-    /**
-     * Called to initiate an update
-     */
-    update: () => void;
 }
 
 /**
- * This component renders the edit external claim modal
- * @param {EditExternalClaimsPropsInterface} props
+ * This lists the external claims.
+ *
+ * @param {EditExternalClaimsPropsInterface} props.
+ *
  * @return {React.ReactElement}
  */
 export const EditExternalClaims = (props: EditExternalClaimsPropsInterface): React.ReactElement => {
 
-    const { open, onClose, claimID, update, dialectID } = props;
+    /**
+     * Attributes to sort the list by
+     */
+    const SORT_BY = [
+        {
+            key: 0,
+            text: "Claim URI",
+            value: "claimURI"
+        },
+        {
+            key: 1,
+            text: "Mapped Local Claim URI",
+            value: "mappedLocalClaimURI"
+        }
+    ];
 
-    const [localClaims, setLocalClaims] = useState<Claim[]>();
-    const [claim, setClaim] = useState<ExternalClaim>(null);
-
-    const [submit, setSubmit] = useTrigger();
+    const [ claims, setClaims ] = useState<ExternalClaim[]>(null);
+    const [ offset, setOffset ] = useState(0);
+    const [ listItemLimit, setListItemLimit ] = useState<number>(0);
+    const [ dialect, setDialect ] = useState<ClaimDialect>(null);
+    const [ addClaim, setAddClaim ] = useState(false);
+    const [ editClaim, setEditClaim ] = useState(false);
+    const [ editClaimID, setEditClaimID ] = useState("");
+    const [ filteredClaims, setFilteredClaims ] = useState<ExternalClaim[]>(null);
+    const [ sortBy, setSortBy ] = useState(SORT_BY[ 0 ]);
+    const [ sortOrder, setSortOrder ] = useState(true);
+    const [ isLoading, setIsLoading ] = useState(true);
 
     const dispatch = useDispatch();
 
+    const appConfig: AppConfigInterface = useContext(AppConfig);
+
+    const { dialectID } = props;
+
     useEffect(() => {
-        getAllLocalClaims(null).then(response => {
-            setLocalClaims(response);
+        setListItemLimit(DEFAULT_USER_LIST_ITEM_LIMIT);
+        setIsLoading(true);
+
+        getADialect(dialectID).then(response => {
+            setDialect(response);
+            setIsLoading(false);
         }).catch(error => {
+            setIsLoading(false);
             dispatch(addAlert(
                 {
-                    description: error?.description || "There was an error while fetching local claims",
+                    description: error?.description || "There was an error while fetching local dialect",
                     level: AlertLevels.ERROR,
                     message: error?.message || "Something went wrong"
                 }
             ));
         });
 
-        getAnExternalClaim(dialectID, claimID).then(response => {
-            setClaim(response);
+    }, []);
+
+    /**
+     * Fetch external claims.
+     *
+     * @param {number} limit.
+     * @param {number} offset.
+     * @param {string} sort.
+     * @param {string} filter.
+     */
+    const getExternalClaims = (limit?: number, offset?: number, sort?: string, filter?: string) => {
+        dialectID && getAllExternalClaims(dialectID, {
+            filter,
+            limit,
+            offset,
+            sort
+        }).then(response => {
+            setClaims(response);
+            setFilteredClaims(response);
         }).catch(error => {
             dispatch(addAlert(
                 {
-                    description: error?.description || "There was an error while fetching the external claim",
+                    description: error?.description || "There was an error while fetching the external claims",
                     level: AlertLevels.ERROR,
                     message: error?.message || "Something went wrong"
                 }
             ));
-        })
-    }, []);
+        });
+    }
+
+    useEffect(() => {
+        getExternalClaims();
+    }, [ dialectID ]);
+
+    useEffect(() => {
+        setFilteredClaims(sortList(filteredClaims, sortBy.value, sortOrder));
+    }, [ sortBy, sortOrder ]);
+
+    /**
+     * Slices and returns a portion of the list.
+     *
+     * @param {ExternalClaim[]} list.
+     * @param {number} limit.
+     * @param {number} offset.
+     * 
+     * @return {ExternalClaim[]}
+     */
+    const paginate = (list: ExternalClaim[], limit: number, offset: number): ExternalClaim[] => {
+        return list?.slice(offset, offset + limit);
+    };
+
+    /**
+     * Handles change in the number of items to show.
+     *
+     * @param {React.MouseEvent<HTMLAnchorElement>} event.
+     * @param {data} data.
+     */
+    const handleItemsPerPageDropdownChange = (event: React.MouseEvent<HTMLAnchorElement>, data: DropdownProps) => {
+        setListItemLimit(data.value as number);
+    };
+
+    /**
+    * Paginates.
+    * @param {React.MouseEvent<HTMLAnchorElement>} event.
+    * @param {PaginationProps} data.
+    */
+    const handlePaginationChange = (event: React.MouseEvent<HTMLAnchorElement>, data: PaginationProps) => {
+        setOffset((data.activePage as number - 1) * listItemLimit);
+    };
+
+    /**
+     * Handle sort strategy change.
+     *
+     * @param {React.SyntheticEvent<HTMLElement>} event.
+     * @param {DropdownProps} data.
+     */
+    const handleSortStrategyChange = (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+        setSortBy(SORT_BY.filter(option => option.value === data.value)[ 0 ]);
+    };
+
+    /**
+    * Handles sort order change.
+     *
+    * @param {boolean} isAscending.
+    */
+    const handleSortOrderChange = (isAscending: boolean) => {
+        setSortOrder(isAscending);
+    };
 
     return (
-        <Modal
-            dimmer="blurring"
-            size="tiny"
-            open={ open }
-            onClose={ onClose }
-        >
-            <Modal.Header>
-                <Header as="h3" content={ "Edit External Claim" } subheader={ claim?.claimURI } />
-            </Modal.Header>
-            <Modal.Content>
-                <Forms
-                    onSubmit={ (values: Map<string, FormValue>) => {
-                        updateAnExternalClaim(dialectID, claimID, {
-                            claimURI: values.get("claimURI").toString(),
-                            mappedLocalClaimURI: values.get("localClaim").toString()
-                        }).then(() => {
-                            dispatch(addAlert(
-                                {
-                                    description: "The external claim has been updated successfully!",
-                                    level: AlertLevels.SUCCESS,
-                                    message: "External claim updated successfully"
-                                }
-                            ));
-                            onClose();
-                            update();
-                        }).catch(error => {
-                            dispatch(addAlert(
-                                {
-                                    description: error?.description || "There was an error while updating the" +
-                                        " external claim",
-                                    level: AlertLevels.ERROR,
-                                    message: error?.message || "Something went wrong"
-                                }
-                            ));
-                        })
-                    } }
-                    submitState={ submit }
-                >
-                    <Field
-                        name="claimURI"
-                        label="Claim URI"
-                        required={ true }
-                        requiredErrorMessage="Claim URI is required"
-                        placeholder="Enter a claim URI"
-                        type="text"
-                        readOnly
-                        value={ claim?.claimURI }
-                    />
-                    <Field
-                        type="dropdown"
-                        name="localClaim"
-                        label="Local Claim URI to map to"
-                        required={ true }
-                        requiredErrorMessage="Select a local claim to map to"
-                        placeholder="Select a Local Claim"
-                        search
-                        value={ claim?.mappedLocalClaimURI }
-                        children={
-                            localClaims?.map((claim: Claim, index) => {
-                                return {
-                                    key: index,
-                                    text: claim?.displayName,
-                                    value: claim?.claimURI
-                                }
-                            })
+        claims?.length > 0
+            ? (
+                <ListLayout
+                    advancedSearch={ <ExternalClaimsSearch onFilter={ (query) => {
+                        //TODO: getExternalClaims(null, null, null, query);
+                        try {
+                            const filteredList: ExternalClaim[] = filterList(
+                                claims, query, sortBy.value, sortOrder
+                            );
+                            setFilteredClaims(filteredList);
+                        } catch (error) {
+                            dispatch(addAlert({
+                                description: error?.message,
+                                level: AlertLevels.ERROR,
+                                message: "Filter query format incorrect"
+                            }));
                         }
+                    } } /> }
+                    currentListSize={ listItemLimit }
+                    listItemLimit={ listItemLimit }
+                    onItemsPerPageDropdownChange={ handleItemsPerPageDropdownChange }
+                    onPageChange={ handlePaginationChange }
+                    onSortStrategyChange={ handleSortStrategyChange }
+                    onSortOrderChange={ handleSortOrderChange }
+                    rightActionPanel={
+                        appConfig?.claimDialects?.features?.externalClaims?.permissions?.create && (
+                            <PrimaryButton
+                                onClick={ () => {
+                                    setAddClaim(true);
+                                } }
+                            >
+                                <Icon name="add" />Add a claim
+                            </PrimaryButton>
+                        )
+                    }
+                    showPagination={ true }
+                    sortOptions={ SORT_BY }
+                    sortStrategy={ sortBy }
+                    totalPages={ Math.ceil(filteredClaims?.length / listItemLimit) }
+                    totalListSize={ filteredClaims?.length }
+                >
+                    <ClaimsList
+                        list={ paginate(filteredClaims, listItemLimit, offset) }
+                        localClaim={ ListType.EXTERNAL }
+                        openEdit={ (claimID: string) => {
+                            setEditClaim(true);
+                            setEditClaimID(claimID);
+                        } }
+                        update={ getExternalClaims }
+                        dialectID={ dialectID }
                     />
-                </Forms>
-            </Modal.Content>
-            <Modal.Actions>
-                <LinkButton
-                    onClick={ onClose }
-                >
-                    Cancel
-                </LinkButton>
-                <PrimaryButton
-                    onClick={ () => {
-                        setSubmit();
-                    } }
-                >
-                    Add
-                </PrimaryButton>
-            </Modal.Actions>
-        </Modal>
-    )
+                </ListLayout>
+            )
+            : !isLoading && (
+                <EmptyPlaceholder
+                    action={
+                        <PrimaryButton
+                            onClick={ () => {
+                                setAddClaim(true);
+                            } }
+                        >
+                            <Icon name="add" /> Add External Claim
+                                </PrimaryButton>
+                    }
+                    title="Add External Claim"
+                    subtitle={ [ "Currently, there is no External Claim available for this dialect." ] }
+                    image={ EmptyPlaceholderIllustrations.emptyList }
+                    imageSize="tiny"
+                />
+            )
+    );
 };
