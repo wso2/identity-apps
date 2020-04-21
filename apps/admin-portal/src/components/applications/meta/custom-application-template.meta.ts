5c0ef2769e0bd8d08e10fd79f2ae4c7d904735cd
NipuniBhagya <nipunib@wso2.com>
2020-04-21 15:05:22 +0530

    Add custom app icon to dark theme

--- apps/admin-portal/src/components/applications/meta/custom-application-template.meta.ts
+++ apps/admin-portal/src/components/applications/meta/custom-application-template.meta.ts
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import { ApplicationTemplateListItemInterface } from "../../../models";
 
 export const CustomApplicationTemplate: ApplicationTemplateListItemInterface = {
