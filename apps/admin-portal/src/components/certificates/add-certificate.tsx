2830af1e41a4e828cc69d63cfa7ef7ca1c551663
Thivi <theviyanthan@wso2.com>
2020-04-23 03:54:17 +0530

    Encode cer to pem and upload certificate.

--- apps/admin-portal/src/components/certificates/add-certificate.tsx
+++ apps/admin-portal/src/components/certificates/add-certificate.tsx
@@ -16,59 +16,110 @@
 * under the License.
 */
 
-import React, { ReactElement, useEffect, useRef, useState } from "react";
-import { Form, Icon, Label, Modal, Segment } from "semantic-ui-react";
+import { AlertLevels } from "@wso2is/core/dist/src/models";
+import { addAlert } from "@wso2is/core/dist/src/store";
 import { LinkButton, PrimaryButton } from "@wso2is/react-components";
+import * as forge from "node-forge";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Card, Divider, Form, Grid, Icon, Image, Message, Modal, Segment, TextArea } from "semantic-ui-react";
+import { createKeystoreCertificate } from "../../api";
 import { CertificateImage, FileUploadPlaceholder } from "../../configs";
+import { Certificate } from "../../models";
 
 interface AddCertificatePropsInterface {
+    /**
+     * Specifies if the modal should be opened or not.
+     */
     open: boolean;
+    /**
+     * Called when the modal is closed.
+     */
     onClose: () => void;
+    /**
+     * Calls update on the parent component.
+     */
+    update: () => void;
 }
 export const AddCertificate = (props: AddCertificatePropsInterface): ReactElement => {
 
-    const { open, onClose } = props;
+    const { open, onClose, update } = props;
+
     const [ name, setName ] = useState("");
     const [ file, setFile ] = useState<File>(null);
+    const [ pem, setPem ] = useState("");
     const [ nameError, setNameError ] = useState(false);
     const [ fileError, setFileError ] = useState(false);
+    const [ certEmpty, setCertEmpty ] = useState(false);
+    const [ fileDecoded, setFileDecoded ] = useState("");
 
     const fileUpload = useRef(null);
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (name) {
             setNameError(false);
         }
     }, [ name ]);
-    
+
     useEffect(() => {
         if (file) {
             setFileError(false);
         }
     }, [ file ]);
 
+    /**
+     * This takes in an ArrayBuffer and converts it to PEM.
+     * 
+     * @param {ArrayBuffer} value .cer `File` converted to `ArrayBuffer`.
+     * 
+     * @returns {Promise<string>} The PEM encoded string.
+     */
+    const convertFromDerToPem = (file: File): Promise<string> => {
+        return file.arrayBuffer().then((value: ArrayBuffer) => {
+            return forge.pki.certificateToPem(
+                forge.pki.certificateFromAsn1(
+                    forge.asn1.fromDer(
+                        forge.util.createBuffer(value)
+                    )
+                )
+            ).replace("-----BEGIN CERTIFICATE-----", "")
+                .replace("-----END CERTIFICATE-----", "");
+        }).catch(() => {
+            setFileError(true);
+            return "";
+        });
+    };
+
     return (
         <Modal
             open={ open }
             onClose={ onClose }
             dimmer="blurring"
-            size="tiny"
+            size="small"
         >
             <Modal.Header>
-                Upload certificate
+                Import certificate
             </Modal.Header>
-            <Modal.Content>
+            <Modal.Content scrolling>
                 <input
                     ref={ fileUpload }
                     type="file"
                     accept="application/x-x509-ca-cert"
                     hidden
                     onChange={ (event) => {
-                        setFile(event.target.files[ 0 ])
+                        const file = event.target.files[ 0 ];
+                        setFile(file);
                         event.target.value = null;
+                        setCertEmpty(false);
+                        setFileError(false);
+                        convertFromDerToPem(file).then((value: string) => {
+                            setFileDecoded(value);
+                        })
                     } }
                 />
-                <Form.Field>
+                <Form>
                     <Form.Input
                         fluid
                         type="text"
@@ -87,52 +138,101 @@ export const AddCertificate = (props: AddCertificatePropsInterface): ReactElemen
                             setName(event.target.value);
                         } }
                     />
-                    <Segment placeholder>
-                        {
-                            file ? (
-                                <Label basic image size="big">
-                                    <img src={ CertificateImage.default }/>
-                                    { file.name }
-                                    <Icon name="delete" onClick={ () => {
-                                        setFile(null);
-                                    } }/>
-                                </Label>
+                </Form>
+                <Segment placeholder>
+                    {
+                        !file
+                            ? (
+                                <Grid columns={ 2 } stackable textAlign='center'>
+                                    <Divider vertical>Or</Divider>
+
+                                    <Grid.Row verticalAlign='middle'>
+                                        <Grid.Column>
+                                            <div style={ { textAlign: "center" } }>
+                                                <FileUploadPlaceholder.ReactComponent />
+                                            </div>
+                                            <LinkButton onClick={ () => {
+                                                fileUpload.current.click();
+                                            } }>
+                                                Add Certificate
+                                            </LinkButton>
+                                        </Grid.Column>
+
+                                        <Grid.Column>
+                                            <Form>
+                                                <TextArea
+                                                    rows={ 10 }
+                                                    placeholder="Paste the content of a PEM certificate"
+                                                    value={ pem }
+                                                    onChange={ (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                                                        setPem(event.target.value);
+                                                        setCertEmpty(false);
+                                                    } }
+                                                />
+                                            </Form>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
                             )
-                                : (
-                                    <>
-                                        <div style={ { textAlign: "center" } }>
-                                            <FileUploadPlaceholder.ReactComponent />
-                                        </div>
-                                        <LinkButton onClick={ () => {
-                                            fileUpload.current.click();
-                                        } }>
-                                            Add Certificate
-                                </LinkButton>
-                                    </>
-                                )
+                            : (
+                                <Segment textAlign="center" basic>
+                                    <CertificateImage.ReactComponent />
+                                    <p>{ file.name }</p>
+                                    <Icon name="trash alternate" link onClick={ () => {
+                                        setFile(null);
+                                        setFileError(false);
+                                        setFileDecoded("");
+                                    } } />
+                                </Segment>
+                            )
+                    }
+                </Segment>
+                {
+                    (fileError || certEmpty) &&
+                    <Message error attached="bottom">
+                        { fileError
+                            ? "An error occurred while decoding the certificate." +
+                            " Please ensure the certificate file is valid."
+                            : "Either add a certificate file or paste the content of a PEM-encoded certificate."
                         }
-                    </Segment>
-                </Form.Field>
+                    </Message>
+                }
             </Modal.Content>
             <Modal.Actions>
                 <LinkButton onClick={ onClose }>
                     Cancel
                 </LinkButton>
                 <PrimaryButton onClick={ () => {
-                    if (!name) {
-                        setNameError(true);
-                        return;
-                    }
-                    if (!file) {
-                        setFileError(true);
+                    !name && setNameError(true);
+                    (!file && !pem) && setCertEmpty(true);
+                    if (!name || (!file && !pem) || fileError) {
                         return;
                     }
 
-                    const reader = new FileReader();
-                    reader.readAsText(file,"der");
+                    const data: Certificate = {
+                        alias: name,
+                        certificate: fileDecoded || pem
+                    };
+
+                    createKeystoreCertificate(data).then(() => {
+                        dispatch(addAlert({
+                            description: "The certificate has been imported successfully.",
+                            level: AlertLevels.SUCCESS,
+                            message: "Certificate import success"
+                        }));
+                        update();
+                        onClose();
+                    }).catch(error => {
+                        dispatch(addAlert({
+                            description: error?.description || "An error occurred while importing the certificate.",
+                            level: AlertLevels.ERROR,
+                            message: error?.message || "Something went wrong!"
+                        }));
+                    })
+
 
                 } }>
-                    Upload
+                    Import
                 </PrimaryButton>
             </Modal.Actions>
         </Modal>
