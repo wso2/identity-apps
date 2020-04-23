fec80bf96486a2c7238dfae63f3341aa01afd112
kanchanagodage <kanchanagodage@gmail.com>
2020-04-23 17:49:25 +0530

    Fix build failure

--- modules/theme/src/index.js
+++ modules/theme/src/index.js
@@ -99,8 +99,8 @@ export const AuthenticationCapabilityIcon =
     require("../dist/lib/themes/default/assets/images/identity-provider-capabilities/authentication.svg");
 export const ProvisionCapabilityIcon = 
     require("../dist/lib/themes/default/assets/images/identity-provider-capabilities/provision.svg");
-export const FacebookIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/facebook.svg");
-export const GithubIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/github.svg");
+export const FacebookIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/facebook-idp-illustration.svg");
+export const GithubIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/github-idp-illustration.svg");
 export const AssociatedAccountsMiniIllustration = 
     require("../dist/lib/themes/default/assets/images/illustrations/associated-accounts-mini.svg");
 export const AssociatedAccountsIllustration = 
@@ -174,8 +174,8 @@ export const GoogleLogo = require("../dist/lib/themes/default/assets/images/soci
 export const TwitterLogo = require("../dist/lib/themes/default/assets/images/social/twitter.svg");
 
 // Identity providers
-export const GoogleIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/google.svg");
-export const TwitterIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/twitter.svg");
+export const GoogleIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/google-idp-illustration.svg");
+export const TwitterIdPIcon = require("../dist/lib/themes/default/assets/images/identity-providers/twitter-idp-illustration.svg");
 
 /* eslint-enable import/order */
 
