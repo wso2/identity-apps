a3b3d6bcfb7ceba68f14e9859c78a168e96acc59
Thivi <theviyanthan@wso2.com>
2020-04-30 21:29:19 +0530

    Add code editor component

--- apps/admin-portal/src/components/user-stores/sql-editor.tsx
+++ apps/admin-portal/src/components/user-stores/sql-editor.tsx
@@ -16,10 +16,262 @@
 * under the License.
 */
 
-import React, { FunctionComponent, ReactElement } from "react";
+import React, { FunctionComponent, ReactElement, useState, useRef, useEffect } from "react";
+import { RequiredBinary } from "../../models";
+import { CodeEditor, Heading } from "@wso2is/react-components";
+import { Sidebar, Segment, Accordion, Menu, Icon, Popup, Checkbox } from "semantic-ui-react";
 
-export const SqlEditor: FunctionComponent<{}> = (): ReactElement => {
-    return(
-        <div></div>
+interface SqlEditorPropsInterface {
+    onChange: (name: string, value: string) => void;
+    properties: RequiredBinary[ "optional" ][ "sql" ];
+}
+
+export const SqlEditor: FunctionComponent<SqlEditorPropsInterface> = (
+    props: SqlEditorPropsInterface
+): ReactElement => {
+
+    const { onChange, properties } = props;
+
+    const [ sideBarVisible, setSideBarVisible ] = useState(true);
+    const [ accordionIndex, setAccordionIndex ] = useState(-1);
+    const [ propertyName, setPropertyName ] = useState("");
+    const [ propertyValue, setPropertyValue ] = useState("");
+    const [ isEditorDarkMode, setIsEditorDarkMode ] = useState(true);
+
+    const sidebar = useRef(null);
+    const editor = useRef(null);
+
+    /**
+     * Triggered on `sideBarVisible` change.
+     */
+    useEffect(() => {
+        let width = "100%";
+
+        if (sideBarVisible) {
+            width = `calc(100% - ${sidebar?.current?.ref?.current?.clientWidth}px)`;
+        }
+
+        editor.current.style.width = width;
+    }, [ sideBarVisible ]);
+
+    const EditorSideBar = (): ReactElement => (
+        <Sidebar
+            as={ Segment }
+            className="script-templates-panel"
+            animation="overlay"
+            icon="labeled"
+            direction="right"
+            vertical
+            secondary
+            visible={ sideBarVisible }
+            ref={ sidebar }
+        >
+            <Heading as="h6" bold>SQL Query Types</Heading>
+            {
+                (properties)
+                && (
+                    <Accordion
+                        as={ Menu }
+                        className="template-category-menu"
+                        fluid
+                        secondary
+                        vertical
+                    >
+                        <Menu.Item>
+                            <Accordion.Title
+                                active={ accordionIndex === 0 }
+                                className="category-name"
+                                content="Create"
+                                index={ accordionIndex }
+                                icon={ <Icon className="angle right caret-icon" /> }
+                                onClick={ () => setAccordionIndex(accordionIndex === 0 ? -1 : 0) }
+                            />
+                            <Accordion.Content
+                                className="template-list"
+                                active={ accordionIndex === 0 }
+                            >
+                                {
+                                    properties.insert.map((property, index) => (
+                                        <Menu.Item
+                                            key={ index }
+                                            onClick={ () => {
+                                                setPropertyName(property.name);
+                                                setPropertyValue(property.value ?? property.defaultValue);
+                                            } }
+                                        >
+                                            <Popup
+                                                trigger={ (
+                                                    <div className="template-name">
+                                                        { property.name }
+                                                    </div>
+                                                ) }
+                                                position="top center"
+                                                content={ property.name }
+                                                inverted
+                                            />
+                                        </Menu.Item>
+                                    ))
+                                }
+                            </Accordion.Content>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Accordion.Title
+                                active={ accordionIndex === 1 }
+                                className="category-name"
+                                content="Read"
+                                index={ accordionIndex }
+                                icon={ <Icon className="angle right caret-icon" /> }
+                                onClick={ () => setAccordionIndex(accordionIndex === 1 ? -1 : 1) }
+                            />
+                            <Accordion.Content
+                                className="template-list"
+                                active={ accordionIndex === 1 }
+                            >
+                                {
+                                    properties.select.map((property, index) => (
+                                        <Menu.Item
+                                            key={ index }
+                                            onClick={ () => {
+                                                setPropertyName(property.name);
+                                                setPropertyValue(property.value ?? property.defaultValue);
+                                            } }
+                                        >
+                                            <Popup
+                                                trigger={ (
+                                                    <div className="template-name">
+                                                        { property.name }
+                                                    </div>
+                                                ) }
+                                                position="top center"
+                                                content={ property.name }
+                                                inverted
+                                            />
+                                        </Menu.Item>
+                                    ))
+                                }
+                            </Accordion.Content>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Accordion.Title
+                                active={ accordionIndex === 2 }
+                                className="category-name"
+                                content="Update"
+                                index={ accordionIndex }
+                                icon={ <Icon className="angle right caret-icon" /> }
+                                onClick={ () => setAccordionIndex(accordionIndex === 2 ? -1 : 2) }
+                            />
+                            <Accordion.Content
+                                className="template-list"
+                                active={ accordionIndex === 2 }
+                            >
+                                {
+                                    properties.update.map((property, index) => (
+                                        <Menu.Item
+                                            key={ index }
+                                            onClick={ () => {
+                                                setPropertyName(property.name);
+                                                setPropertyValue(property.value ?? property.defaultValue);
+                                            } }
+                                        >
+                                            <Popup
+                                                trigger={ (
+                                                    <div className="template-name">
+                                                        { property.name }
+                                                    </div>
+                                                ) }
+                                                position="top center"
+                                                content={ property.name }
+                                                inverted
+                                            />
+                                        </Menu.Item>
+                                    ))
+                                }
+                            </Accordion.Content>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Accordion.Title
+                                active={ accordionIndex === 3 }
+                                className="category-name"
+                                content="Delete"
+                                index={ accordionIndex }
+                                icon={ <Icon className="angle right caret-icon" /> }
+                                onClick={ () => setAccordionIndex(accordionIndex === 3 ? -1 : 3) }
+                            />
+                            <Accordion.Content
+                                className="template-list"
+                                active={ accordionIndex === 3 }
+                            >
+                                {
+                                    properties.delete.map((property, index) => (
+                                        <Menu.Item
+                                            key={ index }
+                                            onClick={ () => {
+                                                setPropertyName(property.name);
+                                                setPropertyValue(property.value ?? property.defaultValue);
+                                            } }
+                                        >
+                                            <Popup
+                                                trigger={ (
+                                                    <div className="template-name">
+                                                        { property.name }
+                                                    </div>
+                                                ) }
+                                                position="top center"
+                                                content={ property.name }
+                                                inverted
+                                            />
+                                        </Menu.Item>
+                                    ))
+                                }
+                            </Accordion.Content>
+                        </Menu.Item>
+                    </Accordion>
+                )
+            }
+        </Sidebar>
+    );
+
+    return (
+        <div className="adaptive-scripts-section">
+            <Sidebar.Pushable className="script-editor-section" >
+                <EditorSideBar />
+                <Sidebar.Pusher>
+                    <div className="script-editor-container" ref={ editor }>
+                        <Menu attached="top" className="action-panel" secondary>
+                            <Menu.Item>
+                                <Checkbox
+                                    label="Dark mode"
+                                    checked={ isEditorDarkMode }
+                                    onChange={ () => { setIsEditorDarkMode(!isEditorDarkMode) } }
+                                    slider
+                                />
+                            </Menu.Item>
+                            <Menu.Menu position="right">
+                                <Menu.Item onClick={ () => setSideBarVisible(!sideBarVisible) } className="action">
+                                    <Icon name="bars" />
+                                </Menu.Item>
+                            </Menu.Menu>
+                        </Menu>
+
+                        <div
+                            className="code-editor-wrapper"
+                        >
+                            <CodeEditor
+                                lint
+                                language="javascript"
+                                sourceCode={ propertyValue }
+                                options={ {
+                                    lineWrapping: true
+                                } }
+                                onChange={ (editor, data, value) => {
+                                    onChange(propertyValue, value);
+                                } }
+                                theme={ isEditorDarkMode ? "dark" : "light" }
+                            />
+                        </div>
+                    </div>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable >
+        </div>
     )
 }
