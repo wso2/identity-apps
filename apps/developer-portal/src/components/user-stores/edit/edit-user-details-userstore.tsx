5920c1e0a650b86bc12d5645f6263cc16347e141
Thivi <theviyanthan@wso2.com>
2020-05-07 13:45:24 +0530

    Localize edit user details-userstore

--- apps/developer-portal/src/components/user-stores/edit/edit-user-details-userstore.tsx
+++ apps/developer-portal/src/components/user-stores/edit/edit-user-details-userstore.tsx
@@ -20,6 +20,7 @@ import { addAlert } from "@wso2is/core/store";
 import { Field, FormValue, Forms } from "@wso2is/forms";
 import { LinkButton, PrimaryButton } from "@wso2is/react-components";
 import React, { ReactElement, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { Grid, Icon } from "semantic-ui-react";
 import { SqlEditor } from "..";
@@ -58,6 +59,8 @@ export const EditUserDetails = (
     const [ sql, setSql ] = useState<Map<string, string>>(null);
 
     const dispatch = useDispatch();
+
+    const { t } = useTranslation();
 
     useEffect(() => {
         if (properties) {
@@ -155,17 +158,21 @@ export const EditUserDetails = (
 
         patchUserStore(id, data).then(() => {
             dispatch(addAlert({
-                description: "This userstore has been updated successfully!",
+                description: t("devPortal:components.userstores.notifications." +
+                    "updateUserstore.success.description"),
                 level: AlertLevels.SUCCESS,
-                message: "Userstore updated successfully!"
+                message: t("devPortal:components.userstores.notifications." +
+                    "updateUserstore.success.message")
             }));
             update();
         }).catch(error => {
             dispatch(addAlert({
                 description: error?.description
-                    || "An error occurred while updating the userstore.",
+                    || t("devPortal:components.userstores.notifications." +
+                        "updateUserstore.genericError.description"),
                 level: AlertLevels.ERROR,
-                message: error?.message || "Something went wrong"
+                message: error?.message || t("devPortal:components.userstores.notifications." +
+                    "updateUserstore.genericError.message")
             }));
         });
     };
@@ -195,10 +202,14 @@ export const EditUserDetails = (
                                                 required={ true }
                                                 label={ name }
                                                 requiredErrorMessage={
-                                                    `${property.description.split("#")[ 0 ]} is  required`
+                                                    t("devPortal:components.userstores.forms.edit.connection." +
+                                                        "custom.requiredErrorMessage",
+                                                        {
+                                                            name: property.description.split("#")[ 0 ]
+                                                        })
                                                 }
-                                                showPassword="Show Password"
-                                                hidePassword="Hide Password"
+                                                showPassword={ t("common:showPassword") }
+                                                hidePassword={ t("common:hidePassword") }
                                             />
                                         )
                                         : toggle
@@ -211,7 +222,11 @@ export const EditUserDetails = (
                                                     required={ false }
                                                     label={ property.description.split("#")[ 0 ] }
                                                     requiredErrorMessage={
-                                                        `${property.description.split("#")[ 0 ]} is  required`
+                                                        t("devPortal:components.userstores.forms.edit.connection." +
+                                                            "custom.requiredErrorMessage",
+                                                            {
+                                                                name: property.description.split("#")[ 0 ]
+                                                            })
                                                     }
                                                     toggle
                                                 />
@@ -225,7 +240,11 @@ export const EditUserDetails = (
                                                     required={ true }
                                                     label={ property.description.split("#")[ 0 ] }
                                                     requiredErrorMessage={
-                                                        `${property.description.split("#")[ 0 ]} is  required`
+                                                        t("devPortal:components.userstores.forms.edit.connection." +
+                                                            "custom.requiredErrorMessage",
+                                                            {
+                                                                name: property.description.split("#")[ 0 ]
+                                                            })
                                                     }
                                                 />
                                             )
@@ -249,7 +268,7 @@ export const EditUserDetails = (
                                 onClick={ () => { setShowMore(!showMore) } }
                             >
                                 <Icon name={ showMore ? "chevron up" : "chevron down" } />
-                                { `Show ${showMore ? "Less" : "More"}` }
+                                { showMore ? t("common:showLess") : t("common:showMore") }
                             </LinkButton>
                         </Grid.Column>
                     </Grid>
@@ -278,10 +297,14 @@ export const EditUserDetails = (
                                                         required={ false }
                                                         label={ name }
                                                         requiredErrorMessage={
-                                                            `${property.description.split("#")[ 0 ]} is  required`
+                                                            t("devPortal:components.userstores.forms.edit.connection." +
+                                                                "custom.requiredErrorMessage",
+                                                                {
+                                                                    name: property.description.split("#")[ 0 ]
+                                                                })
                                                         }
-                                                        showPassword="Show Password"
-                                                        hidePassword="Hide Password"
+                                                        showPassword={ t("common:showPassword") }
+                                                        hidePassword={ t("common:hidePassword") }
                                                     />
                                                 )
                                                 : toggle
@@ -294,7 +317,11 @@ export const EditUserDetails = (
                                                             required={ false }
                                                             label={ property.description.split("#")[ 0 ] }
                                                             requiredErrorMessage={
-                                                                `${property.description.split("#")[ 0 ]} is  required`
+                                                                t("devPortal:components.userstores.forms.edit." +
+                                                                    "connection.custom.requiredErrorMessage",
+                                                                    {
+                                                                        name: property.description.split("#")[ 0 ]
+                                                                    })
                                                             }
                                                             toggle
                                                         />
@@ -308,7 +335,11 @@ export const EditUserDetails = (
                                                             required={ false }
                                                             label={ property.description.split("#")[ 0 ] }
                                                             requiredErrorMessage={
-                                                                `${property.description.split("#")[ 0 ]} is  required`
+                                                                t("devPortal:components.userstores.forms.edit." +
+                                                                    "connection.custom.requiredErrorMessage",
+                                                                    {
+                                                                        name: property.description.split("#")[ 0 ]
+                                                                    })
                                                             }
                                                         />
                                                     )
