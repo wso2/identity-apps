b29f9e8bdc8c784b448c6bb96387353549a8df9e
Brion Mario <info@brionmario.com>
2020-06-09 04:11:17 +0530

    Group components in a separate folder

---
+++ modules/react-components/src/components/index.ts
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+export * from "./accordion";
+export * from "./alert";
+export * from "./analytics";
+export * from "./avatar";
+export * from "./brand";
+export * from "./button";
+export * from "./card";
+export * from "./code-editor";
+export * from "./danger-zone";
+export * from "./error";
+export * from "./footer";
+export * from "./grid";
+export * from "./header";
+export * from "./help-panel";
+export * from "./icon";
+export * from "./input";
+export * from "./jumbotron";
+export * from "./language-switcher";
+export * from "./list";
+export * from "./loader";
+export * from "./modal";
+export * from "./page-header";
+export * from "./pagination";
+export * from "./placeholder";
+export * from "./renderer";
+export * from "./route";
+export * from "./section";
+export * from "./side-panel";
+export * from "./step";
+export * from "./tab";
+export * from "./typography";
+export * from "./theme";
+export * from "./tree-view";
+export * from "./transfer";
