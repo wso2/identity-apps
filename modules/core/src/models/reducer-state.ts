23badcb7faf3f34625a59a4f164c257503174593
Thivi <theviyanthan@outlook.com>
2020-06-22 18:44:03 +0530

    Add scope attribute to reducer interface.

--- modules/core/src/models/reducer-state.ts
+++ modules/core/src/models/reducer-state.ts
@@ -46,6 +46,10 @@ export interface CommonAuthReducerStateInterface {
      * Username decoded from the ID token.
      */
     username: string;
+    /**
+     * Scopes that a user is allowed.
+     */
+    scope: string;
 }
 
 /**
