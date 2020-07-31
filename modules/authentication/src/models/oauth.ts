6cc4140d8f0796132fb1e6a9c5103d3b3930dc4d
Thivi <theviyanthan@outlook.com>
2020-07-31 18:03:06 +0530

    Add SessionData type

--- modules/authentication/src/models/oauth.ts
+++ modules/authentication/src/models/oauth.ts
@@ -21,14 +21,14 @@ import { ConfigInterface } from ".";
 import { SignInResponse, UserInfo } from "./message";
 
 export interface OAuthInterface {
-	httpRequest<T = any>(config: AxiosRequestConfig): Promise<AxiosResponse<T>>;
-	httpRequestAll<T = any>(configs: AxiosRequestConfig[]): Promise<AxiosResponse<T>[]>;
-	signOut(): Promise<boolean>;
-	signIn(): Promise<UserInfo>;
-	initialize(config: ConfigInterface): Promise<boolean>;
-	listenForAuthCode(): Promise<UserInfo>;
-	customGrant(requestParams: CustomGrantRequestParams): Promise<AxiosResponse | boolean | SignInResponse>;
-	revokeToken(): Promise<boolean>;
+    httpRequest<T = any>(config: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+    httpRequestAll<T = any>(configs: AxiosRequestConfig[]): Promise<AxiosResponse<T>[]>;
+    signOut(): Promise<boolean>;
+    signIn(): Promise<UserInfo>;
+    initialize(config: ConfigInterface): Promise<boolean>;
+    listenForAuthCode(): Promise<UserInfo>;
+    customGrant(requestParams: CustomGrantRequestParams): Promise<AxiosResponse | boolean | SignInResponse>;
+    revokeToken(): Promise<boolean>;
 }
 
 export interface OAuthSingletonInterface {
@@ -42,3 +42,5 @@ export interface CustomGrantRequestParams {
     returnsSession: boolean;
     returnResponse: boolean;
 }
+
+export type SessionData = Map<string, string>;
