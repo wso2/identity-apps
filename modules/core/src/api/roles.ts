f2c5442c45971b9653d131644531dff42b8d6432
Thivi <theviyanthan@outlook.com>
2020-08-06 01:21:29 +0530

    Use Authenticate class to make api calls in roles api

--- modules/core/src/api/roles.ts
+++ modules/core/src/api/roles.ts
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import { OAuth } from "@wso2is/oauth-web-worker";
+import { Authenticate, STORAGE } from "@wso2is/authentication";
 import { AxiosError, AxiosResponse } from "axios";
 import { CommonServiceResourcesEndpoints } from "../configs";
 import { RoleConstants } from "../constants";
@@ -29,7 +29,7 @@ import { ContextUtils } from "../utils";
  * Initialize an axios Http client.
  *
  */
-const httpClient = OAuth.getInstance().httpRequest;
+const httpClient = new Authenticate(STORAGE.webWorker).httpRequest;
 
 /**
  * Retrieve the list of groups that are currently in the system.
