cb5216e7ba1c4fec44d2862c0cf681aab37b8daa
Thivi <theviyanthan@outlook.com>
2020-08-06 01:38:06 +0530

    Use Authenticate class to make api calls in federated associations api

--- apps/user-portal/src/api/federated-associations.ts
+++ apps/user-portal/src/api/federated-associations.ts
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import { OAuth } from "@wso2is/oauth-web-worker";
+import { Authenticate, STORAGE } from "@wso2is/authentication";
 import { HttpMethods } from "../models";
 import { store } from "../store";
 
@@ -25,7 +25,7 @@ import { store } from "../store";
  *
  * @type {AxiosHttpClientInstance}
  */
-const httpClient = OAuth.getInstance().httpRequest;
+const httpClient = new Authenticate(STORAGE.webWorker).httpRequest;
 
 /**
  * This function calls the federated association API endpoint and gets the list of federated associations
