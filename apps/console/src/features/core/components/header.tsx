b052ebe093b0a7a1781dbb14eee24c1d13285b83
Brion Mario <info@brionmario.com>
2020-08-10 14:55:17 +0530

    Take `versionUISettings` from config

--- apps/console/src/features/core/components/header.tsx
+++ apps/console/src/features/core/components/header.tsx
@@ -152,7 +152,12 @@ export const Header: FunctionComponent<HeaderPropsInterface> = (
                             ? state.productName
                             : config.ui.productName
                     }
-                    version={ config.deployment.productVersion }
+                    version={ config.ui.productVersionConfig?.override ?? config.deployment.productVersion }
+                    versionUISettings={ {
+                        allowSnapshot: config.ui.productVersionConfig?.allowSnapshot,
+                        labelColor: config.ui.productVersionConfig?.labelColor,
+                        textCase: config.ui.productVersionConfig?.textCase
+                    } }
                 />
             ) }
             brandLink={ config.deployment.appHomePath }
