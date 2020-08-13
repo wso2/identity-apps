cf38a656a493e0693ca9a1daf24f797480fa50af
Thivi <theviyanthan@outlook.com>
2020-08-13 21:48:34 +0530

    Update governance connector form view

--- apps/console/src/features/server-configurations/components/governance-connectors/dynamic-connector-form.tsx
+++ apps/console/src/features/server-configurations/components/governance-connectors/dynamic-connector-form.tsx
@@ -17,16 +17,21 @@
  */
 
 import { I18n } from "@wso2is/i18n";
-import { Hint, RenderInput, RenderToggle } from "@wso2is/react-components";
-import React from "react"
-import { Field, reduxForm } from "redux-form"
+import {
+    Hint,
+    PrimaryButton,
+    RenderInput,
+    RenderToggle
+} from "@wso2is/react-components";
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 import { Grid } from "semantic-ui-react";
 import { ConnectorPropertyInterface } from "../../models";
 import { GovernanceConnectorUtils } from "../../utils";
 
 /**
  * Determine the matching Form component based on the property attributes.
- * 
+ *
  * @param property
  */
 const getFieldComponent = (property: ConnectorPropertyInterface) => {
@@ -35,6 +40,15 @@ const getFieldComponent = (property: ConnectorPropertyInterface) => {
     } else {
         return RenderInput;
     }
+};
+
+/**
+ * Returns if teh connector property is a toggle or not.
+ *
+ * @param {ConnectorPropertyInterface} property Connector property.
+ */
+const isToggle = (property: ConnectorPropertyInterface) => {
+    return property.value === "true" || property.value === "false";
 };
 
 const getFieldType = (property: ConnectorPropertyInterface) => {
@@ -52,61 +66,64 @@ const getFieldType = (property: ConnectorPropertyInterface) => {
  * @constructor
  */
 const DynamicConnectorForm = (props) => {
-    const {
-        handleSubmit,
-        [ "data-testid" ]: testId
-    } = props;
+    const { handleSubmit, [ "data-testid" ]: testId } = props;
     const properties: ConnectorPropertyInterface[] = props.props.properties;
 
     return (
         <form onSubmit={ handleSubmit }>
             <Grid padded={ true }>
-            {
-                properties?.map((property, index) => {
+                { properties?.map((property, index) => {
                     return (
-                        <Grid.Row columns={ 1 } className="pl-3" key={ index }>
-                            <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 14 }>
-                                {
-                                    (getFieldComponent(property) === RenderInput) &&
+                        <Grid.Row columns={ 2 } className="pl-3" key={ index }>
+                            <Grid.Column mobile={ 12 } tablet={ 12 } computer={ 10 }>
+                                { getFieldComponent(property) === RenderInput && (
                                     <label htmlFor={ property.name }>{ property.displayName }</label>
-                                }
-                                <Field
-                                    name={ GovernanceConnectorUtils.encodeConnectorPropertyName(property.name) }
-                                    component={ getFieldComponent(property) }
-                                    type={ getFieldType(property) }
-                                    required={ true }
-                                    width={ 10 }
-                                    placeholder={ property.value }
-                                    data-testid={ `${ testId }-${ property.name }` }
-                                    label={ property.displayName }
-                                />
-                                {
-                                    (property.description !== "" &&
-                                        <Hint>
-                                            { property.description }
-                                        </Hint>
-                                    )
-                                }
+                                ) }
+                                { !isToggle(property) ? (
+                                    <Field
+                                        name={ GovernanceConnectorUtils.encodeConnectorPropertyName(property.name) }
+                                        component={ getFieldComponent(property) }
+                                        type={ getFieldType(property) }
+                                        required={ true }
+                                        width={ 14 }
+                                        placeholder={ property.value }
+                                        data-testid={ `${ testId }-${ property.name }` }
+                                    />
+                                ) : (
+                                        <label>{ property.displayName }</label>
+                                    ) }
+                                { property.description !== "" && <Hint>{ property.description }</Hint> }
+                            </Grid.Column>
+                            <Grid.Column mobile={ 4 } tablet={ 4 } computer={ 6 }>
+                                { isToggle(property) && (
+                                    <Field
+                                        name={ GovernanceConnectorUtils.encodeConnectorPropertyName(property.name) }
+                                        component={ RenderToggle }
+                                        type={ getFieldType(property) }
+                                        required={ true }
+                                        width={ 10 }
+                                        placeholder={ property.value }
+                                        data-testid={ `${ testId }-${ property.name }` }
+                                        label={ property.value === "true" ? "Enabled" : "Disabled" }
+                                    />
+                                ) }
                             </Grid.Column>
                         </Grid.Row>
-                    )
-                })
-            }
+                    );
+                }) }
                 <Grid.Row columns={ 1 } className="pl-3">
                     <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 14 }>
-                        <button type="submit" >
-                            { I18n.instance.t("common:update").toString() }
-                        </button>
+                        <PrimaryButton type="submit">{ I18n.instance.t("common:update").toString() }</PrimaryButton>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
         </form>
-    )
+    );
 };
 
 export default reduxForm({
     enableReinitialize: true
-})(DynamicConnectorForm)
+})(DynamicConnectorForm);
 
 /**
  * Default props for the component.
