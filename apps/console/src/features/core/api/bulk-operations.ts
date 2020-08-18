9cc8a64458098a1f9dc911a3ee5f98eea9127ad8
NipuniBhagya <nipunib@wso2.com>
2020-08-18 13:41:10 +0530

    Add new API method for bulk operations

---
+++ apps/console/src/features/core/api/bulk-operations.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { HttpMethods } from "@wso2is/core/models";
+import { OAuth } from "@wso2is/oauth-web-worker";
+import { store } from "../store";
+
+/**
+ * Initialize an axios Http client.
+ *
+ */
+const httpClient = OAuth.getInstance().httpRequest;
+
+/**
+ * Update bulks of resources
+ *
+ * @param data request payload
+ * @returns {Promise<any>} a promise containing the response.
+ */
+export const updateResources = (data: object): Promise<any> => {
+    const requestConfig = {
+        data,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: HttpMethods.POST,
+        url: store.getState().config.endpoints.bulk
+    };
+
+    return httpClient(requestConfig)
+        .then((response) => {
+            return Promise.resolve(response);
+        })
+        .catch((error) => {
+            return Promise.reject(error);
+        });
+};
