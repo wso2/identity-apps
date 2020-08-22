c43f077e0a93152297f3e14b3b44dd5ad18c97a8
Thivi <theviyanthan@outlook.com>
2020-08-22 20:04:26 +0530

    Use less rewrite plugin from npm

--- modules/theme/src/theme.js
+++ modules/theme/src/theme.js
@@ -19,7 +19,7 @@
 
 const path = require("path");
 const less = require("less");
-const RewriteVariablePlugin = require("./lib");
+const RewriteVariablePlugin = require("less-plugin-rewrite-variable");
 
 export const Theme = {
     compile: (file, options) => {
