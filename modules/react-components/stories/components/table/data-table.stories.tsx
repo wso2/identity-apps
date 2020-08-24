579c27f0198bc6cce631d755ad8ba4192b493d5f
Brion Mario <info@brionmario.com>
2020-08-24 15:22:07 +0530

    Implement initial stories

--- modules/react-components/stories/components/table/data-table.stories.tsx
+++ modules/react-components/stories/components/table/data-table.stories.tsx
@@ -18,10 +18,10 @@
  */
 
 import { action } from "@storybook/addon-actions";
-import React, { ReactElement } from "react";
-import { meta } from "./data-table.stories.meta";
-import { AppAvatar, DataTable } from "../../../src";
-import { Header, Label } from "semantic-ui-react";
+import React, { ReactElement, SyntheticEvent } from "react";
+import { Divider, Form, Grid, Header, Label, SemanticICONS } from "semantic-ui-react";
+import { DEMO_DATA_LIST, DataTableDemoDataInterface, meta } from "./data-table.stories.meta";
+import { AdvancedSearch, AppAvatar, DataTable, LinkButton, PrimaryButton } from "../../../src";
 
 export default {
     parameters: {
@@ -32,87 +32,264 @@ export default {
 };
 
 /**
- * Story to display a default resource list.
+ * Story to display the basic usage of a Data Table.
  *
  * @return {React.ReactElement}
  */
-export const Default = (): ReactElement => (
+export const BasicUsage = (): ReactElement => (
     <DataTable
         padded
-        basic="very"
         actions={ [
             {
-                icon: "pencil alternate",
-                onClick: action("Clicked on edit."),
-                popupText: "edit",
+                icon: (): SemanticICONS => "pencil alternate",
+                onClick: (e: SyntheticEvent, { value: item }: { value: DataTableDemoDataInterface }): void => {
+                    action(`Clicked on ${ item.name }`)
+                },
+                popupText: (): string => "edit",
                 renderer: "semantic-icon"
             },
             {
-                icon: "trash alternate",
-                onClick: action("Clicked on delete."),
-                popupText: "delete",
-                renderer: "dropdown"
+                icon: (): SemanticICONS => "trash alternate",
+                onClick: (e: SyntheticEvent, { value: item }: { value: DataTableDemoDataInterface }): void => {
+                    action(`Clicked on delete of ${ item.name }`)
+                },
+                popupText: (): string => "delete",
+                renderer: "semantic-icon"
             }
         ] }
-        data={ [
-            {
-                clientId: <Label>jvwEqjYtRSeMtb4nGWXUKdMBN2UXpl3t</Label>,
-                name: (
-                    <Header as="h6" image>
-                        <AppAvatar
-                            name="Facebook"
-                            image="http://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19750.png"
-                            size="mini"
-                        />
-                        <Header.Content>
-                            Facebook
-                            <Header.Subheader>Facebook federated login example app.</Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                )
-            },
-            {
-                clientId: <Label>jvasdjYtdfdfRSeMtbasdUKdMBN2UXpl34</Label>,
-                name: (
-                    <Header as="h6" image>
-                        <AppAvatar
-                            name="Facebook"
-                            image="https://miro.medium.com/max/1300/1*pk4gPFD0OLeKbwKlN1v9YA.png"
-                            size="mini"
-                        />
-                        <Header.Content>
-                            GO SDK Demo
-                            <Header.Subheader>Demo of the GO lang SDK implementing the auth code
-                                flow.</Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                )
-            }
-        ] }
+        data={
+            DEMO_DATA_LIST.map((item: DataTableDemoDataInterface, index: number) => {
+                return {
+                    clientId: <Label>{ item.clientId }</Label>,
+                    id: item.clientId,
+                    key: index,
+                    name: (
+                        <Header as="h6" image>
+                            <AppAvatar
+                                name={ item.name }
+                                image={ item.imageUrl }
+                                size="mini"
+                            />
+                            <Header.Content>
+                                { item.name }
+                                <Header.Subheader>{ item.description }</Header.Subheader>
+                            </Header.Content>
+                        </Header>
+                    ),
+                    value: item
+                }
+            })
+        }
         columns={ [
             {
+                allowToggleVisibility: false,
                 dataIndex: "name",
+                id: "name",
                 key: 0,
                 title: "Name"
             },
             {
+                allowToggleVisibility: false,
                 dataIndex: "clientId",
+                id: "clientId",
                 key: 1,
                 title: "Client ID"
             },
             {
+                allowToggleVisibility: false,
                 dataIndex: "action",
+                id: "actions",
                 key: 2,
                 title: "Actions"
             }
         ] }
+        onRowClick={
+            (e: SyntheticEvent, { value: item }: { value: DataTableDemoDataInterface }): void => {
+                action(`Clicked on the row of ${ item.name }`)
+            }
+        }
     />
 );
 
-Default.story = {
+BasicUsage.story = {
     parameters: {
         docs: {
             storyDescription: meta.stories[ 0 ].description
+        }
+    }
+};
+
+/**
+ * Story to display the usage of a Data Table with an operations bar present.
+ *
+ * @return {React.ReactElement}
+ */
+export const WithOperationsBar = (): ReactElement => (
+    <DataTable
+        padded
+        showSearch
+        showOperationsHeader
+        showColumnSelector
+        showToggleDisallowedColumns
+        actions={ [
+            {
+                icon: (): SemanticICONS => "pencil alternate",
+                onClick: (e: SyntheticEvent, { value: item }: { value: DataTableDemoDataInterface }): void => {
+                    action(`Clicked on ${ item.name }`)
+                },
+                popupText: (): string => "edit",
+                renderer: "semantic-icon"
+            },
+            {
+                icon: (): SemanticICONS => "trash alternate",
+                onClick: (e: SyntheticEvent, { value: item }: { value: DataTableDemoDataInterface }): void => {
+                    action(`Clicked on delete of ${ item.name }`)
+                },
+                popupText: (): string => "delete",
+                renderer: "semantic-icon"
+            }
+        ] }
+        data={
+            DEMO_DATA_LIST.map((item: DataTableDemoDataInterface, index: number) => {
+                return {
+                    clientId: <Label>{ item.clientId }</Label>,
+                    id: item.clientId,
+                    key: index,
+                    name: (
+                        <Header as="h6" image>
+                            <AppAvatar
+                                name={ item.name }
+                                image={ item.imageUrl }
+                                size="mini"
+                            />
+                            <Header.Content>
+                                { item.name }
+                                <Header.Subheader>
+                                    { item.description }
+                                </Header.Subheader>
+                            </Header.Content>
+                        </Header>
+                    ),
+                    value: item
+                }
+            })
+        }
+        externalSearch={
+            <AdvancedSearch
+                aligned="left"
+                dropdownPosition="bottom left"
+                fill="white"
+                defaultSearchStrategy={ "name co" }
+                onSearchQuerySubmit={ (processQuery: boolean, query: string) =>
+                    action(`Processing search query ${ query }`)
+                }
+            >
+                <Grid>
+                    <Grid.Row columns={ 1 }>
+                        <Grid.Column width={ 16 }>
+                            <Form style={ { minWidth: "420px" } }>
+                                <Form.Select
+                                    fluid
+                                    label="Filter Attribute"
+                                    options={ [
+                                        {
+                                            key: "name",
+                                            text: "Name",
+                                            value: "name"
+                                        },
+                                        {
+                                            key: "description",
+                                            text: "Description",
+                                            value: "description"
+                                        },
+                                        {
+                                            key: "clientId",
+                                            text: "Client ID",
+                                            value: "clientId"
+                                        }
+                                    ] }
+                                    placeholder="E.g. Name, Description etc."
+                                />
+                                <Form.Group widths="equal">
+                                    <Form.Select
+                                        fluid
+                                        label="Filter Condition"
+                                        options={ [
+                                            {
+                                                key: "sw",
+                                                text: "Starts With",
+                                                value: "sw"
+                                            },
+                                            {
+                                                key: "ew",
+                                                text: "Ends With",
+                                                value: "ew"
+                                            },
+                                            {
+                                                key: "co",
+                                                text: "Contains",
+                                                value: "co"
+                                            },
+                                            {
+                                                key: "eq",
+                                                text: "Equals",
+                                                value: "eq"
+                                            }
+                                        ] }
+                                        placeholder="E.g. Starts With etc."
+                                    />
+                                    <Form.Input label="Filter Value" placeholder="E.g. Zoom, Salesforce etc."/>
+                                </Form.Group>
+                                <Divider hidden/>
+                                <Form.Group inline>
+                                    <PrimaryButton size="small" type="submit">
+                                        Search
+                                    </PrimaryButton>
+                                    <LinkButton size="small" type="reset">
+                                        Reset Filters
+                                    </LinkButton>
+                                </Form.Group>
+                            </Form>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </AdvancedSearch>
+        }
+        columns={ [
+            {
+                allowToggleVisibility: false,
+                dataIndex: "name",
+                id: "name",
+                key: 0,
+                title: "Name"
+            },
+            {
+                allowToggleVisibility: true,
+                dataIndex: "clientId",
+                id: "clientId",
+                key: 1,
+                title: "Client ID"
+            },
+            {
+                allowToggleVisibility: false,
+                dataIndex: "action",
+                id: "actions",
+                key: 2,
+                title: "Actions"
+            }
+        ] }
+        onRowClick={
+            (e: SyntheticEvent, { value: item }: { value: DataTableDemoDataInterface }): void => {
+                action(`Clicked on the row of ${ item.name }`)
+            }
+        }
+    />
+);
+
+WithOperationsBar.story = {
+    parameters: {
+        docs: {
+            storyDescription: meta.stories[ 1 ].description
         }
     }
 };
