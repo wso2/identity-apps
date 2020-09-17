2096c8300d8d946d5132890452df22793ca87e45
Brion Mario <info@brionmario.com>
2020-09-18 02:06:11 +0530

    Commit initial myaccount app utils refactor

--- apps/myaccount/src/init/app-utils.ts
+++ apps/myaccount/src/init/app-utils.ts
@@ -45,7 +45,7 @@ function extend(...args) {
 
     for (let i = 1; i < args.length; i++) {
         if (!args[i])
-        continue;
+            continue;
 
         for (const key in args[i]) {
             if ({}.hasOwnProperty.call(args[i], key)) {
@@ -63,8 +63,36 @@ export const AppUtils = (function() {
         _config: any = {};
 
     const fallbackServerOrigin = "https://localhost:9443";
+    const appBaseForHistoryAPIFallback = "/";
+    const urlPathForSuperTenantOriginsFallback = "";
+    const isSaasFallback = true;
+    const tenantResolutionStrategyFallback = "location";
 
     return {
+        constructAppBaseNameForHistoryAPI: function() {
+            if (_config && _config.appBaseNameForHistoryAPI) {
+                return _config.appBaseNameForHistoryAPI;
+            }
+
+            return this.isSaas() ? appBaseForHistoryAPIFallback : this.getAppBaseWithTenant();
+        },
+
+        constructAppPaths: function(path) {
+            if (!this.isSaas()) {
+                return path;
+            }
+
+            return this.getAppBaseWithTenant() + path;
+        },
+
+        constructRedirectURLs: function(url) {
+            if (!this.isSaas()) {
+                return _config.clientOrigin + this.getTenantPath(true) + "/" + _config.appBaseName + url;
+            }
+
+            return _config.clientOrigin + "/" + _config.appBaseName + url;
+        },
+
         getAppBase: function() {
             const path = this.getLocationPathWithoutTenant();
             const pathChunks = path.split("/");
@@ -80,26 +108,42 @@ export const AppUtils = (function() {
             return "/" + this.getLocationPathWithoutTenant().split("/")[1];
         },
 
+        getAppBaseWithTenant: function() {
+            return this.getTenantPath(true) + "/" + _config.appBaseName;
+        },
+
         getConfig: function() {
+            if (Object.entries(_config).length === 0) {
+                return null;
+            }
+
             return {
                 appBase: _config.appBaseName,
-                appBaseWithTenant: this.getTenantPath() + "/" + _config.appBaseName,
-                clientID: (this.isSuperTenant()) ?
-                _config.clientID : _config.clientID + "_" + this.getTenantName(),
+                appBaseNameForHistoryAPI: this.constructAppBaseNameForHistoryAPI(),
+                appBaseWithTenant: this.getAppBaseWithTenant(),
+                clientID: (this.isSaas() || this.isSuperTenant())
+                    ? _config.clientID
+                    : _config.clientID + "_" + this.getTenantName(),
                 clientOrigin: _config.clientOrigin,
-                clientOriginWithTenant: _config.clientOrigin + this.getTenantPath(),
-                loginCallbackURL: _config.clientOrigin + this.getTenantPath() + "/" + _config.appBaseName +
-                    _config.loginCallbackPath,
-                logoutCallbackURL: _config.clientOrigin + this.getTenantPath() + "/" + _config.appBaseName +
-                    _config.logoutCallbackPath,
+                clientOriginWithTenant: _config.clientOrigin + this.getTenantPath(true),
+                debug: _config.debug,
+                idpConfigs: this.resolveIdpConfigs(),
+                isSaas: _config.isSaas,
+                loginCallbackURL: this.constructRedirectURLs(_config.loginCallbackPath),
+                logoutCallbackURL: this.constructRedirectURLs(_config.logoutCallbackPath),
                 productVersion: _config.productVersion,
                 productVersionConfig: _config.ui.productVersionConfig,
-                routes: _config.routePaths,
+                routes: {
+                    home: this.constructAppPaths(_config.routePaths.home),
+                    login: this.constructAppPaths(_config.routePaths.login),
+                    logout: this.constructAppPaths(_config.routePaths.logout)
+                },
                 serverOrigin: _config.serverOrigin,
-                serverOriginWithTenant: _config.serverOrigin + this.getTenantPath(),
+                serverOriginWithTenant: _config.serverOrigin + this.getTenantPath(true),
                 session: _config.session,
                 tenant: (this.isSuperTenant()) ? this.getSuperTenant() : this.getTenantName(),
                 tenantPath: this.getTenantPath(),
+                tenantPrefix: this.getTenantPrefix(),
                 ui: _config.ui
             };
         },
@@ -108,7 +152,7 @@ export const AppUtils = (function() {
             const path = window.location.pathname;
             const pathChunks = path.split("/");
 
-            if ( (pathChunks[1] === this.getTenantPrefix()) && (pathChunks[2] === this.getTenantName()) ) {
+            if ( (pathChunks[1] === this.getTenantPrefix()) && (pathChunks[2] === this.getTenantName(true)) ) {
                 pathChunks.splice(1, 2);
 
                 return pathChunks.join("/");
@@ -121,7 +165,11 @@ export const AppUtils = (function() {
             return _args.superTenant || "carbon.super";
         },
 
-        getTenantName: function() {
+        getTenantName: function(fromLocation = this.getTenantResolutionStrategy() === "location") {
+            if (!fromLocation && this.getTenantResolutionStrategy() === "id_token" && _config.tenant) {
+                return _config.tenant;
+            }
+
             const paths = window.location.pathname.split("/");
             const tenantIndex = paths.indexOf(this.getTenantPrefix());
 
@@ -133,13 +181,23 @@ export const AppUtils = (function() {
             }
         },
 
-        getTenantPath: function() {
+        getTenantPath: function(skipSuperTenant = false) {
+            if (skipSuperTenant && (this.getTenantName() === this.getSuperTenant() || this.getTenantName() === "")) {
+                return urlPathForSuperTenantOriginsFallback;
+            }
+
             return (this.getTenantName() !== "") ?
                 "/" + this.getTenantPrefix() + "/" + this.getTenantName() : "";
         },
 
         getTenantPrefix: function() {
             return _args.tenantPrefix || "t";
+        },
+
+        getTenantResolutionStrategy: function() {
+            return (_config && _config.tenantResolutionStrategy)
+                ? _config.tenantResolutionStrategy
+                : tenantResolutionStrategyFallback;
         },
 
         init: function(Args) {
@@ -161,6 +219,8 @@ export const AppUtils = (function() {
                     throw "'appBaseName' config is missing in " + _args.deploymentConfigFile;
                 if (!{}.hasOwnProperty.call(configResponse, "clientID"))
                     throw "'clientID' config is missing in " + _args.deploymentConfigFile;
+                if (!{}.hasOwnProperty.call(configResponse, "idpConfigs"))
+                    throw "'idpConfigs' are missing in " + _args.deploymentConfigFile;
                 if (!{}.hasOwnProperty.call(configResponse, "loginCallbackPath"))
                     throw "'loginCallbackPath' config is missing in " + _args.deploymentConfigFile;
                 if (!{}.hasOwnProperty.call(configResponse, "logoutCallbackPath"))
@@ -174,8 +234,38 @@ export const AppUtils = (function() {
             });
         },
 
-        isSuperTenant: function(){
+        isSaas: function() {
+            return (_config && _config.isSaas !== undefined) ? _config.isSaas : isSaasFallback;
+        },
+
+        isSuperTenant: function() {
+            if (_config && _config.tenant && _config.tenant === this.getSuperTenant()) {
+                return true;
+            }
+
             return (this.getTenantName() === "");
+        },
+
+        resolveIdpConfigs: function() {
+            return {
+                serverOrigin: this.isSaas()
+                    ? _config.serverOrigin
+                    : _config.serverOrigin + this.getTenantPath(true),
+                ..._config.idpConfigs
+            }
+        },
+
+        updateTenantQualifiedBaseName: function(tenant) {
+            _config.tenant = tenant;
+            _config.tenantPath = this.getTenantPath();
+
+            if (tenant === this.getSuperTenant()) {
+                _config.appBaseWithTenant =  "/" + _config.appBaseName;
+
+                return;
+            }
+
+            _config.appBaseWithTenant = "/" + this.getTenantPrefix() + "/" + tenant + "/" + _config.appBaseName;
         }
     };
 }());
