05f216bb7a642c05074dca06740e72e39b84ce34
Thivi <theviyanthan@outlook.com>
2020-09-18 11:45:34 +0530

    Fix lint issues

--- modules/core/src/api/userstores.ts
+++ modules/core/src/api/userstores.ts
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import { IdentityClient, Storage } from "@asgardio/oidc-js";
+import { IdentityClient } from "@asgardio/oidc-js";
 import { AxiosError, AxiosResponse } from "axios";
 import { CommonServiceResourcesEndpoints } from "../configs";
 import { UserstoreConstants } from "../constants";
