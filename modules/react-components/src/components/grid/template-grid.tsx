b3718e0fd2f980b1d3b371f9db56d5f81130c098
Thivi <theviyanthan@outlook.com>
2020-10-08 17:11:59 +0530

    Get the template icon from the default key if no valid image is found

--- modules/react-components/src/components/grid/template-grid.tsx
+++ modules/react-components/src/components/grid/template-grid.tsx
@@ -239,25 +239,26 @@ export const TemplateGrid = <T extends WithPropertiesInterface>(
      * @return Predefined image if available. If not, return input parameter.
      */
     const resolveTemplateImage = (image: any) => {
-        if (typeof image !== "string") {
-            return image;
-        }
+        if (image) {
+            if (typeof image !== "string") {
+                return image;
+            }
 
-        if ((URLUtils.isHttpsUrl(image) || URLUtils.isHttpUrl(image)) && ImageUtils.isValidImageExtension(image)) {
-            return image;
-        }
+            if ((URLUtils.isHttpsUrl(image) || URLUtils.isHttpUrl(image)) && ImageUtils.isValidImageExtension(image)) {
+                return image;
+            }
 
-        if (URLUtils.isDataUrl(image)) {
-            return image;
-        }
+            if (URLUtils.isDataUrl(image)) {
+                return image;
+            }
 
-        if (!templateIcons) {
-            return image;
+            if (!templateIcons) {
+                return image;
+            }
         }
-
         const match = Object.keys(templateIcons).find(key => key.toString() === image);
 
-        return match ? templateIcons[match] : image;
+        return match ? templateIcons[ match ] : templateIcons[ "default" ] ?? image;
     };
 
     /**
