7d3c797154ccea75c9a0b80840e5c995a0adee05
Brion Mario <info@brionmario.com>
2020-10-22 11:11:47 +0530

    Add license headers for lint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
+/**
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+// Base ESLint Config which can be extended to be used in the development environment.
+
 module.exports = {
     extends: [
         "eslint:recommended",
@@ -108,7 +129,10 @@ module.exports = {
                         variables: false,
                         typedefs: false
                     }
-                ]
+                ],
+                // In development, error level is set to `warn`. This will be overridden
+                // by the production env linting config.
+                "no-debugger": 1
             }
         }
     ]
