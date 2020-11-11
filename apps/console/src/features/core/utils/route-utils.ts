a76e77c38f36a055f32cb9e071ce00c11e14af77
Brion Mario <info@brionmario.com>
2020-11-11 15:31:02 +0530

    Add util to gracefully navigate users

---
+++ apps/console/src/features/core/utils/route-utils.ts
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+import { RouteInterface } from "@wso2is/core/models";
+import sortBy from "lodash/sortBy";
+import { AppConstants } from "../constants";
+import { history } from "../helpers";
+
+/**
+ * Utility class for application routes related operations.
+ */
+export class RouteUtils {
+
+    /**
+     * Private constructor to avoid object instantiation from outside
+     * the class.
+     *
+     * @hideconstructor
+     */
+    private constructor() { }
+
+    /*
+     * Gracefully handles application routing to avoid less privileged users from
+     * receiving unauthorized pages.
+     *
+     * @param {RouteInterface[]} routes - Set of app routes.
+     * @param {string} view - Current view ex: Admin or Develop. 
+     * @param {string} pathname - Current path from location.
+     */
+    public static gracefullyHandleRouting(routes: RouteInterface[], view: string, pathname: string): void {
+
+        if (routes && Array.isArray(routes) && routes.length === 1) {
+            if (routes[0].redirectTo === AppConstants.getPaths().get("PAGE_NOT_FOUND")) {
+                if (RouteUtils.isHomePath(view)) {
+                    if (view === AppConstants.getDeveloperViewBasePath()) {
+                        history.push(AppConstants.getAdminViewBasePath());
+
+                        return;
+                    }
+
+                    if (view === AppConstants.getAdminViewBasePath()) {
+                        history.push(AppConstants.getDeveloperViewBasePath());
+
+                        return;
+                    }
+                }
+
+                history.push({
+                    pathname: AppConstants.getPaths().get("UNAUTHORIZED"),
+                    search: "?error=" + AppConstants.LOGIN_ERRORS.get("ACCESS_DENIED")
+                });
+            }
+        }
+
+        const isRouteAvailable: RouteInterface = routes.find((route: RouteInterface) => route.path === pathname);
+
+        if (isRouteAvailable) {
+            return;
+        }
+
+        history.push(sortBy(routes, "order")[0].path);
+    }
+
+    /**
+     * Checks if the passed in path is the home path defined in deployment.config.json.
+     *
+     * @param {string} path - Path to check.
+     * @return {boolean} Is home path or not.
+     */
+    public static isHomePath(path: string): boolean {
+
+        if (path === AppConstants.getAppHomePath()) {
+            return true;
+        }
+
+        return path?.split("/")[0] === AppConstants.getAppHomePath()?.split("/")[0];
+    }
+}
