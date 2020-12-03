945051905243f7cee2ef2c6f34b5e9da274e8452
Yasin <wytm97@protonmail.com>
2020-12-03 16:58:26 +0530

    Replaced the action interpolation value with the newly implemented function.
    Signed-off-by: Yasin <wytm97@protonmail.com>

--- apps/console/src/features/identity-providers/components/settings/outbound-provisioning-settings.tsx
+++ apps/console/src/features/identity-providers/components/settings/outbound-provisioning-settings.tsx
@@ -330,23 +330,12 @@ export const OutboundProvisioningSettings: FunctionComponent<ProvisioningSetting
     ): SegmentedAccordionTitleActionInterface[] => {
         const isDefaultConnector = connector.data?.isDefault;
         return [
-            // Checkbox which triggers the default state of connector.
-            {
-                defaultChecked: isDefaultConnector,
-                disabled: connector.data?.isDefault || !connector.data?.isEnabled,
-                label: t(isDefaultConnector ?
-                    "devPortal:components.idp.forms.outboundConnectorAccordion.default.0" :
-                    "devPortal:components.idp.forms.outboundConnectorAccordion.default.1"
-                ),
-                onChange: handleDefaultConnectorChange,
-                type: "checkbox"
-            },
             // Toggle Switch which enables/disables the connector state.
             {
                 defaultChecked: connector.data?.isEnabled,
                 label: t(connector.data?.isEnabled ?
-                    "devPortal:components.idp.forms.outboundConnectorAccordion.enable.0" :
-                    "devPortal:components.idp.forms.outboundConnectorAccordion.enable.1"
+                    "console:develop.features.idp.forms.outboundConnectorAccordion.enable.0" :
+                    "console:develop.features.idp.forms.outboundConnectorAccordion.enable.1"
                 ),
                 disabled: isDefaultConnector,
                 onChange: handleConnectorEnableToggle,
@@ -400,24 +389,7 @@ export const OutboundProvisioningSettings: FunctionComponent<ProvisioningSetting
                                                         authenticators={
                                                             [
                                                                 {
-                                                                    actions: [
-                                                                        {
-                                                                            defaultChecked: connector.data?.isDefault,
-                                                                            label: t("console:develop.features.idp." +
-                                                                                "forms.outboundConnectorAccordion." +
-                                                                                "default"),
-                                                                            onChange: handleDefaultConnectorChange,
-                                                                            type: "checkbox"
-                                                                        },
-                                                                        {
-                                                                            defaultChecked: connector?.data?.isEnabled,
-                                                                            label: t("console:develop.features.idp." +
-                                                                                "forms.outboundConnectorAccordion." +
-                                                                                "enable"),
-                                                                            onChange: handleConnectorEnableToggle,
-                                                                            type: "toggle"
-                                                                        }
-                                                                    ],
+                                                                    actions: createAccordionActions(connector),
                                                                     content: (
                                                                         <OutboundProvisioningConnectorFormFactory
                                                                             metadata={ connector.meta }
