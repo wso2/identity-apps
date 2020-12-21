93520a3cb5d16c9bd7eda3f9d5ea8479f96af400
Brion Mario <info@brionmario.com>
2020-12-21 13:44:11 +0530

    Update wizard help

--- apps/console/src/features/applications/data/application-templates/templates/oidc-web-application/create-wizard-help.tsx
+++ apps/console/src/features/applications/data/application-templates/templates/oidc-web-application/create-wizard-help.tsx
@@ -67,8 +67,9 @@ const OIDCWebApplicationCreateWizardHelp: FunctionComponent<OIDCWebApplicationCr
             <>
                 <Heading as="h5">Allowed redirect URLs</Heading>
                 <p>
-                    After the authentication, we will only redirect to the above allowed redirect URLs. You
-                    can also specify more than one URL if needed.
+                    After user sign-in or sign-out, we will only redirect to these URLs. You can specify
+                    multiple valid URLs. Make sure to specify the protocol ( https:// ) otherwise the
+                    redirect may fail in some cases.
                 </p>
                 <p>E.g. https://www.conotoso.com/login</p>
                 <Message warning>Note: This field is required for a functional app.</Message>
