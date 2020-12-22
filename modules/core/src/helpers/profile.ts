3fc4dbea002eb2bdf8476d1d02be52b4ab850fef
Shan Jayathilaka <shanchathusanda@gmail.com>
2020-12-23 00:27:17 +0530

    [IS 5.11.0]Change display name order in myaccount application

--- modules/core/src/helpers/profile.ts
+++ modules/core/src/helpers/profile.ts
@@ -32,7 +32,9 @@ export const resolveUserDisplayName = (profileInfo: ProfileInfoInterface,
                                        authState?: AuthReducerStateInterface,
                                        fallback: string = null): string => {
 
-    if (profileInfo.name && (profileInfo.name.givenName || profileInfo.name.familyName)) {
+    if (profileInfo.displayName) {
+        return profileInfo.displayName;
+    } else if (profileInfo.name && (profileInfo.name.givenName || profileInfo.name.familyName)) {
         const givenName = isEmpty(profileInfo.name.givenName) ? "" : profileInfo.name.givenName + " ";
         const familyName = isEmpty(profileInfo.name.familyName) ? "" : profileInfo.name.familyName;
 
