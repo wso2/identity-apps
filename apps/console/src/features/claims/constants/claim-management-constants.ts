176e086d726701546c9783cd1f0a756b3359705f
Thivi <theviyanthan@outlook.com>
2021-02-12 18:48:30 +0530

    Add oidc, scim constants

--- apps/console/src/features/claims/constants/claim-management-constants.ts
+++ apps/console/src/features/claims/constants/claim-management-constants.ts
@@ -108,4 +108,15 @@ export class ClaimManagementConstants {
         ClaimManagementConstants.ATTRIBUTE_DIALECT_IDS.get("SCIM2_SCHEMAS_EXT_ENT_USER"),
         ClaimManagementConstants.ATTRIBUTE_DIALECT_IDS.get("SCIM_SCHEMAS_CORE")
     ]
+
+    public static readonly OIDC_MAPPING: string[] = [
+        "http://wso2.org/oidc/claim"
+    ];
+
+    public static readonly SCIM_MAPPING: string[] = [
+        "urn:ietf:params:scim:schemas:core:2.0:User",
+        "urn:scim:schemas:core:1.0",
+        "urn:ietf:params:scim:schemas:core:2.0",
+        "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"
+    ]
 }
