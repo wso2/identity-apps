3f209cd7a8f424532395ccdc9239d9285dc0e4f5
Thivi <theviyanthan@outlook.com>
2021-02-15 22:13:27 +0530

    Revert "Extract the list to a separate component"
    This reverts commit 0e100bdf21681ac5464a653fe220f6ff90f18df6.

--- apps/console/src/features/applications/components/settings/attribute-management/attirbute-selection-wizard-other-dialect.tsx
+++ apps/console/src/features/applications/components/settings/attribute-management/attirbute-selection-wizard-other-dialect.tsx
@@ -17,18 +17,20 @@
  */
 
 import { TestableComponentInterface } from "@wso2is/core/models";
-import { useTrigger } from "@wso2is/forms";
 import {
     Code,
     Heading,
     LinkButton,
-    PrimaryButton
+    PrimaryButton,
+    TransferComponent,
+    TransferList,
+    TransferListItem
 } from "@wso2is/react-components";
-import React, { FunctionComponent, ReactElement } from "react";
+import { sortBy, union } from "lodash";
+import React, { FunctionComponent, ReactElement, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Modal } from "semantic-ui-react";
 import { ExtendedExternalClaimInterface } from "./attribute-settings";
-import { AttributeSelectList } from "../../../../core/components/attribute-select-list";
 
 interface AttributeSelectionWizardOtherDialectPropsInterface extends TestableComponentInterface {
     availableExternalClaims: ExtendedExternalClaimInterface[];
@@ -47,10 +49,12 @@ interface AttributeSelectionWizardOtherDialectPropsInterface extends TestableCom
  *
  * @return {React.ReactElement}
  */
-export const AttributeSelectionWizardOtherDialect:
-    FunctionComponent<AttributeSelectionWizardOtherDialectPropsInterface> = (
-    props: AttributeSelectionWizardOtherDialectPropsInterface
+export const AttributeSelectionWizardOtherDialect: FunctionComponent<
+    AttributeSelectionWizardOtherDialectPropsInterface
+    > = (
+        props: AttributeSelectionWizardOtherDialectPropsInterface
 ): ReactElement => {
+
     const {
         selectedExternalClaims,
         setAvailableExternalClaims,
@@ -58,49 +62,165 @@ export const AttributeSelectionWizardOtherDialect:
         showAddModal,
         setShowAddModal,
         availableExternalClaims,
-        ["data-testid"]: testId
+        [ "data-testid" ]: testId
     } = props;
 
-    const [submit, triggerSubmit] = useTrigger();
-
     const { t } = useTranslation();
+
+    const [tempAvailableClaims, setTempAvailableClaims] = useState<ExtendedExternalClaimInterface[]>([]);
+    const [tempSelectedClaims, setTempSelectedClaims] = useState<ExtendedExternalClaimInterface[]>([]);
+    const [filterTempAvailableClaims, setFilterTempAvailableClaims] = useState<ExtendedExternalClaimInterface[]>([]);
+    const [isSelectAssignedAllClaimsChecked, setIsSelectAssignedAllClaimsChecked] = useState<boolean>(false);
 
     const handleAttributeModal = () => {
         setShowAddModal(false);
     };
+
+    // search operation for available claims
+    const searchTempAvailable = (event) => {
+        const changeValue = event.target.value;
+        if (changeValue.length > 0) {
+            setFilterTempAvailableClaims(filterTempAvailableClaims.filter((item) =>
+                item.claimURI.toLowerCase().indexOf(changeValue.toLowerCase()) !== -1));
+        } else {
+            if (selectedExternalClaims.length > 0) {
+                setFilterTempAvailableClaims(sortBy(
+                    union(selectedExternalClaims, availableExternalClaims ), "localClaimDisplayName")
+                );
+            } else {
+                setFilterTempAvailableClaims(sortBy(tempAvailableClaims, "localClaimDisplayName"));
+            }
+        }
+    };
+
+    /**
+     * The following method handles the onChange event of the
+     * checkbox field of an unassigned item.
+     */
+    const handleUnassignedItemCheckboxChange = (claim) => {
+        const checkedRoles = [...tempSelectedClaims];
+
+        if (checkedRoles?.includes(claim)) {
+            checkedRoles.splice(checkedRoles.indexOf(claim), 1);
+            setTempSelectedClaims(checkedRoles);
+        } else {
+            claim.requested = true;
+            checkedRoles.push(claim);
+            setTempSelectedClaims(checkedRoles);
+        }
+
+    };
+
+    /**
+     * The following function enables the user to select all the roles at once.
+     */
+    const selectAllUnAssignedList = () => {
+        setIsSelectAssignedAllClaimsChecked(!isSelectAssignedAllClaimsChecked);
+    };
+
+    /**
+     *  Set initial values for modal.
+     */
+    useEffect(() => {
+        if (showAddModal) {
+            if (selectedExternalClaims.length > 0) {
+                const sortedClaims = sortBy(union(selectedExternalClaims, availableExternalClaims ), 
+                    "localClaimDisplayName");
+                setTempAvailableClaims(sortedClaims);
+                setFilterTempAvailableClaims(sortedClaims);
+            } else {
+                const sortedClaims = sortBy(availableExternalClaims, "localClaimDisplayName");
+                setTempAvailableClaims(sortedClaims);
+                setFilterTempAvailableClaims(sortedClaims);
+            }
+            setTempSelectedClaims(selectedExternalClaims);
+        } else {
+            setTempAvailableClaims([]);
+            setFilterTempAvailableClaims([]);
+            setTempSelectedClaims([]);
+        }
+    }, [showAddModal]);
+
+    /**
+     * Select all selected claims
+     */
+    useEffect(() => {
+        if (isSelectAssignedAllClaimsChecked) {
+            setTempSelectedClaims(filterTempAvailableClaims);
+        } else {
+            setTempSelectedClaims([]);
+        }
+    }, [isSelectAssignedAllClaimsChecked]);
+
+    /**
+     *  Save the selected claims
+     */
+    const updateSelectedClaims = (() => {
+        setInitialSelectedExternalClaims([...tempSelectedClaims]);
+        setAvailableExternalClaims([...tempAvailableClaims]);
+        handleAttributeModal();
+    });
+
 
     return (
         <Modal open={ showAddModal } size="large" className="user-roles attribute-modal" data-testid={ testId }>
             <Modal.Header>
                 { t("console:develop.features.applications.edit.sections.attributes.selection.addWizard.header") }
                 <Heading subHeading ellipsis as="h6">
-                    { t(
-                        "console:develop.features.applications.edit.sections.attributes.selection.addWizard." +
-                            "subHeading"
-                    ) }
+                    { t("console:develop.features.applications.edit.sections.attributes.selection.addWizard." + 
+                        "subHeading") }
                 </Heading>
             </Modal.Header>
             <Modal.Content image>
-                { showAddModal && (
-                    <AttributeSelectList
-                        triggerSubmit={ submit }
-                        setAvailableExternalClaims={ setAvailableExternalClaims }
-                        setInitialSelectedExternalClaims={ setInitialSelectedExternalClaims }
-                        selectedExternalClaims={ selectedExternalClaims }
-                        availableExternalClaims={ availableExternalClaims }
-                        onUpdate={ handleAttributeModal }
-                    />
-                ) }
+                <TransferComponent
+                    selectionComponent
+                    searchPlaceholder={
+                        t("console:develop.features.applications.edit.sections.attributes.selection.addWizard" +
+                            ".steps.select.transfer.searchPlaceholders.attribute")
+                    }
+                    handleHeaderCheckboxChange={ selectAllUnAssignedList }
+                    isHeaderCheckboxChecked={ isSelectAssignedAllClaimsChecked }
+                    handleUnelectedListSearch={ searchTempAvailable }
+                    data-testid={ `${ testId }-transfer-component` }
+                >
+                    <TransferList
+                        selectionComponent
+                        isListEmpty={ !(filterTempAvailableClaims.length > 0) }
+                        listType="unselected"
+                        data-testid={ `${ testId }-unselected-transfer-list` }
+                        
+                    >
+                        {
+                            filterTempAvailableClaims?.map((claim, index) => {
+                                return (
+                                    <TransferListItem
+                                        handleItemChange={ () => handleUnassignedItemCheckboxChange(claim) }
+                                        key={ claim.claimURI }
+                                        listItem={ claim.localClaimDisplayName }
+                                        listItemId={ claim.id }
+                                        listItemIndex={ claim.claimURI }
+                                        isItemChecked={ tempSelectedClaims.includes(claim) }
+                                        showSecondaryActions={ false }
+                                        showListSubItem={ true }
+                                        listSubItem={ claim.claimURI }
+                                        data-testid={ `${ testId }-unselected-transfer-list-item-${ index }` }
+                                    />
+                                );
+                            })
+                        }
+                    </TransferList>
+                </TransferComponent>
             </Modal.Content>
             <Modal.Actions>
-                <LinkButton onClick={ handleAttributeModal } data-testid={ `${testId}-cancel-button` }>
+                <LinkButton
+                    onClick={ handleAttributeModal }
+                    data-testid={ `${ testId }-cancel-button` }
+                >
                     { t("common:cancel") }
                 </LinkButton>
                 <PrimaryButton
-                    onClick={ () => {
-                        triggerSubmit();
-                    } }
-                    data-testid={ `${testId}-save-button` }
+                    onClick={ updateSelectedClaims }
+                    data-testid={ `${ testId }-save-button` }
                 >
                     { t("common:save") }
                 </PrimaryButton>
