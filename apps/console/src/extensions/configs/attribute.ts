e9aa5ab429af44f8ac8460dbe2aee6bc58627a86
Thivi <theviyanthan@outlook.com>
2021-03-12 14:01:20 +0530

    Change attribute config

--- apps/console/src/extensions/configs/attribute.ts
+++ apps/console/src/extensions/configs/attribute.ts
@@ -1,134 +1,101 @@
 /**
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * This software is the property of WSO2 Inc. and its suppliers, if any.
- * Dissemination of any information or reproduction of any material contained
- * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
- * You may not alter or remove any copyright or other notice from copies of this content."
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import { Claim, ClaimDialect, ExternalClaim } from "@wso2is/core/models";
 import { I18n } from "@wso2is/i18n";
 import { SemanticICONS } from "semantic-ui-react/dist/commonjs/generic";
-import { ClaimManagementConstants } from "../../features/claims";
-
-/**
- * Check whether claims is  identity claims or not.
- *
- * @param claim claim
- */
-const isIdentityClaims = (claim: ExternalClaim): boolean => {
-    const identityRegex = new RegExp("wso2.org/claims/identity");
-    return identityRegex.test(claim.mappedLocalClaimURI);
-};
 
 export const attributeConfig = {
-    addAttributeMapping: false,
+    addAttributeMapping: true,
     attributeMappings: {
-        deleteAction: false,
-        editAttributeMappingDetails: false,
+        deleteAction: true,
+        editAttributeMappingDetails: true,
         getExternalAttributes: (attributeType: string, response: ExternalClaim[]): ExternalClaim[] => {
-            const claims: ExternalClaim[] = [];
-
-            if (attributeType == ClaimManagementConstants.SCIM) {
-                response.forEach((claim: ExternalClaim) => {
-                    if (!claim.mappedLocalClaimURI.match(/\/identity\//)) {
-                        claims.push(claim);
-                    }
-                });
-            } else {
-                claims.push(...response);
-            }
-
-            return claims;
+            return response;
         },
-        showDangerZone: false,
-        showSCIMCore1: false
+        showDangerZone: true,
+        showSCIMCore1: true
     },
     attributes: {
-        addAttribute: false,
-        deleteAction: false,
-        description: I18n.instance.t("extensions:manage.attributes.attributes.description"),
-        excludeIdentityClaims: true,
-        //I18n.instance.t("console:manage.features.claims.local.pageLayout.local.description"),
-        showEditTabs: false,
-        showUserstoreMappingWarningIcon: false
+        addAttribute: true,
+        deleteAction: true,
+        description: I18n.instance.t("console:manage.features.claims.local.pageLayout.local.description"),
+        excludeIdentityClaims: false,
+        showEditTabs: true,
+        showUserstoreMappingWarningIcon: true
     },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     attributesPlaceholderAddButton: (attributeType: string): boolean => {
-        return attributeType !== ClaimManagementConstants.SCIM;
+        return true;
     },
     editAttributeMappings: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         showAddExternalAttributeButton: (dialectID: string): boolean => {
-            return dialectID === ClaimManagementConstants.ATTRIBUTE_DIALECT_IDS.get("OIDC");
+            return true;
         }
     },
     editAttributes: {
         getDisplayOrder: (existingDisplayOrder: number, newDisplayOrder: string): number => {
-            const DEFAULT_ATTRIBUTE_DISPLAY_ORDER = 20;
-            return existingDisplayOrder > 0 ? existingDisplayOrder : DEFAULT_ATTRIBUTE_DISPLAY_ORDER;
-            // newDisplayOrder ? parseInt(newDisplayOrder) : existingDisplayOrder;
+            return newDisplayOrder ? parseInt(newDisplayOrder) : existingDisplayOrder;
         },
-        showDangerZone: false,
-        showDisplayOrderInput: false,
-        showRequiredCheckBox: false
+        showDangerZone: true,
+        showDisplayOrderInput: true,
+        showRequiredCheckBox: true
     },
     externalAttributes: {
         editAttribute: (claim: ExternalClaim, editClaimID: string, callback: (claimID: string) => void): void => {
-            if (!isIdentityClaims(claim)) {
-                callback(editClaimID ? "" : claim?.id);
-            }
+            callback(editClaimID ? "" : claim?.id);
         },
         getEditIcon: (claim: ExternalClaim, editClaimID: string): SemanticICONS => {
-            if (isIdentityClaims(claim)) {
-                return "eye";
-            }
-            if (editClaimID === claim?.id) {
-                return "times";
-            }
-            return "pencil alternate";
-            /*  editClaim === claim?.id
-                    ? "times"
-                    : "pencil alternate", */
+            return editClaimID === claim?.id
+                ? "times"
+                : "pencil alternate";
         },
         getEditPopupText: (claim: ExternalClaim, editClaimID: string): string => {
-            if (isIdentityClaims(claim)) {
-                return I18n.instance.t("common:view");
-            }
             if (editClaimID === claim?.id) {
                 return I18n.instance.t("common:cancel");
             }
             return I18n.instance.t("common:edit");
         },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         hideDeleteIcon: (claim: ExternalClaim): boolean => {
-            return claim?.claimURI === "sub" || isIdentityClaims(claim);
+            return false;
         },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         isEditActionClickable: (claim: ExternalClaim): boolean => {
-            if (isIdentityClaims(claim)) {
-                return false;
-            }
-
             return true;
         },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         isRowClickable: (dialectID: string, item: any): boolean => {
-            return (
-                dialectID === ClaimManagementConstants.ATTRIBUTE_DIALECT_IDS.get("OIDC") &&
-                !isIdentityClaims(item) &&
-                item?.claimURI !== "sub"
-            );
+            return true;
         },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         showActions: (dialectID: string): boolean => {
-            return dialectID === ClaimManagementConstants.ATTRIBUTE_DIALECT_IDS.get("OIDC");
+            return true;
         },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         showDeleteIcon: (dialectID: string): boolean => {
-            return dialectID === ClaimManagementConstants.ATTRIBUTE_DIALECT_IDS.get("OIDC");
+            return true;
         }
     },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     isRowSelectable: (claim: Claim | ExternalClaim | ClaimDialect): boolean => {
-        if (isIdentityClaims(claim as ExternalClaim)) {
-            return false;
-        }
-
         return true;
     },
-    isSCIMEditable: false
+    isSCIMEditable: true
 };
