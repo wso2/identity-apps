28f6172d3a165052422e96456ec0e976b0283e11
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2021-03-16 15:02:27 +0530

    Update apps/console/src/features/core/constants/ui-constants.ts
    Co-authored-by: Brion Mario <info@brionmario.com>

--- apps/console/src/features/core/constants/ui-constants.ts
+++ apps/console/src/features/core/constants/ui-constants.ts
@@ -55,7 +55,7 @@ export class UIConstants {
      * @constant
      * @type {number}
      */
-    public static readonly ALERT_DISMISS_INTERVAL: number = 15; //Increased dismiss interval to 15s
+    public static readonly ALERT_DISMISS_INTERVAL: number = 15;
 
     /**
      * AJAX top loading bar height.
