80d79effd7636208f9f828eb134b57a7dd0287a4
Thivi <theviyanthan@outlook.com>
2021-03-18 00:02:12 +0530

    Refactor advance settings

--- apps/console/src/features/identity-providers/components/settings/advance-settings.tsx
+++ apps/console/src/features/identity-providers/components/settings/advance-settings.tsx
@@ -75,9 +75,11 @@ export const AdvanceSettings: FunctionComponent<AdvanceSettingsPropsInterface> =
         updateIdentityProviderDetails({ id: editingIDP.id, ...values })
             .then(() => {
                 dispatch(addAlert({
-                    description: t("console:develop.features.idp.notifications.updateIDP.success.description"),
+                    description: t("console:develop.features.authenticationProvider.notifications." +
+                        "updateIDP.success.description"),
                     level: AlertLevels.SUCCESS,
-                    message: t("console:develop.features.idp.notifications.updateIDP.success.message")
+                    message: t("console:develop.features.authenticationProvider.notifications." +
+                        "updateIDP.success.message")
                 }));
                 onUpdate(editingIDP.id);
             })
@@ -87,7 +89,7 @@ export const AdvanceSettings: FunctionComponent<AdvanceSettingsPropsInterface> =
     };
 
     return (
-        <EmphasizedSegment className="advanced-configuration-section" padded="very">
+        <EmphasizedSegment className="advanced-configuration-section">
             <AdvanceConfigurationsForm
                 config={ advancedConfigurations }
                 onSubmit={ handleAdvancedConfigFormSubmit }
