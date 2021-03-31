ac370bb9ed82ab8516f793a3b3a59142b54b9847
Minoli de Silva <minolid@wso2.com>
2021-03-31 09:24:10 +0530

    Updated sinhala and french messages

--- modules/i18n/src/translations/fr-FR/portals/console.ts
+++ modules/i18n/src/translations/fr-FR/portals/console.ts
@@ -5746,8 +5746,8 @@ export const console: ConsoleNS = {
                                     empty: "Le nom d'utilisateur est obligatoire",
                                     invalid: "Ce nom d'utilisateur n'est pas disponible.",
                                     invalidCharacters: "Le nom d'utilisateur semble contenir des caractères non valides.",
-                                    regExViolation: "Nom d'utilisateur invalide. Il doit s'agir d'un e-mail valide "+
-                                        "pouvant contenir entre 3 et 50 caractères alphanumériques, traits de "+
+                                    regExViolation: "Veuillez saisir une adresse e-mail valide. Il ne peut contenir "+
+                                        "qu'entre 3 et 50 caractères, y compris les caractères alphanumériques, traits de "+
                                         "soulignement (_), tirets (-), signes plus (+) et points (.) et un signes at "+
                                         "(@) Sans espaces."
                                 }
