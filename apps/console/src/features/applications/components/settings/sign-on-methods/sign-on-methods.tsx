df56c3d722c9bfba07938c87c731fd5761e5f023
Brion Mario <info@brionmario.com>
2021-04-25 11:43:34 +0530

    Add modals to handle google authenticator missing/duplicate scenarios

--- apps/console/src/features/applications/components/settings/sign-on-methods/sign-on-methods.tsx
+++ apps/console/src/features/applications/components/settings/sign-on-methods/sign-on-methods.tsx
@@ -17,18 +17,27 @@
  */
 
 import { SBACInterface, TestableComponentInterface } from "@wso2is/core/models";
-import { EmphasizedSegment } from "@wso2is/react-components";
+import { Code, ConfirmationModal, EmphasizedSegment, LabeledCard, Text } from "@wso2is/react-components";
+import isEmpty from "lodash-es/isEmpty";
 import React, { FunctionComponent, ReactElement, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
+import { Divider } from "semantic-ui-react";
 import { SignInMethodCustomization } from "./sign-in-method-customization";
 import { SignInMethodLanding } from "./sign-in-method-landing";
 import DefaultFlowConfigurationSequenceTemplate from "./templates/default-sequence.json";
 import GoogleLoginSequenceTemplate from "./templates/google-login-sequence.json";
 import SecondFactorTOTPSequenceTemplate from "./templates/second-factor-totp-sequence.json";
-import { AppState, ConfigReducerStateInterface, FeatureConfigInterface } from "../../../../core";
+import {
+    AppConstants,
+    AppState,
+    ConfigReducerStateInterface,
+    FeatureConfigInterface,
+    history
+} from "../../../../core";
 import { GenericAuthenticatorInterface, IdentityProviderManagementUtils } from "../../../../identity-providers";
 import { IdentityProviderManagementConstants } from "../../../../identity-providers/constants";
+import { ApplicationManagementConstants } from "../../../constants";
 import { AuthenticationSequenceInterface, LoginFlowTypes } from "../../../models";
 import { AdaptiveScriptUtils } from "../../../utils";
 
@@ -88,6 +97,20 @@ export const SignOnMethods: FunctionComponent<SignOnMethodsPropsInterface> = (
 
     const [ loginFlow, setLoginFlow ] = useState<LoginFlowTypes>(undefined);
     const [ authenticators, setAuthenticators ] = useState<GenericAuthenticatorInterface[][]>(undefined);
+    const [ googleAuthenticators, setGoogleAuthenticators ] = useState<GenericAuthenticatorInterface[]>(undefined);
+    const [ showMissingGoogleAuthenticatorModal, setShowMissingGoogleAuthenticatorModal ] = useState<boolean>(false);
+    const [
+        showDuplicateGoogleAuthenticatorSelectionModal,
+        setShowDuplicateGoogleAuthenticatorSelectionModal
+    ] = useState<boolean>(false);
+    const [
+        selectedGoogleAuthenticator,
+        setSelectedGoogleAuthenticator
+    ] = useState<GenericAuthenticatorInterface>(undefined);
+    const [
+        moderatedAuthenticationSequence,
+        setModeratedAuthenticationSequence
+    ] = useState<AuthenticationSequenceInterface>(authenticationSequence);
 
     /**
      * Loads federated authenticators and local authenticators on component load.
@@ -95,9 +118,20 @@ export const SignOnMethods: FunctionComponent<SignOnMethodsPropsInterface> = (
     useEffect(() => {
         IdentityProviderManagementUtils.getAllAuthenticators()
             .then((response: GenericAuthenticatorInterface[][]) => {
-                    setAuthenticators(response);
-                }
-            );
+
+                const google: GenericAuthenticatorInterface[] = [];
+
+                response[ 1 ].filter((authenticator: GenericAuthenticatorInterface) => {
+                    if (authenticator.defaultAuthenticator.authenticatorId
+                        ===  IdentityProviderManagementConstants.GOOGLE_OIDC_AUTHENTICATOR_ID) {
+
+                        google.push(authenticator);
+                    }
+                });
+
+                setGoogleAuthenticators(google);
+                setAuthenticators(response);
+            });
     }, []);
 
     /**
@@ -122,43 +156,199 @@ export const SignOnMethods: FunctionComponent<SignOnMethodsPropsInterface> = (
     };
 
     /**
-     * Resolve the authentication sequence based on the login flow.
+     * Handles the login flow select action.
      *
      * @param {LoginFlowTypes} loginFlow - Selected login flow.
-     * @param {AuthenticationSequenceInterface} defaultSequence - Default sequence.
-     *
-     * @return {AuthenticationSequenceInterface}
      */
-    const resolveAuthenticationSequence = (loginFlow: LoginFlowTypes,
-        defaultSequence: AuthenticationSequenceInterface): AuthenticationSequenceInterface => {
+    const handleLoginFlowSelect = (loginFlow: LoginFlowTypes): void => {
 
         if (!loginFlow) {
-            return defaultSequence;
-        }
-        
-        if (loginFlow === LoginFlowTypes.DEFAULT) {
-            return {
-                ...defaultSequence,
+            setModeratedAuthenticationSequence(authenticationSequence);
+        } else if (loginFlow === LoginFlowTypes.DEFAULT) {
+            setModeratedAuthenticationSequence({
+                ...authenticationSequence,
                 ...DefaultFlowConfigurationSequenceTemplate
-            } as AuthenticationSequenceInterface;
-        }
-        
-        if (loginFlow === LoginFlowTypes.GOOGLE_LOGIN) {
-            return {
-                ...defaultSequence,
-                ...GoogleLoginSequenceTemplate
-            } as AuthenticationSequenceInterface;
-        }
-        
-        if (loginFlow === LoginFlowTypes.SECOND_FACTOR_TOTP) {
-            return {
-                ...defaultSequence,
+            });
+        } else if (loginFlow === LoginFlowTypes.SECOND_FACTOR_TOTP) {
+            setModeratedAuthenticationSequence({
+                ...authenticationSequence,
                 ...SecondFactorTOTPSequenceTemplate
-            } as AuthenticationSequenceInterface;
+            });
+        } else if (loginFlow === LoginFlowTypes.GOOGLE_LOGIN) {
+            // If there are no IDP's with google authenticator, show missing authenticator modal.
+           if (isEmpty(googleAuthenticators)) {
+               setShowMissingGoogleAuthenticatorModal(true);
+               
+               return;
+           }
+
+            // If there are more than 1 IDP's with google authenticator, show authenticator select modal.
+           if (googleAuthenticators.length > 1) {
+               setShowDuplicateGoogleAuthenticatorSelectionModal(true);
+               
+               return;
+           }
+
+            // If there are only 1 IDP's with google authenticator, move on.
+           if (googleAuthenticators.length === 1) {
+               const modifiedGoogleLoginSequenceTemplate = { ...GoogleLoginSequenceTemplate };
+               modifiedGoogleLoginSequenceTemplate.steps[0].options[0].idp = googleAuthenticators[0].idp;
+
+               setModeratedAuthenticationSequence({
+                   ...authenticationSequence,
+                   ...modifiedGoogleLoginSequenceTemplate
+               });
+           }
         }
-        
-        return defaultSequence;
+
+        setLoginFlow(loginFlow);
     };
+
+    /**
+     * Shows the missing google authenticator modal.
+     *
+     * @return {ReactElement}
+     */
+    const renderMissingGoogleAuthenticatorModal = (): ReactElement => (
+        <ConfirmationModal
+            type="info"
+            onClose={ () => setShowMissingGoogleAuthenticatorModal(false) }
+            open={ showMissingGoogleAuthenticatorModal }
+            primaryAction={
+                t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                    "flowBuilder.addMissingGoogleAuthenticatorModal.primaryButton")
+            }
+            secondaryAction={
+                t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                    "flowBuilder.addMissingGoogleAuthenticatorModal.secondaryButton")
+            }
+            onSecondaryActionClick={ () => setShowMissingGoogleAuthenticatorModal(false) }
+            onPrimaryActionClick={ (): void => {
+                history.push({
+                    pathname: AppConstants.getPaths().get("IDP_TEMPLATES"),
+                    search: `?${
+                        IdentityProviderManagementConstants.IDP_CREATE_WIZARD_TRIGGER_URL_SEARCH_PARAM_KEY }=${
+                        IdentityProviderManagementConstants.IDP_TEMPLATE_IDS.GOOGLE
+                        }`
+                });
+            } }
+            data-testid={ `${ testId }-add-missing-authenticator-modal` }
+            closeOnDimmerClick={ false }
+        >
+            <ConfirmationModal.Header>
+                {
+                    t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                        "flowBuilder.addMissingGoogleAuthenticatorModal.heading")
+                }
+            </ConfirmationModal.Header>
+            <ConfirmationModal.Message attached info>
+                {
+                    t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                        "flowBuilder.addMissingGoogleAuthenticatorModal.content.message")
+                }
+            </ConfirmationModal.Message>
+            <ConfirmationModal.Content>
+                <Trans
+                    i18nKey={
+                        "console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                        "flowBuilder.addMissingGoogleAuthenticatorModal.content.body"
+                    }
+                >
+                    You do not have an Identity Provider configured with <Code>Google Authenticator</Code>. 
+                    Click on the <strong>Configure</strong> button to initiate the configuration process or 
+                    navigate to the <Code>Identity Providers</Code> section to manually configure.
+                </Trans>
+            </ConfirmationModal.Content>
+        </ConfirmationModal>
+    );
+
+    /**
+     * Shows a modal to select the IDP when multiple IDP's have google authenticator configured as default.
+     *
+     * @return {ReactElement}
+     */
+    const renderDuplicateGoogleAuthenticatorSelectionModal = (): ReactElement => (
+        <ConfirmationModal
+            type="warning"
+            onClose={ () => setShowDuplicateGoogleAuthenticatorSelectionModal(false) }
+            open={ showDuplicateGoogleAuthenticatorSelectionModal }
+            primaryAction={
+                t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                    "flowBuilder.duplicateGoogleAuthenticatorSelectionModal.primaryButton")
+            }
+            secondaryAction={
+                t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                    "flowBuilder.duplicateGoogleAuthenticatorSelectionModal.secondaryButton")
+            }
+            onSecondaryActionClick={ () => setShowDuplicateGoogleAuthenticatorSelectionModal(false) }
+            onPrimaryActionClick={ (): void => {
+                
+                const modifiedGoogleLoginSequenceTemplate = { ...GoogleLoginSequenceTemplate };
+                modifiedGoogleLoginSequenceTemplate.steps[0].options[0].idp = selectedGoogleAuthenticator.idp;
+
+                setModeratedAuthenticationSequence({
+                    ...authenticationSequence,
+                    ...modifiedGoogleLoginSequenceTemplate
+                });
+
+                setLoginFlow(LoginFlowTypes.GOOGLE_LOGIN);
+                setShowDuplicateGoogleAuthenticatorSelectionModal(false);
+            } }
+            data-testid={ `${ testId }-duplicate-authenticator-selection-modal` }
+            closeOnDimmerClick={ false }
+        >
+            <ConfirmationModal.Header>
+                {
+                    t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                        "flowBuilder.duplicateGoogleAuthenticatorSelectionModal.heading")
+                }
+            </ConfirmationModal.Header>
+            <ConfirmationModal.Message attached info>
+                {
+                    t("console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                        "flowBuilder.duplicateGoogleAuthenticatorSelectionModal.content.message")
+                }
+            </ConfirmationModal.Message>
+            <ConfirmationModal.Content>
+                <Text>
+                    <Trans
+                        i18nKey={
+                            "console:develop.features.applications.edit.sections.signOnMethod.sections.landing." +
+                            "flowBuilder.duplicateGoogleAuthenticatorSelectionModal.content.body"
+                        }
+                    >
+                        You have multiple Identity Providers configured with <Code>Google Authenticator</Code>. 
+                        Select the desired one from the selection bellow to proceed
+                    </Trans>
+                </Text>
+                <Divider hidden />
+                <div>
+                    {
+                        googleAuthenticators.map((authenticator, index) => (
+                            <LabeledCard
+                                key={ index }
+                                multilineLabel
+                                className="authenticator-card"
+                                size="tiny"
+                                selected={ selectedGoogleAuthenticator?.id === authenticator.id }
+                                image={ authenticator.image }
+                                label={
+                                    ApplicationManagementConstants
+                                        .AUTHENTICATOR_DISPLAY_NAMES.get(authenticator.name)
+                                    || authenticator.displayName
+                                }
+                                labelEllipsis={ true }
+                                data-testid={
+                                    `${ testId }-authenticator-${ authenticator.name }`
+                                }
+                                onClick={ () => setSelectedGoogleAuthenticator(authenticator) }
+                            />
+                        ))
+                    }
+                </div>
+            </ConfirmationModal.Content>
+        </ConfirmationModal>
+    );
 
     return (
         <EmphasizedSegment className="sign-on-methods-tab-content" padded="very">
@@ -168,7 +358,7 @@ export const SignOnMethods: FunctionComponent<SignOnMethodsPropsInterface> = (
                         <SignInMethodLanding
                             isLoading={ isLoading }
                             readOnly={ readOnly }
-                            onLoginFlowSelect={ (type: LoginFlowTypes) => setLoginFlow(type) }
+                            onLoginFlowSelect={ handleLoginFlowSelect }
                         />
                     )
                     : (
@@ -176,14 +366,14 @@ export const SignOnMethods: FunctionComponent<SignOnMethodsPropsInterface> = (
                             <SignInMethodCustomization
                                 appId={ appId }
                                 authenticators={ authenticators }
-                                authenticationSequence={
-                                    resolveAuthenticationSequence(loginFlow, authenticationSequence)
-                                }
+                                authenticationSequence={ moderatedAuthenticationSequence }
                                 onUpdate={ onUpdate }
                             />
                         </>
                     )
             }
+            { showMissingGoogleAuthenticatorModal && renderMissingGoogleAuthenticatorModal() }
+            { showDuplicateGoogleAuthenticatorSelectionModal && renderDuplicateGoogleAuthenticatorSelectionModal() }
         </EmphasizedSegment>
     );
 };
