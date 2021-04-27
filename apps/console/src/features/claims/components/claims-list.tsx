185e33a5ff29a6f408bb0962398a252a0c3bddf8
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2021-04-27 09:21:33 +0530

    Update apps/console/src/features/claims/components/claims-list.tsx
    Co-authored-by: Nipuni Paaris <nipunib@wso2.com>

--- apps/console/src/features/claims/components/claims-list.tsx
+++ apps/console/src/features/claims/components/claims-list.tsx
@@ -894,7 +894,7 @@ export const ClaimsList: FunctionComponent<ClaimsListPropsInterface> = (
                     renderer: "semantic-icon"
                 },
                 attributeConfig.attributeMappings.deleteAction && {
-                    hidden: (): boolean => hasRequiredScopes(featureConfig?.attributeDialects,
+                    hidden: (): boolean => !hasRequiredScopes(featureConfig?.attributeDialects,
                         featureConfig?.attributeDialects?.scopes?.delete, allowedScopes),
                     icon: (): SemanticICONS => "trash alternate",
                     onClick: (e: SyntheticEvent, dialect: ClaimDialect): void => initDelete(ListType.DIALECT, dialect),
