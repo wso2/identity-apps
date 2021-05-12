0209621e9d72062b05fb311f5587ff4357e1f803
Brion Mario <info@brionmario.com>
2021-05-13 02:38:21 +0530

    Fix template ids type issue

--- apps/console/src/features/identity-providers/constants/identity-provider-management-constants.ts
+++ apps/console/src/features/identity-providers/constants/identity-provider-management-constants.ts
@@ -81,9 +81,9 @@ export class IdentityProviderManagementConstants {
      * Set of IDP template Ids.
      * @type {Record<string, unknown>}
      */
-    public static readonly IDP_TEMPLATE_IDS: Record<string, unknown> = {
-        GOOGLE: "8ea23303-49c0-4253-b81f-82c0fe6fb4a0",
+    public static readonly IDP_TEMPLATE_IDS: Record<string, string> = {
         ENTERPRISE: "enterprise-idp",
+        GOOGLE: "8ea23303-49c0-4253-b81f-82c0fe6fb4a0"
     };
 
     /**
