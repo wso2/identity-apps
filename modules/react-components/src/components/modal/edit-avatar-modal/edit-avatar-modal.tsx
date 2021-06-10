fc871b314e3cdff1f2ac4743b937e4e6f46ce825
Kayathiri <kayathirikaya33@gmail.com>
2021-06-10 09:33:59 +0530

    Add separate method for GravatarUrl

--- modules/react-components/src/components/modal/edit-avatar-modal/edit-avatar-modal.tsx
+++ modules/react-components/src/components/modal/edit-avatar-modal/edit-avatar-modal.tsx
@@ -199,6 +199,7 @@ export const EditAvatarModal: FunctionComponent<EditAvatarModalPropsInterface> =
     const [ gravatarURLs, setGravatarURLs ] = useState<Map<string, string>>(undefined);
     const [ selectedAvatarType, setSelectedAvatarType ] = useState<AvatarTypes>(undefined);
     const [ hostedURL, setHostedURL ] = useState<string>(undefined);
+    const [isGravatarUrl, setIsGravatarUrl] = useState<boolean>(false);
     const [ hostedURLError, setHostedURLError ] = useState<LabelProps>(undefined);
     const [
         outputURL,
@@ -283,14 +284,37 @@ export const EditAvatarModal: FunctionComponent<EditAvatarModalPropsInterface> =
         setGravatarURLs(urls);
     }, [ selectedGravatarEmail, isGravatarQualified ]);
 
+    /**
+     * Set the Avatar types based on imageUrl.
+     */
     useEffect(() => {
         if (imageUrl) {
-            setOutputURL(imageUrl);
-            setSelectedAvatarType(AvatarTypes.GRAVATAR);
+            if (isGravatarUrl) {
+                setOutputURL(imageUrl);
+                setSelectedAvatarType(AvatarTypes.GRAVATAR);
+            } else {
+                setHostedURL(imageUrl);
+                setSelectedAvatarType(AvatarTypes.URL);
+            }
         } else {
             setSelectedAvatarType(AvatarTypes.SYSTEM_GENERATED);
         }
-    }, [ imageUrl ]);
+    }, [ imageUrl , isGravatarUrl ]);
+
+    /**
+     * Check the type of imageUrl.
+     */
+    useEffect(() => {
+
+        if (gravatarURLs) {
+            gravatarURLs.forEach((value: string, key: string) => {
+                if (imageUrl.localeCompare(value) == 0) {
+                    setIsGravatarUrl(true);
+                }
+            })
+
+        }
+    }, [ gravatarURLs ]);
 
     /**
      * Handles selected gravatar email change.
