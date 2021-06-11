188e1d055598d97dcc748ae756b47f32b169d679
Brion Mario <info@brionmario.com>
2021-06-11 21:51:27 +0530

    Update apps/console/src/extensions/configs/models/application.ts

--- apps/console/src/extensions/configs/models/application.ts
+++ apps/console/src/extensions/configs/models/application.ts
@@ -77,7 +77,7 @@ export interface ApplicationConfig {
                 secondFactorDisabledInFirstStep: ReactNode;
             };
         };
-    },
+    };
     templates: {
         android: boolean;
         oidc: boolean;
