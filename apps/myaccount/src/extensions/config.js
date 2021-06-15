5f73e40dd52abfdbc87d4df4557e70cef28d5ba3
Kayathiri <kayathirikaya33@gmail.com>
2021-06-15 16:32:56 +0530

    Update apps/myaccount/src/extensions/config.js
    Co-authored-by: Nipuni Paaris <nipunib@wso2.com>

--- apps/myaccount/src/extensions/config.js
+++ apps/myaccount/src/extensions/config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
  * 
  * This software is the property of WSO2 Inc. and its suppliers, if any.
  * Dissemination of any information or reproduction of any material contained
