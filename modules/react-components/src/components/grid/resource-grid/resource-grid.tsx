66d5a14e09a72beaac19ca290622c4a58adf2471
Brion Mario <info@brionmario.com>
2021-06-28 20:34:20 +0530

    Implement resource grid component

---
+++ modules/react-components/src/components/grid/resource-grid/resource-grid.tsx
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { TestableComponentInterface } from "@wso2is/core/models";
+import classNames from "classnames";
+import React, {
+    Fragment,
+    FunctionComponent, PropsWithChildren,
+    ReactElement
+} from "react";
+import {
+    Card,
+    CardGroupProps,
+    Divider
+} from "semantic-ui-react";
+import { ResourceGridCard } from "./resource-grid-card";
+import { SearchWithFilterLabels, SearchWithFilterLabelsPropsInterface } from "../../input";
+
+/**
+ * Interface for the Resource Grid sub components.
+ */
+export interface ResourceGridSubComponentsInterface {
+    Card: typeof ResourceGridCard;
+}
+
+/**
+ * Interface for the Resource Grid component.
+ */
+export interface ResourceGridPropsInterface extends CardGroupProps, TestableComponentInterface {
+
+    /**
+     * Show/Hide search.
+     */
+    search?: ReactElement;
+    /**
+     * Search component options.
+     */
+    searchOptions?: SearchWithFilterLabelsPropsInterface;
+    /**
+     * CSS classes for the wrapper.
+     */
+    wrapperClassName?: string;
+}
+
+/**
+ * Resource Grid component.
+ * Displays resources as cards on a grid.
+ *
+ * @param {ResourceGridPropsInterface} props - Props injected to the component.
+ * @return {React.ReactElement}
+ */
+export const ResourceGrid: FunctionComponent<
+    PropsWithChildren<ResourceGridPropsInterface>> & ResourceGridSubComponentsInterface = (
+        props: ResourceGridPropsInterface
+): ReactElement => {
+
+    const {
+        children,
+        className,
+        searchOptions,
+        showSearch,
+        testId,
+        wrapperClassName,
+        ...rest
+    } = props;
+
+    const classes = classNames(
+        "resource-grid",
+        className
+    );
+    
+    const wrapperClasses = classNames(
+        "resource-grid-wrapper",
+        wrapperClassName
+    );
+
+    return (
+        <div className={ wrapperClasses } data-testid={ testId }>
+            {
+                showSearch && (
+                    <Fragment>
+                        <SearchWithFilterLabels
+                            data-testid={ `${ testId }-search` }
+                            { ...searchOptions }
+                        />
+                        <Divider hidden />
+                    </Fragment>
+                )
+            }
+            <Card.Group className={ classes } { ...rest }>
+                { children }
+            </Card.Group>
+        </div>
+    );
+};
+
+/**
+ * Default props for the component.
+ */
+ResourceGrid.defaultProps = {
+    "data-testid": "resource-grid"
+};
+
+ResourceGrid.Card = ResourceGridCard;
