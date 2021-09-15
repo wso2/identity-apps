d957dd509152de50f4200a89976ba71208e85b7b
Thivi <theviyanthan@outlook.com>
2021-09-16 03:44:02 +0530

    Remove init entry

--- apps/myaccount/webpack.config.js
+++ apps/myaccount/webpack.config.js
@@ -140,7 +140,6 @@ module.exports = (env) => {
                 : false
             : isDevelopment && "eval-cheap-module-source-map",
         entry: {
-            init: [ "@babel/polyfill", "./src/init/init.ts" ],
             main: "./src/index.tsx"
         },
         infrastructureLogging: {
