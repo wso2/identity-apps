bab106ae8f34c9980ab5fcf27114cfa48d873894
Thivi <theviyanthan@outlook.com>
2021-10-06 17:00:04 +0530

    Remove i18n logic from the index file

--- apps/console/src/index.tsx
+++ apps/console/src/index.tsx
@@ -17,15 +17,7 @@
  */
 
 import { AuthParams, AuthProvider, SPAUtils } from "@asgardeo/auth-react";
-import { setSupportedI18nLanguages } from "@wso2is/core/store";
-import { ContextUtils, StringUtils } from "@wso2is/core/utils";
-import {
-    I18n,
-    I18nInstanceInitException,
-    I18nModuleConstants,
-    LanguageChangeException,
-    isLanguageSupported
-} from "@wso2is/i18n";
+import { ContextUtils } from "@wso2is/core/utils";
 import axios from "axios";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
@@ -39,51 +31,6 @@ import "regenerator-runtime/runtime";
 
 // Set the runtime config in the context.
 ContextUtils.setRuntimeConfig(Config.getDeploymentConfig());
-
-// If `appBaseNameWithoutTenant` is "", avoids adding a forward slash.
-const resolvedAppBaseNameWithoutTenant: string = StringUtils.removeSlashesFromPath(
-    Config.getDeploymentConfig().appBaseNameWithoutTenant)
-    ? `/${ StringUtils.removeSlashesFromPath(Config.getDeploymentConfig().appBaseNameWithoutTenant) }`
-    : "";
-
-const metaFileNames = I18nModuleConstants.META_FILENAME.split(".");
-const metaFileName = `${ metaFileNames[ 0 ] }.${ process.env.metaHash }.${ metaFileNames[ 1 ] }`;
-
-// Since the portals are not deployed per tenant, looking for static resources in tenant qualified URLs
-// will fail. This constructs the path without the tenant, therefore it'll look for the file in
-// `https://localhost:9443/<PORTAL>/resources/i18n/meta.json` rather than looking for the file in
-// `https://localhost:9443/t/wso2.com/<PORTAL>/resources/i18n/meta.json`.
-const metaPath = `${ resolvedAppBaseNameWithoutTenant }/
-${ StringUtils.removeSlashesFromPath(Config.getI18nConfig().resourcePath) }/${ metaFileName }`;
-
-// Fetch the meta file to get the supported languages and paths.
-axios.get(metaPath)
-    .then((response) => {
-    // Set up the i18n module.
-    I18n.init({
-        ...Config.getI18nConfig(response?.data)?.initOptions,
-        debug: window["AppUtils"].getConfig().debug
-        },
-        Config.getI18nConfig()?.overrideOptions,
-        Config.getI18nConfig()?.langAutoDetectEnabled,
-        Config.getI18nConfig()?.xhrBackendPluginEnabled)
-        .then(() => {
-                // Set the supported languages in redux store.
-                store.dispatch(setSupportedI18nLanguages(response?.data));
-
-                const isSupported = isLanguageSupported(I18n.instance.language, null, response?.data);
-
-                if (!isSupported) {
-                    I18n.instance.changeLanguage(I18nModuleConstants.DEFAULT_FALLBACK_LANGUAGE)
-                        .catch((error) => {
-                            throw new LanguageChangeException(I18nModuleConstants.DEFAULT_FALLBACK_LANGUAGE, error);
-                        });
-                }
-            });
-        })
-        .catch((error) => {
-            throw new I18nInstanceInitException(error);
-        });
 
 const getAuthParams = (): Promise<AuthParams> => {
     if (!SPAUtils.hasAuthSearchParamsInURL() && process.env.NODE_ENV === "production") {
