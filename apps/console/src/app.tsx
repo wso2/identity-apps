90f0d0e0187d0e8f25aa6df29efff4a4d96a1089
Brion Mario <info@brionmario.com>
2021-10-29 12:45:29 +0530

    Update app.tsx

--- apps/console/src/app.tsx
+++ apps/console/src/app.tsx
@@ -65,6 +65,9 @@ import { AppState } from "./features/core/store";
  * @return {React.ReactElement}
  */
 export const App: FunctionComponent<Record<string, never>> = (): ReactElement => {
+    
+    const a = "test";
+
     const dispatch = useDispatch();
 
     const userName: string = useSelector((state: AppState) => state.auth.username);
