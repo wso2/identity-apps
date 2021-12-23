952ffdef22b61d1e52c054023cf83b3926e318de
Brion Mario <info@brionmario.com>
2021-12-23 19:44:16 +0530

    Add extended feature resource endpoints

--- apps/console/src/features/core/configs/app.ts
+++ apps/console/src/features/core/configs/app.ts
@@ -19,6 +19,7 @@
 import { DocumentationConstants } from "@wso2is/core/constants";
 import { DocumentationProviders, DocumentationStructureFileTypes } from "@wso2is/core/models";
 import { I18nModuleInitOptions, I18nModuleOptionsInterface, MetaI18N, generateBackendPaths } from "@wso2is/i18n";
+import { getExtendedFeatureResourceEndpoints } from "../../../extensions/configs/endpoints";
 import { getApplicationsResourceEndpoints } from "../../applications";
 import { getCertificatesResourceEndpoints } from "../../certificates";
 import { getClaimResourceEndpoints } from "../../claims";
@@ -177,6 +178,7 @@ export class Config {
             ...getGroupsResourceEndpoints(this.getDeploymentConfig().serverHost),
             ...getRemoteFetchConfigResourceEndpoints(this.getDeploymentConfig().serverHost),
             ...getSecretsManagementEndpoints(this.getDeploymentConfig().serverHost),
+            ...getExtendedFeatureResourceEndpoints(this.getDeploymentConfig().serverHost),
             CORSOrigins: `${this.getDeploymentConfig().serverHost}/api/server/v1/cors/origins`,
             // TODO: Remove this endpoint and use ID token to get the details
             me: `${this.getDeploymentConfig().serverHost}/scim2/Me`,
