e228f873f911a0539676d20b5c4c25f6d8ecdd12
Thivi <theviyanthan@outlook.com>
2022-02-17 09:16:15 +0530

    Allow fido to be a first factor

--- apps/console/src/features/applications/constants/application-management.ts
+++ apps/console/src/features/applications/constants/application-management.ts
@@ -352,7 +352,8 @@ export class ApplicationManagementConstants {
     // First factor authenticators.
     public static readonly FIRST_FACTOR_AUTHENTICATORS = [
         IdentityProviderManagementConstants.BASIC_AUTHENTICATOR,
-        IdentityProviderManagementConstants.IDENTIFIER_FIRST_AUTHENTICATOR
+        IdentityProviderManagementConstants.IDENTIFIER_FIRST_AUTHENTICATOR,
+        IdentityProviderManagementConstants.FIDO_AUTHENTICATOR
     ];
 
     // Second factor authenticators.
