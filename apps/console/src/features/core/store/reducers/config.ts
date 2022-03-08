08ca916da03158c43c889b48d2cfd76ae2e3c475
Yathindra <yathindrarawya123@gmail.com>
2022-03-08 13:27:57 +0530

    Fx redux store null pointer issue

--- apps/console/src/features/core/store/reducers/config.ts
+++ apps/console/src/features/core/store/reducers/config.ts
@@ -36,8 +36,18 @@ export const commonConfigReducerInitialState: CommonConfigReducerStateInterface<
     UIConfigInterface> = {
 
         deployment: {
-            accountApp: null,
-            adminApp: null,
+            accountApp: {
+                basePath: "",
+                displayName: "",
+                path: "",
+                tenantQualifiedPath: ""
+            },
+            adminApp: {
+                basePath: "",
+                displayName: "",
+                path: "",
+                tenantQualifiedPath: ""
+            },
             allowMultipleAppProtocols: undefined,
             appBaseName: "",
             appBaseNameWithoutTenant: "",
