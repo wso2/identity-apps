d24ae1ca105aca0d879739ba7ea7b2c5898189c7
Yasin <wytm97@protonmail.com>
2022-05-26 09:46:19 +0530

    fix(applications): duplicate issuer error message

--- apps/console/src/features/applications/components/wizard/minimal-application-create-wizard.tsx
+++ apps/console/src/features/applications/components/wizard/minimal-application-create-wizard.tsx
@@ -365,8 +365,7 @@ export const MinimalAppCreateWizard: FunctionComponent<MinimalApplicationCreateW
                 if (error.response && error.response.data && error.response.data.code &&
                     error.response.data.code === ApplicationManagementConstants.ERROR_CODE_ISSUER_EXISTS) {
                     if (protocolValuesChange) {
-                        if (selectedTemplate.authenticationProtocol === SupportedAuthProtocolTypes.SAML && 
-                            samlConfigureMode !== SAMLConfigModes.MANUAL) {
+                        if (samlConfigureMode !== SAMLConfigModes.MANUAL) {
                             setAlert({
                                 description: error.response.data.description,
                                 level: AlertLevels.ERROR,
