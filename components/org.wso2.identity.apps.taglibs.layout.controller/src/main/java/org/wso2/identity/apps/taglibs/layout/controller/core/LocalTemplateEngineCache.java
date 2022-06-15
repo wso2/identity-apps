abbd38b563fd4209c2bf5403db4cb58d493dbc5e
Dilshan Senarath <dilshan99news@gmail.com>
2022-06-15 21:55:23 +0530

    resolve bugs

--- components/org.wso2.identity.apps.taglibs.layout.controller/src/main/java/org/wso2/identity/apps/taglibs/layout/controller/core/LocalTemplateEngineCache.java
+++ components/org.wso2.identity.apps.taglibs.layout.controller/src/main/java/org/wso2/identity/apps/taglibs/layout/controller/core/LocalTemplateEngineCache.java
@@ -37,9 +37,9 @@ import javax.cache.spi.CachingProvider;
  * Cache implementation of the TemplateEngine interface using local compiler
  */
 public class LocalTemplateEngineCache implements TemplateEngine {
-	
-	private static final long serialVersionUID = 128582202509525932L;
-	public ExecutableIdentifier compiledObject = null;
+
+    private static final long serialVersionUID = 128582202509525932L;
+    public ExecutableIdentifier compiledObject = null;
     public DefaultExecutor executor = null;
 
     /**
