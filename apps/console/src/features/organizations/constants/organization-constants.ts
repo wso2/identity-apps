2a3ce9716fe7421ec9218c5c3978e421f04e32c4
Thivi <theviyanthan@outlook.com>
2022-07-08 12:00:15 +0530

    Fix typo

--- apps/console/src/features/organizations/constants/organization-constants.ts
+++ apps/console/src/features/organizations/constants/organization-constants.ts
@@ -34,7 +34,7 @@ export class OrganizationManagementConstants {
     /**
      * Root organization object.
      *
-     * @@constant
+     * @constant
      * @type {OrganizationInterface}
      * @default
      */
