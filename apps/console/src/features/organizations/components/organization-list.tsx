ba643a8f28734171008b43ebcee28f4150dc438a
kavinda1995 <kavindajayakody@gmail.com>
2022-07-11 16:30:51 +0530

    Change of name of the edit organization - profile tab to overview, change deletion modal type to negative

--- apps/console/src/features/organizations/components/organization-list.tsx
+++ apps/console/src/features/organizations/components/organization-list.tsx
@@ -460,7 +460,7 @@ export const OrganizationList: FunctionComponent<OrganizationListPropsInterface>
             { deletingOrganization && (
                 <ConfirmationModal
                     onClose={ (): void => setShowDeleteConfirmationModal(false) }
-                    type="warning"
+                    type="negative"
                     open={ showDeleteConfirmationModal }
                     assertionHint={ t(
                         "console:manage.features.organizations.confirmations.deleteOrganization." + "assertionHint"
