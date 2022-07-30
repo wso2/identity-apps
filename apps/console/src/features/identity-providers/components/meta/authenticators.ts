a06d6f13d4ae0b5e7ec3bfc1033476d01204746d
Brion <info@brionmario.com>
2022-07-30 20:40:07 +0530

    Add authenticator descriptions

--- apps/console/src/features/identity-providers/components/meta/authenticators.ts
+++ apps/console/src/features/identity-providers/components/meta/authenticators.ts
@@ -17,78 +17,91 @@
  */
 
 import { getAuthenticatorIcons, getIdPIcons } from "../../configs";
+import { AuthenticatorMeta } from "../../meta";
 import { FederatedAuthenticatorMetaDataInterface, StrictGenericAuthenticatorInterface } from "../../models";
 
 export const getFederatedAuthenticators = (): FederatedAuthenticatorMetaDataInterface[] => {
     return [
         {
             authenticatorId: "T2ZmaWNlMzY1QXV0aGVudGljYXRvcg",
+            description: "Seamless integration with Office 365.",
             displayName: "Office 365",
             icon: getIdPIcons().office365,
             name: "Office365Authenticator"
         },
         {
             authenticatorId: "VHdpdHRlckF1dGhlbnRpY2F0b3I",
+            description: "Login users with existing Twitter accounts.",
             displayName: "Twitter",
             icon: getIdPIcons().twitter,
             name: "TwitterAuthenticator"
         },
         {
             authenticatorId: "RmFjZWJvb2tBdXRoZW50aWNhdG9y",
+            description: "Login users with existing Facebook accounts.",
             displayName: "Facebook",
             icon: getIdPIcons().facebook,
             name: "FacebookAuthenticator"
         },
         {
             authenticatorId: "R29vZ2xlT0lEQ0F1dGhlbnRpY2F0b3I",
-            displayName: "Google OIDC",
+            description: "Login users with existing Google accounts.",
+            displayName: "Google",
             icon: getIdPIcons().google,
             name: "GoogleOIDCAuthenticator"
         },
         {
             authenticatorId: "TWljcm9zb2Z0V2luZG93c0xpdmVBdXRoZW50aWNhdG9y",
+            description: "Login users with their Microsoft Live accounts.",
             displayName: "Microsoft Windows Live",
             icon: getIdPIcons().microsoft,
             name: "MicrosoftWindowsLiveAuthenticator"
         },
         {
             authenticatorId: "UGFzc2l2ZVNUU0F1dGhlbnRpY2F0b3I",
+            description: "Login users with WS Federation.",
             displayName: "Passive STS",
             icon: getIdPIcons().wsFed,
             name: "PassiveSTSAuthenticator"
         },
         {
             authenticatorId: "WWFob29PQXV0aDJBdXRoZW50aWNhdG9y",
-            displayName: "Yahoo OAuth 2",
+            description: "Login users with their Yahoo accounts.",
+            displayName: "Yahoo",
             icon: getIdPIcons().yahoo,
             name: "YahooOAuth2Authenticator"
         },
         {
             authenticatorId: "SVdBS2VyYmVyb3NBdXRoZW50aWNhdG9y",
+            description: "Login users to Microsoft Windows servers.",
             displayName: "IWA Kerberos",
             icon: getIdPIcons().iwaKerberos,
             name: "IWAKerberosAuthenticator"
         },
         {
             authenticatorId: "U0FNTFNTT0F1dGhlbnRpY2F0b3I",
-            displayName: "SAML SSO",
+            description: "Login users with their accounts using SAML protocol.",
+            displayName: "SAML",
             icon: getIdPIcons().saml,
             name: "SAMLSSOAuthenticator"
         },
         {
             authenticatorId: "T3BlbklEQ29ubmVjdEF1dGhlbnRpY2F0b3I",
+            description: "Login users with their accounts using OpenID Connect protocol.",
             displayName: "OpenID Connect",
             icon: getIdPIcons().oidc,
             name: "OpenIDConnectAuthenticator"
         },
         {
             authenticatorId: "RW1haWxPVFA",
+            description: AuthenticatorMeta.getAuthenticatorDescription("RW1haWxPVFA"),
             displayName: "Email OTP",
             icon: getIdPIcons().emailOTP,
             name: "EmailOTP"
         },
         {
             authenticatorId: "U01TT1RQ",
+            description: AuthenticatorMeta.getAuthenticatorDescription("U01TT1RQ"),
             displayName: "SMS OTP",
             icon: getIdPIcons().smsOTP,
             name: "SMSOTP"
