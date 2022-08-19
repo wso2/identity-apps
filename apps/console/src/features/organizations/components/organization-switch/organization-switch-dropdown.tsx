e9929d6b9769dfbf37832e22b8f6aee1c9d2a7c3
kavinda1995 <kavindajayakody@gmail.com>
2022-08-19 11:15:52 +0530

    Fix merge issues

--- apps/console/src/features/organizations/components/organization-switch/organization-switch-dropdown.tsx
+++ apps/console/src/features/organizations/components/organization-switch/organization-switch-dropdown.tsx
@@ -116,58 +116,60 @@ const OrganizationSwitchDropdown: FunctionComponent<OrganizationSwitchDropdownIn
                 superOrg = {
                     id: superOrgId,
                     name: data?.name,
-                    ref: ""
+                    ref: "",
+                    status: "ACTIVE"
                 };
             }
         } catch(error) {
             superOrg = { ...OrganizationManagementConstants.ROOT_ORGANIZATION };
         }
 
-        getOrganizations(filterStrWithDisableFilter, 5, after, before, false, false).then((response: OrganizationListInterface) => {
-            if (!response || !response.organizations) {
+        getOrganizations(filterStrWithDisableFilter, 5, after, before, false, false)
+            .then((response: OrganizationListInterface) => {
+                if (!response || !response.organizations) {
+                    superOrg && setAssociatedOrganizations([ superOrg ]);
+                    setPaginationData(response.links);
+                } else {
+                    const organizations: OrganizationInterface[] = superOrg
+                        ? [ superOrg, ...response?.organizations ]
+                        : [ ...response?.organizations ];
+
+                    setAssociatedOrganizations(organizations);
+
+                    setPaginationData(response.links);
+                }
+            }).catch((error) => {
                 superOrg && setAssociatedOrganizations([ superOrg ]);
-                setPaginationData(response.links);
-            } else {
-                const organizations: OrganizationInterface[] = superOrg
-                    ? [ superOrg, ...response?.organizations ]
-                    : [ ...response?.organizations ];
 
-                setAssociatedOrganizations(organizations);
+                if (error?.description) {
+                    dispatch(
+                        addAlert({
+                            description: error.description,
+                            level: AlertLevels.ERROR,
+                            message: t(
+                                "console:manage.features.organizations.notifications." +
+                                    "getOrganizationList.error.message"
+                            )
+                        })
+                    );
 
-                setPaginationData(response.links);
-            }
-        }).catch((error) => {
-            superOrg && setAssociatedOrganizations([ superOrg ]);
+                    return;
+                }
 
-            if (error?.description) {
                 dispatch(
                     addAlert({
-                        description: error.description,
+                        description: t(
+                            "console:manage.features.organizations.notifications.getOrganizationList" +
+                                ".genericError.description"
+                        ),
                         level: AlertLevels.ERROR,
                         message: t(
                             "console:manage.features.organizations.notifications." +
-                                    "getOrganizationList.error.message"
+                                "getOrganizationList.genericError.message"
                         )
                     })
                 );
-
-                return;
-            }
-
-            dispatch(
-                addAlert({
-                    description: t(
-                        "console:manage.features.organizations.notifications.getOrganizationList" +
-                                ".genericError.description"
-                    ),
-                    level: AlertLevels.ERROR,
-                    message: t(
-                        "console:manage.features.organizations.notifications." +
-                                "getOrganizationList.genericError.message"
-                    )
-                })
-            );
-        });
+            });
     }, [ data ]);
 
     const setPaginationData = (links: OrganizationLinkInterface[]) => {
