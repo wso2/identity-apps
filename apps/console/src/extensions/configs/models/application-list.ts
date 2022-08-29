7933beeab96bbf2b88018674b3070d1f81d73e29
malshan1998 <malshanranawella@gmail.com>
2022-08-29 14:12:08 +0530

    Added TSDoc comment

--- apps/console/src/extensions/configs/models/application-list.ts
+++ apps/console/src/extensions/configs/models/application-list.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2022, WSO2 LLC (http://www.wso2.com).
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -17,5 +17,8 @@
  */
 
 export interface ApplicationListConfig {
+    /**
+     * Enable Table Headers variable.
+     */
     enableTableHeaders: boolean
 }
