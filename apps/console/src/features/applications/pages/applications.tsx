9ae50bb8490d9b4602fab0edd6686b9c3941ccef
Ravindu Perera <ravinduramesh@users.noreply.github.com>
2022-09-09 12:52:44 +0530

    fix element clickable issues in tablet devices (#3445)
    * fix element clickable issues in tablet devices
    
    * reduce search bar size more
    
    * fix comment styling issue.

--- apps/console/src/features/applications/pages/applications.tsx
+++ apps/console/src/features/applications/pages/applications.tsx
@@ -406,7 +406,7 @@ const ApplicationsPage: FunctionComponent<ApplicationsPageInterface> = (
                                     ".placeholder")
                                 }
                                 placeholder={ t("console:develop.features.applications.advancedSearch.placeholder") }
-                                style={ { minWidth: "500px" } }
+                                style={ { minWidth: "430px" } }
                                 defaultSearchAttribute="name"
                                 defaultSearchOperator="co"
                                 predefinedDefaultSearchStrategy={
@@ -470,7 +470,7 @@ const ApplicationsPage: FunctionComponent<ApplicationsPageInterface> = (
                                     placeholder={
                                         t("console:develop.features.applications.advancedSearch.placeholder")
                                     }
-                                    style={ { minWidth: "500px" } }
+                                    style={ { minWidth: "430px" } }
                                     defaultSearchAttribute="name"
                                     defaultSearchOperator="co"
                                     predefinedDefaultSearchStrategy={
