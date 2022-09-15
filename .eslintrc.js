99b420c3982a24257ed6b63854dec5e3231d280b
Brion <info@brionmario.com>
2022-09-15 12:00:56 +0530

    Add ability to validate file headers with ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +17,12 @@
  *
  */
 
+const fs = require("fs");
+const path = require("path");
+
 // Base ESLint Config which can be extended to be used in the development environment.
+
+const LICENSE_HEADER_PATTERN_OVERRIDE_FILE_NAME = "license-header-override.js";
 
 const LINE_PADDING_RULES = [
     1,
@@ -46,6 +51,57 @@ const LINE_PADDING_RULES = [
     { blankLine: "always", next: "*", prev: "import" },
     { blankLine: "any", next: "import", prev: "import" }
 ];
+
+/**
+ * Check if an override license header file is defined , if so,
+ * return that else return the default license header pattern.
+ *
+ * @example
+ * Here's a simple example of overriding the license header pattern.:
+ * ```
+ * // create a `license-header-override.js` at the same level of `.eslintrc.js`
+ * module.exports = [
+ *     " * New Company.",
+       " * Copyright 2022.",
+ * ];
+ * ```
+ *
+ * @returns License Header Pattern.
+ */
+const getLicenseHeaderPattern = () => {
+
+    const LICENSE_HEADER_DEFAULT_PATTERN = [
+        "*",
+        {
+            pattern: " Copyright \\(c\\) \\d{4}, WSO2 LLC. \\(https://www.wso2.com\\) All Rights Reserved.",
+            template: ` * Copyright (c) ${
+                new Date().getFullYear()
+            }, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.`
+        },
+        " *",
+        " * WSO2 LLC. licenses this file to you under the Apache License,",
+        " * Version 2.0 (the \"License\"); you may not use this file except",
+        " * in compliance with the License.",
+        " * You may obtain a copy of the License at",
+        " *",
+        " *     http://www.apache.org/licenses/LICENSE-2.0",
+        " *",
+        " * Unless required by applicable law or agreed to in writing,",
+        " * software distributed under the License is distributed on an",
+        " * \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY",
+        " * KIND, either express or implied. See the License for the",
+        " * specific language governing permissions and limitations",
+        " * under the License.",
+        " *",
+        " "
+    ];
+
+    if (!fs.existsSync(path.resolve(__dirname, LICENSE_HEADER_PATTERN_OVERRIDE_FILE_NAME))) {
+        return LICENSE_HEADER_DEFAULT_PATTERN;
+    }
+
+    return require(path.resolve(__dirname, LICENSE_HEADER_PATTERN_OVERRIDE_FILE_NAME));
+};
 
 module.exports = {
     env: {
@@ -133,12 +189,21 @@ module.exports = {
         ecmaVersion: 9,
         sourceType: "module"
     },
-    plugins: [ "import", "eslint-plugin-tsdoc" ],
+    plugins: [
+        "import",
+        "eslint-plugin-tsdoc",
+        "header"
+    ],
     root: true,
     rules: {
         "array-bracket-spacing": [ 1, "always" ],
         "comma-dangle": [ "warn", "never" ],
         "eol-last": "error",
+        "header/header": [
+            "warn",
+            "block",
+            getLicenseHeaderPattern()
+        ],
         "import/order": [
             "warn",
             {
