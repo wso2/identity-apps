d7d9330efe1cd566df500bcdd17fc2706c2bfe67
Brion <info@brionmario.com>
2022-09-15 12:31:03 +0530

    Remove redundant `*`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -14,7 +14,6 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
 
 const fs = require("fs");
@@ -92,7 +91,6 @@ const getLicenseHeaderPattern = () => {
         " * KIND, either express or implied. See the License for the",
         " * specific language governing permissions and limitations",
         " * under the License.",
-        " *",
         " "
     ];
 
