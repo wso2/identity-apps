e941b558d3fe4b6932fe30014ae85d944e4ffe34
Ravindu Ramesh Perera <ravinduramesh@gmail.com>
2022-09-19 11:25:55 +0530

    fix linting warnings

--- apps/console/src/features/applications/components/application-list.tsx
+++ apps/console/src/features/applications/components/application-list.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -306,7 +306,7 @@ export const ApplicationList: FunctionComponent<ApplicationListPropsInterface> =
                                 return template.id === app.templateId;
                             });
 
-                        if (relevantApplicationTemplate) {
+                        if (relevantApplicationTemplate?.templateGroup) {
                             const templateGroupId: string = relevantApplicationTemplate.templateGroup;
 
                             templateDisplayName = groupedApplicationTemplates
