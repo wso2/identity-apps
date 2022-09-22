73accd922a7906d7bcbdcf3db3b3527ecb6bf9c9
DonOmalVindula <omalvindula@live.com>
2022-09-22 11:07:52 +0530

    Add null check to localClaimDisplayName

--- apps/console/src/features/core/components/attribute-select-list.tsx
+++ apps/console/src/features/core/components/attribute-select-list.tsx
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -190,12 +190,12 @@ export const AttributeSelectList: FunctionComponent<AttributeSelectListPropsInte
                 emptyPlaceholderDefaultContent={ t("console:manage.features.transferList.list."
                             + "emptyPlaceholders.default") }
             >
-                { filterTempAvailableClaims?.map((claim, index) => {
+                { filterTempAvailableClaims?.map((claim, index) => {                    
                     return (
                         <TransferListItem
                             handleItemChange={ () => handleUnassignedItemCheckboxChange(claim) }
                             key={ claim.claimURI }
-                            listItem={ claim.localClaimDisplayName }
+                            listItem={ claim?.localClaimDisplayName ? claim.localClaimDisplayName : claim.claimURI }
                             listItemId={ claim.id }
                             listItemIndex={ claim.claimURI }
                             isItemChecked={ tempSelectedClaims.includes(claim) }
