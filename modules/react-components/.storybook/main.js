44a684e5e1792ea82431c482d927e2699822cdbb
Brion <info@brionmario.com>
2022-09-30 01:38:49 +0530

    Refactor `addon-essentials` plugin

--- modules/react-components/.storybook/main.js
+++ modules/react-components/.storybook/main.js
@@ -36,8 +36,23 @@ module.exports = {
         "../src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
     addons: [
-        "@storybook/addon-essentials",
+        "@storybook/addon-links",
         "@storybook/addon-actions",
+        "@storybook/addon-viewport",
+        {
+          name: "@storybook/addon-docs",
+          options: {
+            configureJSX: true,
+            babelOptions: {},
+            sourceLoaderOptions: null,
+            transcludeMarkdown: true,
+          },
+        },
+        "@storybook/addon-controls",
+        "@storybook/addon-backgrounds",
+        "@storybook/addon-toolbars",
+        "@storybook/addon-measure",
+        "@storybook/addon-outline",
         "@nrwl/react/plugins/storybook"
     ],
     "previewHead": (head) => (`
