53ccaadeb97f82a6ad148db5c1cee62bf245f6b3
Indeewai Wijesiri <indeewari@wso2.com>
2022-09-29 12:46:58 +0530

    Revert "Fixing formatting issues"
    This reverts commit 87d97862bd68687d149f2348770888635e95499d.

--- modules/react-components/src/components/code-editor/code-editor.tsx
+++ modules/react-components/src/components/code-editor/code-editor.tsx
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * WSO2 LLC. licenses this file to you under the Apache License,
+ * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -161,9 +161,9 @@ export interface CodeEditorContentI18nInterface {
 /**
  * Code editor component.
  *
- * @param props - Props injected to the danger zone component.
+ * @param {CodeEditorProps} props - Props injected to the danger zone component.
  *
- * return React element
+ * @return {React.ReactElement}
  */
 export const CodeEditor: FunctionComponent<CodeEditorProps> = (
     props: CodeEditorProps
@@ -291,8 +291,8 @@ export const CodeEditor: FunctionComponent<CodeEditorProps> = (
     /**
      * Resolves the language mode.
      *
-     * @param  language - Selected language.
-     * return  Resolved mode.
+     * @param {string} language - Selected language.
+     * @return {object} Resolved mode.
      */
     const resolveMode = (language: string): {
         json: boolean;
@@ -316,7 +316,7 @@ export const CodeEditor: FunctionComponent<CodeEditorProps> = (
     /**
      * Resolves the editor theme.
      *
-     * return Resolved mode.
+     * @return {"material" | "default"} Resolved mode.
      */
     const resolveTheme = (): "material" | "default" => {
         if (getThemeFromEnvironment) {
@@ -333,7 +333,7 @@ export const CodeEditor: FunctionComponent<CodeEditorProps> = (
     /**
      * Beautifies the source code.
      *
-     * return Beautified source code snippet.
+     * @return {string} Beautified source code snippet.
      */
     const beautifyCode = (): string => {
         let code = sourceCode;
@@ -394,7 +394,7 @@ export const CodeEditor: FunctionComponent<CodeEditorProps> = (
     /**
      * Render inner content.
      *
-     * return React Element
+     * @return {React.ReactElement}
      */
     const renderContent = (): ReactElement => {
 
