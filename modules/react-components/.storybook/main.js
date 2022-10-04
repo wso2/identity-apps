7a652b49ecf29c6be7f4f0defb1aff4c25de9e27
Brion <info@brionmario.com>
2022-10-04 16:50:12 +0530

    Fix ESLint issues

--- modules/react-components/.storybook/main.js
+++ modules/react-components/.storybook/main.js
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,11 +16,9 @@
  * under the License.
  */
 
-const webpack = require("webpack");
 const path = require("path");
-const fs = require("fs");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const CopyWebpackPlugin  = require("copy-webpack-plugin");
 const DeploymentConfig = require("../public/deployment.config.json");
 
 const STATIC_DIRECTORY_NAME = "public";
@@ -28,25 +26,18 @@ const UN_MINIFIED_THEME_STYLESHEET_NAME = "theme.css";
 const MINIFIED_THEME_STYLESHEET_NAME = "theme.min.css";
 
 module.exports = {
-    core: {
-        builder: "webpack5"
-    },
-    stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-actions",
         "@storybook/addon-viewport",
         {
-          name: "@storybook/addon-docs",
-          options: {
-            configureJSX: true,
-            babelOptions: {},
-            sourceLoaderOptions: null,
-            transcludeMarkdown: true,
-          },
+            name: "@storybook/addon-docs",
+            options: {
+                babelOptions: {},
+                configureJSX: true,
+                sourceLoaderOptions: null,
+                transcludeMarkdown: true
+            }
         },
         "@storybook/addon-controls",
         "@storybook/addon-backgrounds",
@@ -55,12 +46,19 @@ module.exports = {
         "@storybook/addon-outline",
         "@nrwl/react/plugins/storybook"
     ],
-    "previewHead": (head) => (`
+    core: {
+        builder: "webpack5"
+    },
+    previewHead: head => `
         ${head}
         <link rel="stylesheet" href="${RELATIVE_PATHS.storybookDefaultTheme}" />
-    `),
+    `,
     staticDirs: [ path.resolve(__dirname, "..", STATIC_DIRECTORY_NAME) ],
-    webpackFinal: async (config) => {
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    webpackFinal: async config => {
         config.plugins.push(
             new CopyWebpackPlugin({
                 patterns: [
@@ -73,13 +71,24 @@ module.exports = {
 
                             if (fileName.startsWith("theme.")) {
                                 if (fileName.endsWith(".min.css")) {
-                                    moderatedAbsoluteFileName = moderatedAbsoluteFileName.replace(fileName, MINIFIED_THEME_STYLESHEET_NAME);
+                                    moderatedAbsoluteFileName = moderatedAbsoluteFileName.replace(
+                                        fileName,
+                                        MINIFIED_THEME_STYLESHEET_NAME
+                                    );
                                 } else if (fileName.endsWith(".css")) {
-                                    moderatedAbsoluteFileName = moderatedAbsoluteFileName.replace(fileName, UN_MINIFIED_THEME_STYLESHEET_NAME);
+                                    moderatedAbsoluteFileName = moderatedAbsoluteFileName.replace(
+                                        fileName,
+                                        UN_MINIFIED_THEME_STYLESHEET_NAME
+                                    );
                                 }
                             }
 
-                            return `${ ABSOLUTE_PATHS.storybookThemes }/${path.relative(context, moderatedAbsoluteFileName)}`;
+                            return `${
+                                ABSOLUTE_PATHS.storybookThemes
+                            }/${path.relative(
+                                context,
+                                moderatedAbsoluteFileName
+                            )}`;
                         }
                     }
                 ]
@@ -89,18 +98,18 @@ module.exports = {
         config.resolve.plugins = [
             ...(config.resolve.plugins || []),
             new TsconfigPathsPlugin({
-              configFile: path.resolve(__dirname, "tsconfig.json"),
-              extensions: config.resolve.extensions,
-             })
+                configFile: path.resolve(__dirname, "tsconfig.json"),
+                extensions: config.resolve.extensions
+            })
         ];
 
         config.resolve.fallback = {
             crypto: false,
-            path: require.resolve("path-browserify"),
+            path: require.resolve("path-browserify")
         };
 
         return config;
-    },
+    }
 };
 
 /**
@@ -110,18 +119,28 @@ module.exports = {
  * @returns Set of paths relative to the root directory.
  */
 const getRelativePaths = (env, context) => {
-
     const defaultTheme = context.config.theme.name || "default";
 
     return {
         source: "src",
         staticDirectory: STATIC_DIRECTORY_NAME,
-        storybookThemes: path.join(STATIC_DIRECTORY_NAME, "themes"),
         // Default theme relative to the static directory. (public)
-        storybookDefaultTheme: path.join("themes", defaultTheme, MINIFIED_THEME_STYLESHEET_NAME),
+        storybookDefaultTheme: path.join(
+            "themes",
+            defaultTheme,
+            MINIFIED_THEME_STYLESHEET_NAME
+        ),
         storybookHelpers: "storybook-helpers",
         storybookPreviewHeader: "preview-head.html",
-        themes: path.join("node_modules", "@wso2is", "theme", "dist", "lib", "themes")
+        storybookThemes: path.join(STATIC_DIRECTORY_NAME, "themes"),
+        themes: path.join(
+            "node_modules",
+            "@wso2is",
+            "theme",
+            "dist",
+            "lib",
+            "themes"
+        )
     };
 };
 
@@ -133,7 +152,6 @@ const getRelativePaths = (env, context) => {
  * @returns Set of absolute paths.
  */
 const getAbsolutePaths = (env, context) => {
-
     const RELATIVE_PATHS = getRelativePaths(env, context);
 
     return {
@@ -145,8 +163,15 @@ const getAbsolutePaths = (env, context) => {
             RELATIVE_PATHS.source,
             RELATIVE_PATHS.storybookHelpers
         ),
-        storybookPreviewHeader: path.resolve(__dirname, RELATIVE_PATHS.storybookPreviewHeader),
-        storybookThemes: path.resolve(__dirname, "..", RELATIVE_PATHS.storybookThemes),
+        storybookPreviewHeader: path.resolve(
+            __dirname,
+            RELATIVE_PATHS.storybookPreviewHeader
+        ),
+        storybookThemes: path.resolve(
+            __dirname,
+            "..",
+            RELATIVE_PATHS.storybookThemes
+        ),
         themes: path.resolve(__dirname, "..", RELATIVE_PATHS.themes),
         tsconfig: path.resolve(__dirname, "..", "tsconfig.json")
     };
