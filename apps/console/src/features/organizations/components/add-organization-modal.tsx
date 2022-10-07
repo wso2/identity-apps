24f31b2f747a9836617af537dcd23b57eb859df2
Thivi <theviyanthan@outlook.com>
2022-10-07 11:35:08 +0530

    Update prop type of add organization modal

--- apps/console/src/features/organizations/components/add-organization-modal.tsx
+++ apps/console/src/features/organizations/components/add-organization-modal.tsx
@@ -33,7 +33,7 @@ import {
     ORGANIZATION_NAME_MIN_LENGTH,
     ORGANIZATION_TYPE
 } from "../constants";
-import { AddOrganizationInterface, OrganizationInterface } from "../models";
+import { AddOrganizationInterface, OrganizationInterface, OrganizationResponseInterface } from "../models";
 
 interface OrganizationAddFormProps {
     name: string;
@@ -46,7 +46,7 @@ interface OrganizationAddFormProps {
  */
 export interface AddOrganizationModalPropsInterface extends IdentifiableComponentInterface {
     closeWizard: () => void;
-    parent?: OrganizationInterface;
+    parent?: OrganizationInterface | OrganizationResponseInterface;
     /**
      * Callback to update the organization details.
      */
