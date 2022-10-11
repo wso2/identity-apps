8698fd6b2912a9525b87ae2fab7a9bb7cd77f5d8
Thivi <theviyanthan@outlook.com>
2022-10-11 14:52:57 +0530

    Add super organization breadcrumb method

---
+++ apps/console/src/extensions/configs/organization.tsx
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from "react";
+import { OrganizationConfigs } from "./models";
+import { BreadcrumbItem } from "../../features/organizations/models";
+
+export const organizationConfigs: OrganizationConfigs = {
+    showOrganizationDropdown: true,
+    superOrganizationBreadcrumb: (
+        breadcrumbItem: BreadcrumbItem,
+        onClick: (breadcrumbItem: BreadcrumbItem) => void
+    ) => {
+        return (
+            <span onClick={ () => onClick(breadcrumbItem) }>
+                { breadcrumbItem.name }
+            </span>
+        );
+    }
+};
