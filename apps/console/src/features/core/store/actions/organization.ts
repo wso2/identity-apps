1db5cc5c190f6c802d16e19801b21d9c4c828794
Thivi <theviyanthan@outlook.com>
2022-10-14 21:39:49 +0530

    Create a redux action to isFirstLevelOrganization

--- apps/console/src/features/core/store/actions/organization.ts
+++ apps/console/src/features/core/store/actions/organization.ts
@@ -1,24 +1,25 @@
 /**
- * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com) All Rights Reserved.
-*
-* WSO2 Inc. licenses this file to you under the Apache License,
-* Version 2.0 (the "License"); you may not use this file except
-* in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import {
     OrganizationActionTypes,
     SetGetOrganizationLoadingActionInterface,
+    SetIsFirstLevelOrganizationInterface,
     SetOrganizationActionInterface
 } from "./types";
 import { OrganizationResponseInterface } from "../../../organizations/models";
@@ -26,9 +27,9 @@ import { OrganizationResponseInterface } from "../../../organizations/models";
 /**
  * This action sets an organization in the redux store.
  *
- * @param {OrganizationInterface} organization - AN organization object
+ * @param organization - AN organization object
  *
- * @returns {SetOrganizationActionInterface} - A set organization action
+ * @returns - A set organization action
  */
 export const setOrganization = (organization: OrganizationResponseInterface): SetOrganizationActionInterface => {
     return {
@@ -40,12 +41,26 @@ export const setOrganization = (organization: OrganizationResponseInterface): Se
 /**
  * This action sets the loading state of the get organization API.
  *
- * @param {boolean} isLoading - A boolean value to set the loading state of the organization
- * @returns {SetGetOrganizationLoadingActionInterface} - A set get organization loading action
+ * @param isLoading - A boolean value to set the loading state of the organization
+ * @returns - A set get organization loading action
  */
 export const setGetOrganizationLoading = (isLoading: boolean): SetGetOrganizationLoadingActionInterface => {
     return {
         payload: isLoading,
         type: OrganizationActionTypes.SET_GET_ORGANIZATION_LOADING
+    };
+};
+
+/**
+ * This action sets if the current organization is a first level organization.
+ *
+ * @param isFirstLevel - A boolean value to set the loading state of the organization
+ *
+ * @returns - An action that sets the if it is a first level organization.
+ */
+export const setIsFirstLevelOrganization = (isFirstLevel: boolean): SetIsFirstLevelOrganizationInterface => {
+    return {
+        payload: isFirstLevel,
+        type: OrganizationActionTypes.SET_IS_FIRST_LEVEL_ORGANIZATION
     };
 };
