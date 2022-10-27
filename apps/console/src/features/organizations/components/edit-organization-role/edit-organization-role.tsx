be71835ba6671aa7127d0514f41a1a084353aaff
Thivi <theviyanthan@outlook.com>
2022-10-27 17:01:44 +0530

    Fix lint issues

--- apps/console/src/features/organizations/components/edit-organization-role/edit-organization-role.tsx
+++ apps/console/src/features/organizations/components/edit-organization-role/edit-organization-role.tsx
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com) All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -44,7 +44,7 @@ interface EditRoleProps extends SBACInterface<FeatureConfigInterface> {
 /**
  * Component which will allow editing of a selected role.
  *
- * @param props contains role details to be edited.
+ * @param props - contains role details to be edited.
  */
 export const EditOrganizationRole: FunctionComponent<EditRoleProps> = (props: EditRoleProps): ReactElement => {
 
