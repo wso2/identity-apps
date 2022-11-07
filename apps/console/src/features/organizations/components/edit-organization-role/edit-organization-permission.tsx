81488c55dd6c9c7a63575dee78e71f2b80643963
kavinda1995 <kavindajayakody@gmail.com>
2022-11-07 10:24:51 +0530

    CR Fixes for PR #3150

--- apps/console/src/features/organizations/components/edit-organization-role/edit-organization-permission.tsx
+++ apps/console/src/features/organizations/components/edit-organization-role/edit-organization-permission.tsx
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com) All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +13,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
- * under the License
+ * under the License.
  */
 
 import { AlertLevels } from "@wso2is/core/models";
@@ -50,7 +50,8 @@ interface RolePermissionDetailProps {
 
 /**
  * Component to update permissions of the selected role.
- * @param props Contains role id to get permission details.
+ *
+ * @param props - Contains role id to get permission details.
  */
 export const RolePermissionDetails: FunctionComponent<RolePermissionDetailProps> = (props:
     RolePermissionDetailProps): ReactElement => {
