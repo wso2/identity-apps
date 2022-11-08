d17e9e60732062281416b716d823f1a4a36b2558
DonOmalVindula <omalvindula@live.com>
2022-11-08 10:08:24 +0530

    Fix formatting

--- apps/console/src/protected-app.tsx
+++ apps/console/src/protected-app.tsx
@@ -624,7 +624,7 @@ export const ProtectedApp: FunctionComponent<AppPropsInterface> = (): ReactEleme
             !featureConfig.users
         ) {
             return;
-        }        
+        }
 
         const [
             devRoutes,
