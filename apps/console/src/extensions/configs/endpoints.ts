812c8e060012e7962b0b545503d5bf28c53a2cef
Shalitha Pathiranage <shali23shri@gmail.com>
2022-12-21 00:31:38 +0530

    Fix Linting Issues

--- apps/console/src/extensions/configs/endpoints.ts
+++ apps/console/src/extensions/configs/endpoints.ts
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,14 +16,14 @@
  * under the License.
  */
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ExtendedFeatureResourceEndpointsInterface } from "./models";
 import { DeploymentConfigInterface } from "../../features/core";
 
 /**
  * Get the resource endpoints for the extended features.
  *
- * @param {string} serverHost - Server Host.
- * @return {ExtendedFeatureResourceEndpointsInterface}
+ * serverHost - Server Host.
  */
 export const getExtendedFeatureResourceEndpoints = (serverHost: string, 
     deploymentConfig: DeploymentConfigInterface): ExtendedFeatureResourceEndpointsInterface => {
