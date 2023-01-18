984ce2fbfb973aaabd02b2acd887c678a9e7003c
Thivi <theviyanthan@outlook.com>
2023-01-18 19:32:31 +0530

    Add update api

--- apps/console/src/features/password-history-count/api/password-history-count.ts
+++ apps/console/src/features/password-history-count/api/password-history-count.ts
@@ -22,8 +22,17 @@ import useRequest, {
     RequestErrorInterface,
     RequestResultInterface
 } from "../../../features/core/hooks/use-request";
-import { GovernanceConnectorInterface } from "../../server-configurations";
-import { AxiosError } from "axios";
+import { GovernanceConnectorInterface, ServerConfigurationsConstants, UpdateGovernanceConnectorConfigInterface } from "../../server-configurations";
+import { AxiosError, AxiosResponse } from "axios";
+import { HttpMethods } from "@wso2is/core/models";
+import { AsgardeoSPAClient } from "@asgardeo/auth-react";
+import { IdentityAppsApiException } from "@wso2is/core/exceptions";
+
+/**
+ * Initialize an axios Http client.
+ *
+ */
+const httpClient = AsgardeoSPAClient.getInstance().httpRequest.bind(AsgardeoSPAClient.getInstance());
 
 export const useGetPasswordHistoryCount = <Data = GovernanceConnectorInterface, RequestError = RequestErrorInterface>(
 ): { data: Data, error: AxiosError<RequestError>, isLoading: boolean; } => {
@@ -32,11 +41,46 @@ export const useGetPasswordHistoryCount = <Data = GovernanceConnectorInterface, 
             "Accept": "application/json",
             "Content-Type": "application/json"
         },
-        method: "GET",
+        method: HttpMethods.GET,
         url: store.getState()?.config?.endpoints?.passwordHistory
     };
 
     const { data, error } = useRequest<Data, RequestError>(requestConfig);
 
     return { data, error, isLoading: !data && !error };
+};
+
+export const updatePasswordHistoryCount = (data: UpdateGovernanceConnectorConfigInterface): Promise<any> => {
+    const requestConfig = {
+        data,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: HttpMethods.PATCH,
+        url: store.getState()?.config?.endpoints?.passwordHistory
+    };
+
+    return httpClient(requestConfig)
+        .then((response: AxiosResponse) => {
+            if (response.status !== 200) {
+                throw new IdentityAppsApiException(
+                    "Received an invalid status code while updating the password validation.",
+                    null,
+                    response.status,
+                    response.request,
+                    response,
+                    response.config);
+            }
+
+            return Promise.resolve(response.data);
+        })
+        .catch((error) => {
+            throw new IdentityAppsApiException(
+                "An error ocurred while updating the password validation.",
+                error.stack,
+                error.code,
+                error.request,
+                error.response,
+                error.config);
+        });
 };
