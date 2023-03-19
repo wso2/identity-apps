e24958317671ab6290d26bef5be0141005962718
ThaminduDilshan <thamindudill071@gmail.com>
2023-03-19 19:16:17 +0530

    Update FIDO connector in console pages

--- apps/console/src/features/applications/components/settings/sign-on-methods/sign-in-method-landing.tsx
+++ apps/console/src/features/applications/components/settings/sign-on-methods/sign-in-method-landing.tsx
@@ -18,6 +18,7 @@
 
 import { SBACInterface, TestableComponentInterface } from "@wso2is/core/models";
 import { Heading, InfoCard, useMediaContext } from "@wso2is/react-components";
+import { identityProviderConfig } from "apps/console/src/extensions";
 import React, { FunctionComponent, ReactElement } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
@@ -217,11 +218,12 @@ export const SignInMethodLanding: FunctionComponent<SignInMethodLandingPropsInte
                                     data-testid="usernameless-flow-card"
                                     image={ getAuthenticatorIcons().fido }
                                     imageSize="mini"
-                                    header={ t(
-                                        "console:develop.features.applications.edit.sections" +
-                                            ".signOnMethod.sections.landing.flowBuilder." +
-                                            "types.usernameless.heading"
-                                    ) }
+                                    header={ 
+                                        identityProviderConfig.getOverriddenAuthenticatorDisplayName(
+                                            IdentityProviderManagementConstants.FIDO_AUTHENTICATOR_ID, 
+                                            t("console:develop.features.applications.edit.sections.signOnMethod" 
+                                                + ".sections.landing.flowBuilder.types.usernameless.heading")) 
+                                    }
                                     description={ t(
                                         "console:develop.features.applications.edit.sections" +
                                             ".signOnMethod.sections.landing.flowBuilder." +
