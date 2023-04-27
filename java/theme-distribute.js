791f7d18835802bb0f63dd21de62f1d9fe6ea077
jeradrutnam <jeradrutnam@outlook.com>
2023-04-27 10:44:47 +0530

    Fix license header lint issue in theme-distribute.js

--- java/theme-distribute.js
+++ java/theme-distribute.js
@@ -1,6 +1,5 @@
 /**
- * 
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -15,7 +14,6 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
  */
 
 const path = require("path");
