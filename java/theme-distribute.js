a858d329a21626f316362a1b63df7bf8f2810d8d
jeradrutnam <jeradrutnam@outlook.com>
2023-04-27 10:27:06 +0530

    Fix eslint rules

--- java/theme-distribute.js
+++ java/theme-distribute.js
@@ -1,23 +1,34 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://wso2.com). All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
  *
- * This software is the property of WSO2 Inc. and its suppliers, if any.
- * Dissemination of any information or reproduction of any material contained
- * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
- * You may not alter or remove any copyright or other notice from copies of this content."
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 const path = require("path");
 const fs = require("fs-extra");
 const themeFiles = path.join(__dirname, "..", "modules", "theme", "dist", "lib");
-const apps = ["authentication-portal", "recovery-portal", "x509-certificate-authentication-portal"];
+const apps = [ "authentication-portal", "recovery-portal", "x509-certificate-authentication-portal" ];
 
 apps.forEach((app) => {
     fs.copy(themeFiles, path.join(__dirname, "apps", app, "src", "main", "webapp", "libs"))
         .then(() => {
+            // eslint-disable-next-line no-console
             console.error("Theme files copied to " + app + ".");
         })
         .catch((error) => {
+            // eslint-disable-next-line no-console
             console.error(error);
         });
 });
