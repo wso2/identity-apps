6d00a2c33fd2d440955954ac48aa3122e3e641c2
Pavindu Lakshan <pavindu@wso2.com>
2023-05-02 11:16:43 +0530

    Use null over empty fragment as the placeholder value

--- apps/console/src/extensions/configs/common.tsx
+++ apps/console/src/extensions/configs/common.tsx
@@ -17,7 +17,7 @@
  */
 
 import { HeaderExtension, HeaderLinkCategoryInterface } from "@wso2is/react-components";
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import { CommonConfig } from "./models";
 import { HeaderSubPanelItemInterface } from "../../features/core/components";
 
@@ -37,7 +37,7 @@ export const commonConfig: CommonConfig = {
             _associatedTenants: any[]
         ): Promise<HeaderExtension[]> => Promise.resolve([]),
         getHeaderSubPanelExtensions: (): HeaderSubPanelItemInterface[] => [],
-        getUserDropdownFooterExtensions: (): ReactElement => <></>,
+        getUserDropdownFooterExtensions: (): ReactElement => null,
         getUserDropdownLinkExtensions: (
             _tenantDomain: string,
             _associatedTenants: any[]): Promise<HeaderLinkCategoryInterface[]> => Promise.resolve([]),
