c7e6707a0d4aee53bda860b97a96565a42d8f13c
Santhusha Mallawatantri <83592694+Santhusha-Mallawatantri@users.noreply.github.com>
2023-05-04 16:32:57 +0530

    Fix lint errors

--- apps/console/src/features/identity-providers/data/identity-provider-templates/templates/expert-mode/create-wizard-help.tsx
+++ apps/console/src/features/identity-providers/data/identity-provider-templates/templates/expert-mode/create-wizard-help.tsx
@@ -1,7 +1,7 @@
 /**
- * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.com) All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -29,7 +29,7 @@ interface ExpertModeIdPCreateWizardHelpProps {
      */
     currentStepInSidePanelGuide: number;
 }
-const ExpertModeIdPCreateWizardHelp = ({ currentStepInSidePanelGuide }: ExpertModeIdPCreateWizardHelpProps) => {
+const ExpertModeIdPCreateWizardHelp = ({ currentStepInSidePanelGuide }: ExpertModeIdPCreateWizardHelpProps):any => {
     const { t } = useTranslation();
     const [ useNewConnectionsView ] = useState<boolean>(undefined);
     const [ currentState, setCurrentState ] = useState <any>();
