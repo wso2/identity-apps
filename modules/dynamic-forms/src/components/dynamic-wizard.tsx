aafc83a297130eb956fff8b7888b5d1ac0aaaa84
NipuniBhagya <nipunib@wso2.com>
2023-08-11 11:57:05 +0530

    Resolve PR comment

--- modules/dynamic-forms/src/components/dynamic-wizard.tsx
+++ modules/dynamic-forms/src/components/dynamic-wizard.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -75,7 +75,7 @@ const ImperativeWizard = (props: DynamicWizardProps, ref: React.ForwardedRef<unk
 
     // Form reference to trigger the form submit externally
     // on each page submission.
-    const formRef: MutableRefObject<any> = useRef(null);
+    const formRef: MutableRefObject<HTMLFormElement> = useRef(null);
 
     const absoluteChildCount: number = React.Children.count(children);
     const lastPageIndex: number = absoluteChildCount - 1;
