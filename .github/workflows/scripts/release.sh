6cc5b79067d28758f4c801fc41b494e051fbb3b1
JayaShakthi97 <jayashakthiperera@gmail.com>
2023-08-21 16:59:19 +0530

    Test release workflow

--- .github/workflows/scripts/release.sh
+++ .github/workflows/scripts/release.sh
@@ -44,13 +44,11 @@ process_console_package() {
     ARTIFACT_VERSION="1.6.365.2"
     artifact="console"
 
-    STAGING_ARTIFACT_URL="$RELEASE_REPO_URL/$GROUP_ID_PATH/$artifact/$ARTIFACT_VERSION/$artifact-$ARTIFACT_VERSION.war"
+    STAGING_ARTIFACT_URL="$STAGING_REPO_URL/$GROUP_ID_PATH/$artifact/$ARTIFACT_VERSION/$artifact-$ARTIFACT_VERSION.war"
 
     echo "Uploading artifact to $STAGING_ARTIFACT_URL"
     status_code=$(curl -u "$NEXUS_USERNAME":"$NEXUS_PASSWORD" --upload-file "$artifact_path" $STAGING_ARTIFACT_URL -o /dev/null -w '%{http_code}')
     echo "HTTP Status Code: $status_code"
-
-    mvn help:effective-settings
 }
 
 process_myaccount_package() {
