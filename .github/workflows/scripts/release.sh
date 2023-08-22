102ac6b66f6d805ad0175025e9bb14e6e93a6397
JayaShakthi97 <jayashakthiperera@gmail.com>
2023-08-23 00:14:25 +0530

    [Test] Update release workflow

--- .github/workflows/scripts/release.sh
+++ .github/workflows/scripts/release.sh
@@ -15,7 +15,7 @@ process_console_package() {
     mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:perform -B --settings ~/.m2/settings.xml
 
     goToRootDirectory
-    cd java/features/org.wso2.identity.apps.console.server.feature || exit 1
+    cd "java/features/org.wso2.identity.apps.console.server.feature" || exit 1
 
     mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:prepare -B && \
     mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:perform -B --settings ~/.m2/settings.xml
