f96b1b4a0e836c5caeb83abd2ad31e0404702cee
JayaShakthi97 <jayashakthiperera@gmail.com>
2023-08-24 21:12:09 +0530

    [Test] Update release workflow

--- .github/workflows/scripts/release.sh
+++ .github/workflows/scripts/release.sh
@@ -14,6 +14,7 @@ process_console_package() {
     local console_release_version
     console_release_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | sed 's/-SNAPSHOT$//')
 
+    echo "Releasing console version: $console_release_version"
     mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:prepare -B && \
     mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:perform -B --settings ~/.m2/settings.xml && \
 
@@ -24,30 +25,48 @@ process_console_package() {
     git add pom.xml && \
     git commit -m "Update console version to $console_release_version" && \
 
+    echo "Releasing console feature version: $console_release_version"
     mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:prepare -B && \
     mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:perform -B --settings ~/.m2/settings.xml
 }
 
 process_myaccount_package() {
-    goToRootDirectory
+    goToRootDirectory && \
+    cd "apps/myaccount" || exit 1 && \
 
-    mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:prepare -pl apps/myaccount,java/features/org.wso2.identity.apps.myaccount.server.feature -B && \
-    mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:perform -pl apps/myaccount,java/features/org.wso2.identity.apps.myaccount.server.feature -B --settings ~/.m2/settings.xml
+    local myaccount_release_version
+    myaccount_release_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | sed 's/-SNAPSHOT$//')
+
+    echo "Releasing myaccount version: $myaccount_release_version"
+    mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:prepare -B && \
+    mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:perform -B --settings ~/.m2/settings.xml && \
+
+    goToRootDirectory && \
+    cd "java/features/org.wso2.identity.apps.myaccount.server.feature" || exit 1 && \
+
+    mvn versions:set-property -Dproperty=myaccount.version -DnewVersion="$myaccount_release_version" && \
+    git add pom.xml && \
+    git commit -m "Update myaccount version to $myaccount_release_version" && \
+
+    echo "Releasing myaccount feature version: $myaccount_release_version"
+    mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:prepare -B && \
+    mvn -Dresume=false -Darguments='-Dadditionalparam=-Xdoclint:none' -Dmaven.test.skip=true release:perform -B --settings ~/.m2/settings.xml
 }
 
 PACKAGES=$1
 
 if [ -z "$PACKAGES" ]; then
-    echo "No packages to be released. Exiting..."
-    exit 1
+    echo "No packages to be released. Exiting..." &&
+    exit 0
 fi
 
 echo "Releasing packages: $PACKAGES"
 
-package_list=$(echo "$PACKAGES" | tr "," " ")
+# Iterate over each package in the JSON array
+for package in $(echo "$PACKAGES" | jq -c '.[]'); do
+    name=$(echo "$package" | jq -r '.name')
 
-for package in $package_list; do
-    case "$package" in
+    case "$name" in
             "@wso2is/console")
                 process_console_package
                 ;;
@@ -55,7 +74,7 @@ for package in $package_list; do
                 process_myaccount_package
                 ;;
             *)
-                echo "Unknown package: $package"
+                echo "Irrelevent package: $package"
                 ;;
         esac
 done
