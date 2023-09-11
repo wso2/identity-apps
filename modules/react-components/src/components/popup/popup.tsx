b42398fc234eeff371cd0084b078df64fcf2a3c6
JayaShakthi97 <jayashakthiperera@gmail.com>
2023-09-11 15:33:09 +0530

    Fix typecheck issue and update PR-builder

--- modules/react-components/src/components/popup/popup.tsx
+++ modules/react-components/src/components/popup/popup.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -18,7 +18,7 @@
 
 import React, { ReactElement } from "react";
 // eslint-disable-next-line no-restricted-imports
-import { PopupProps, Popup as SemanticPopup } from "semantic-ui-react";
+import { PopupContentProps, PopupHeaderProps, PopupProps, Popup as SemanticPopup } from "semantic-ui-react";
 
 /**
  * A wrapper for the semantic Popup component.
@@ -27,7 +27,11 @@ import { PopupProps, Popup as SemanticPopup } from "semantic-ui-react";
  *
  * @returns
  */
-export const Popup = (props: PopupProps): ReactElement => {
+export const Popup: {
+    (props: PopupProps): ReactElement;
+    Content: React.FC<PopupContentProps>;
+    Header: React.FC<PopupHeaderProps>;
+} = (props: PopupProps): ReactElement => {
     return (
         <SemanticPopup
             popper={ <div style={ { filter: "none" } }></div> }
