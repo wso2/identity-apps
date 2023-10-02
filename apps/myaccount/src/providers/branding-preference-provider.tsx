7ee6440ba7639aae79b3985a1ce4f085cb9a15c9
Brion Mario <info@brionmario.com>
2023-10-02 14:28:51 +0530

    Fix lint issues

--- apps/myaccount/src/providers/branding-preference-provider.tsx
+++ apps/myaccount/src/providers/branding-preference-provider.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -25,7 +25,6 @@ import { useGetBrandingPreference } from "../api";
 import { generateAsgardeoTheme } from "../branding/theme";
 import { BrandingPreferenceContext, BrandingPreferenceContextProps } from "../contexts";
 import { BrandingPreferenceMeta } from "../meta";
-
 
 /**
  * Props interface for the BrandingPreferenceProvider.
