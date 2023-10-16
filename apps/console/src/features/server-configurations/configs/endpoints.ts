108e91eadc3426e94b5c524bbe6a5b7f4ce75877
Pavindu Lakshan <pavindulakshan@gmail.com>
2023-10-16 10:17:53 +0530

    Fix eslint warnings

--- apps/console/src/features/server-configurations/configs/endpoints.ts
+++ apps/console/src/features/server-configurations/configs/endpoints.ts
@@ -70,6 +70,6 @@ export const getServerConfigurationsResourceEndpoints = (
             ServerConfigurationsConstants.IDENTITY_GOVERNANCE_ACCOUNT_MANAGEMENT_POLICIES_ID
         }/connectors/${ServerConfigurationsConstants.SELF_SIGN_UP_CONNECTOR_ID}`,
         serverConfigurations: `${ serverHost }/api/server/v1/configs`,
-        serverSupportedSchemas: `${ serverHost }/api/server/v1/configs/schemas`,
+        serverSupportedSchemas: `${ serverHost }/api/server/v1/configs/schemas`
     };
 };
