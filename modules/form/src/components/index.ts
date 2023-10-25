1f0dca1d7db2b0921fbed4a997390083f2632430
Brion <info@brionmario.com>
2023-10-25 18:54:42 +0530

    Add `SelectFieldAdapter`

--- modules/form/src/components/index.ts
+++ modules/form/src/components/index.ts
@@ -19,6 +19,7 @@
 export * from "./adapters/__DEPRECATED__adapters";
 export * from "./adapters/text-field-adapter";
 export { default as TextFieldAdapter } from "./adapters/text-field-adapter";
+export { default as SelectFieldAdapter } from "./adapters/select-field-adapter";
 export * from "./adapters/url-field-adapter";
 export { default as URLFieldAdapter } from "./adapters/url-field-adapter";
 export * from "./adapters/checkbox-field-adapter";
