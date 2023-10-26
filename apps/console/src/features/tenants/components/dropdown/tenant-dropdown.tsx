576d68b2c118276df6e798e9fb87795456e8f459
Shenali <shenali@wso2.com>
2023-10-26 14:12:51 +0530

    Fix syntax issues

--- apps/console/src/features/tenants/components/dropdown/tenant-dropdown.tsx
+++ apps/console/src/features/tenants/components/dropdown/tenant-dropdown.tsx
@@ -17,10 +17,9 @@
  */
 
 import { AsgardeoSPAClient, DecodedIDTokenPayload } from "@asgardeo/auth-react";
-import { ArrowLeftArrowRightIcon, BuildingCircleCheckIcon, HierarchyIcon, PlusIcon } from "@oxygen-ui/react-icons";
+import { HierarchyIcon } from "@oxygen-ui/react-icons";
 import { FeatureStatus, useCheckFeatureStatus } from "@wso2is/access-control";
 import {
-    AlertInterface,
     AlertLevels,
     TenantAssociationsInterface,
     TestableComponentInterface
@@ -28,11 +27,8 @@ import {
 import { addAlert, setTenants } from "@wso2is/core/store";
 import {
     Button,
-    GenericIcon,
-    HeaderPropsInterface as ReusableHeaderPropsInterface
-} from "@wso2is/react-components";
-import { AxiosResponse } from "axios";
-import React, { FunctionComponent, ReactElement, ReactNode, SyntheticEvent, useEffect, useState } from "react";
+    GenericIcon} from "@wso2is/react-components";
+import React, { FunctionComponent, ReactElement, SyntheticEvent, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "redux";
@@ -41,12 +37,9 @@ import {
     Dropdown,
     Grid,
     Icon,
-    Input,
     Item,
     Menu,
-    Placeholder,
-    SemanticICONS
-} from "semantic-ui-react";
+    Placeholder} from "semantic-ui-react";
 import { organizationConfigs } from "../../../../extensions";
 import { 
     FeatureGateConstants 
@@ -57,32 +50,11 @@ import { history } from "../../../core/helpers/history";
 import { AppState } from "../../../core/store";
 import { OrganizationType } from "../../../organizations/constants";
 import { useGetOrganizationType } from "../../../organizations/hooks/use-get-organization-type";
-import { getAssociatedTenants, makeTenantDefault } from "../../api";
+import { getAssociatedTenants } from "../../api";
 import { TenantInfo, TenantRequestResponse, TriggerPropTypesInterface } from "../../models";
 import { handleTenantSwitch } from "../../utils";
 import { AddTenantWizard } from "../add-modal";
 
-/**
- * Dashboard layout Prop types.
- */
-interface TenantDropdownLinkInterface extends Omit<ReusableHeaderPropsInterface, "basicProfileInfo" | "profileInfo"> {
-    /**
-     * Content of dropdown item.
-     */
-    content?: string;
-    /**
-     * Name of dropdown item.
-     */
-    name: string;
-    /**
-     * Icon of dropdown item.
-     */
-    icon: SemanticICONS | ReactNode;
-    /**
-     * Function called when dropdown item is clicked.
-     */
-    onClick: () => void;
-}
 
 /**
  * Interface for tenant dropdown.
@@ -110,7 +82,6 @@ const TenantDropdown: FunctionComponent<TenantDropdownInterface> = (props: Tenan
     const [ tempTenantAssociationsList, setTempTenantAssociationsList ] = useState<string[]>(undefined);
     const [ showTenantAddModal, setShowTenantAddModal ] = useState<boolean>(false);
     const [ isSwitchTenantsSelected, setIsSwitchTenantsSelected ] = useState<boolean>(false);
-    const [ isSetDefaultTenantInProgress, setIsSetDefaultTenantInProgress ] = useState<boolean>(false);
     const [ associatedTenants, setAssociatedTenants ] = useState<string[]>([]);
     const [ defaultTenant, setDefaultTenant ] = useState<string>("");
     const [ isDropDownOpen, setIsDropDownOpen ] = useState<boolean>(false);
@@ -280,64 +251,6 @@ const TenantDropdown: FunctionComponent<TenantDropdownInterface> = (props: Tenan
         }
     };
 
-    const tenantDropdownLinks: TenantDropdownLinkInterface[] = [
-        {
-            icon: <PlusIcon fill="black" />,
-            name: t("extensions:manage.features.tenant.header.tenantAddHeader"),
-            onClick: () => { setShowTenantAddModal(true); }
-        }
-    ];
-
-    const setDefaultTenantInDropdown = (tenantName: string): void => {
-        setIsSetDefaultTenantInProgress(true);
-        makeTenantDefault(tenantName)
-            .then((response: AxiosResponse) => {
-                if (response.status === 200) {
-                    dispatch(addAlert<AlertInterface>({
-                        description: t("extensions:manage.features.tenant.notifications.defaultTenant.success." +
-                            "description", { tenantName: tenantName }),
-                        level: AlertLevels.SUCCESS,
-                        message: t("extensions:manage.features.tenant.notifications.defaultTenant.success.message")
-                    }));
-
-                    setDefaultTenant(tenantName);
-                }
-            })
-            .catch(() => {
-                dispatch(addAlert<AlertInterface>({
-                    description:
-                    t("extensions:manage.features.tenant.notifications.defaultTenant.genericError.description"),
-                    level: AlertLevels.ERROR,
-                    message: t("extensions:manage.features.tenant.notifications.defaultTenant.genericError.message")
-                }));
-            })
-            .finally(() => {
-                setIsSetDefaultTenantInProgress(false);
-                setIsDropDownOpen(!isDropDownOpen);
-            });
-    };
-
-    /**
-     * Search the tenant list.
-     *
-     * @param event - Input event
-     */
-    const searchTenantList = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        const changeValue: string = event.target.value;
-
-        if (tenantAssociations && Array.isArray(tenantAssociations.associatedTenants)) {
-            let result: string | string[];
-
-            if (changeValue.length > 0) {
-                result = tenantAssociations.associatedTenants.filter((item: string) =>
-                    item.toLowerCase().indexOf(changeValue.toLowerCase()) !== -1);
-            } else {
-                result = tenantAssociations.associatedTenants;
-            }
-            setTempTenantAssociationsList(result);
-        }
-    };
-
     /**
      * Resets the dropdown states.
      */
@@ -467,39 +380,40 @@ const TenantDropdown: FunctionComponent<TenantDropdownInterface> = (props: Tenan
                                                         content="Primary organizations"
                                                     />
                                                 )
-                                        }
-                                        {
-                                            !isSubOrg && (
-                                                <>
-                                                    <Divider />
-                                                    <Dropdown.Item
-                                                        className="action-panel"
-                                                        onClick={ handleOrganizationsPageRoute }
-                                                        data-componentid={ "sub-organizations-menu" }
-                                                    >
-                                                        <HierarchyIcon fill="black" />
+                                            }
+                                            {
+                                                !isSubOrg && (
+                                                    <>
+                                                        <Divider />
+                                                        <Dropdown.Item
+                                                            className="action-panel"
+                                                            onClick={ handleOrganizationsPageRoute }
+                                                            data-componentid={ "sub-organizations-menu" }
+                                                        >
+                                                            <HierarchyIcon fill="black" />
                                                         Organizations
-                                                    </Dropdown.Item>
-                                                </>
-                                            )
-                                        }
-                                    </>
-                                )
-                            }
-                        </Dropdown.Menu>
-                    ) : (
-                        <Dropdown.Menu onClick={ handleDropdownClick }>
-                            <Item.Group className="current-tenant" unstackable>
-                                <Item
-                                    className="header back-button-wrapper"
-                                    key={ "current-tenant" }
-                                >
-                                    <div className="link pointing" onClick={ resetTenantDropdown }>
-                                        <Icon
-                                            className="link-icon spaced-right"
-                                            name="arrow left"
-                                        />
-                                    </div>
+                                                        </Dropdown.Item>
+                                                    </>
+                                                )
+                                            }
+                                        </>
+                                    )
+                                }
+                            </Dropdown.Menu>
+                        ) : (
+                            <Dropdown.Menu onClick={ handleDropdownClick }>
+                                <Item.Group className="current-tenant" unstackable>
+                                    <Item
+                                        className="header back-button-wrapper"
+                                        key={ "current-tenant" }
+                                    >
+                                        <div className="link pointing" onClick={ resetTenantDropdown }>
+                                            <Icon
+                                                className="link-icon spaced-right"
+                                                name="arrow left"
+                                            />
+                                        </div>
+                                    </Item>
                                 </Item.Group>
                                 {
                                     tenantAssociations
