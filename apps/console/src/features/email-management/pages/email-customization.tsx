50c22e5e903ae00f1e678ea62b29c0d16629777d
asha15 <165079T@uom.lk>
2023-10-26 16:48:27 +0530

    remove filtering from availableEmailTemplates

--- apps/console/src/features/email-management/pages/email-customization.tsx
+++ apps/console/src/features/email-management/pages/email-customization.tsx
@@ -94,26 +94,23 @@ const EmailCustomizationPage: FunctionComponent<EmailCustomizationPageInterface>
     } = useEmailTemplate(selectedEmailTemplateId, selectedLocale);
 
     useEffect(() => {
-        // As of now, we need to show only the used email templates, and we don't have a good displayName and
-        // description coming from the backend for the email template types. So as we agreed to filter only the used
-        // templates, and we will use the displayName and description from the email template types config defined in
-        // the deployment.toml file. The below code will first filter all the email templates and map the email template
+        // we don't have a good displayName and description coming from the backend
+        // for the email template types. So as we agreed use the displayName and
+        // description from the email template types config defined in
+        // the deployment.toml file. The below code will map the email template
         // types with the config's displayName and description.
-        const availableEmailTemplates: EmailTemplateType[] = emailTemplatesList?.filter(
-            (template: EmailTemplateType) => {
-                return emailTemplates?.find((emailTemplate: Record<string, string>) =>
-                    emailTemplate.id === template.id);
-            })
-            .map((template: EmailTemplateType) => {
-                const mappedTemplate: Record<string, string> = emailTemplates
-                    ?.find((emailTemplate: Record<string, string>) => emailTemplate.id === template.id);
+        const availableEmailTemplates: EmailTemplateType[] = emailTemplatesList
+          ? emailTemplatesList.map((template: EmailTemplateType) => {
+              const mappedTemplate: Record<string, string> | undefined = emailTemplates
+                ?.find((emailTemplate: Record<string, string>) => emailTemplate.id === template.id);
 
-                return {
-                    ...template,
-                    description: mappedTemplate?.description || `${ template.displayName } Template`,
-                    displayName: mappedTemplate?.displayName || template.displayName
-                };
-            });
+              return {
+                ...template,
+                description: mappedTemplate?.description || `${template.displayName} Template`,
+                displayName: mappedTemplate?.displayName || template.displayName,
+              };
+            })
+          : [];
 
         setAvailableEmailTemplatesList(availableEmailTemplates);
 
