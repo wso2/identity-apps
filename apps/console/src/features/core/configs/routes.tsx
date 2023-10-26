18595799f957d289480ce3e2789b08e26c4ff0b4
savindi7 <savindi@wso2.com>
2023-10-26 19:51:05 +0530

    sort other settings to the end of the list

--- apps/console/src/features/core/configs/routes.tsx
+++ apps/console/src/features/core/configs/routes.tsx
@@ -19,11 +19,13 @@
 import {
     ArrowRightToBracketPencilIcon,
     BoltIcon,
+    BuildingIcon,
     EnvelopeGearIcon,
     EnvelopeIcon,
-    HierarchyIcon,
+    EnvelopeMagnifyingGlassIcon,
     LightbulbOnIcon,
     LinearNodesIcon,
+    NodesIcon,
     UserCircleDotIcon,
     UserGroupIcon
 } from "@oxygen-ui/react-icons";
@@ -203,9 +205,7 @@ export const getAppViewRoutes = (useExtendedRoutes: boolean = false): RouteInter
                         component: lazy(() => import("../../connections/pages/connections")),
                         exact: true,
                         icon: {
-                            icon: identityProviderConfig?.useNewConnectionsView
-                                ? getSidePanelIcons().connections
-                                : getSidePanelIcons().identityProviders
+                            icon: <NodesIcon />
                         },
                         id: "identityProviders",
                         name: identityProviderConfig?.useNewConnectionsView
@@ -374,7 +374,7 @@ export const getAppViewRoutes = (useExtendedRoutes: boolean = false): RouteInter
                         component: lazy(() => import("../../../features/organizations/pages/organizations")),
                         exact: true,
                         icon: {
-                            icon: <HierarchyIcon fill="black" />
+                            icon: <BuildingIcon />
                         },
                         id: "organizations",
                         name: "console:manage.features.sidePanel.organizations",
@@ -422,7 +422,7 @@ export const getAppViewRoutes = (useExtendedRoutes: boolean = false): RouteInter
                         }),
                         exact: true,
                         icon: {
-                            icon: <HierarchyIcon fill="black" />
+                            icon: <EnvelopeMagnifyingGlassIcon />
                         },
                         id: "organizationDiscovery",
                         name: "console:manage.features.sidePanel.emailDomainDiscovery",
