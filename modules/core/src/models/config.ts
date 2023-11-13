0c624cc654588ca1a98a321580e7b54db32562a7
Nipun Thennakoon <nipunsampath@gmail.com>
2023-11-13 08:23:28 +0530

    Improve comments.

--- modules/core/src/models/config.ts
+++ modules/core/src/models/config.ts
@@ -187,7 +187,7 @@ export interface CommonUIConfigInterface<T = Record<string, unknown>> {
      */
     isLeftNavigationCategorized?: boolean;
     /**
-    * Flags to check whether the password validation is performed using input validation listener.
+    * Flag to check whether the password validation is performed using input validation listener.
     */
     isPasswordInputValidationEnabled: boolean;
     /**
