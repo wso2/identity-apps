5ac5bdef611ecdc39d338d1eedfe03c0eb4f5358
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2023-11-14 12:17:39 +0530

    (fix) Configuration flow for email magic link not redirecting propoerly

--- apps/console/src/extensions/configs/application.tsx
+++ apps/console/src/extensions/configs/application.tsx
@@ -356,16 +356,12 @@ export const applicationConfig: ApplicationConfig = {
         },
         getStrongAuthenticationFlowTabIndex: (
             clientId: string,
-            tenantDomain: string,
-            templateId: string,
-            customApplicationTemplateId: string
+            tenantDomain: string
         ): number => {
             if (clientId === getTryItClientId(tenantDomain)) {
                 return 2; // For Asgardeo Try It App
-            } else if (templateId === customApplicationTemplateId) {
-                return 3; // For other apps built on Custom Application Templates
             } else {
-                return 4; // Anything else
+                return 3; // For other applications
             }
         },
         getTabExtensions: (
