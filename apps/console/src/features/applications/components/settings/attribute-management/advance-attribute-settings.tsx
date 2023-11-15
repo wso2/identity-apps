4dbdd62d9e274a5d7290ff22aa078349024ac86f
Rivindu <rivindum.15@cse.mrt.ac.lk>
2023-11-15 15:01:32 +0530

    Update subject type hint

--- apps/console/src/features/applications/components/settings/attribute-management/advance-attribute-settings.tsx
+++ apps/console/src/features/applications/components/settings/attribute-management/advance-attribute-settings.tsx
@@ -447,19 +447,17 @@ export const AdvanceAttributeSettings: FunctionComponent<AdvanceAttributeSetting
                                                 ariaLabel={ `Subject type ${subjectType}` }
                                                 name={ "subjectType" }
                                                 value={ subjectType }
-                                                label={ subjectType }
+                                                label={ t("console:develop.features.applications.forms" +
+                                                     ".advancedAttributeSettings.sections.subject.fields" +
+                                                     ".subjectType." + subjectType + ".label") }
+                                                hint={ subjectType === SubjectTypes.PAIRWISE &&
+                                                        t("console:develop.features.applications.forms" +
+                                                       ".advancedAttributeSettings.sections.subject.fields" +
+                                                       ".subjectType." + subjectType + ".hint") }
                                                 checked={ selectedSubjectType === subjectType }
                                                 listen={ () => {
                                                     setSelectedSubjectType(subjectType);
                                                 } }
-                                            />
-                                            <Message
-                                                type="info"
-                                                content={
-                                                    t("console:develop.features.applications.forms" +
-                                                        ".advancedAttributeSettings.sections.subject.fields" +
-                                                        ".subjectType." + subjectType + ".hint")
-                                                }
                                             />
                                         </>
                                     );
