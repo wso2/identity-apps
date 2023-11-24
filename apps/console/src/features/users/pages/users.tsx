029f78fb4adc2e0d1a10dd5c2f68dbbd5f08e826
Pavindu Lakshan <pavindulakshan@gmail.com>
2023-11-24 13:03:03 +0530

    Exclude groups and roles from the user list request in Users page

--- apps/console/src/features/users/pages/users.tsx
+++ apps/console/src/features/users/pages/users.tsx
@@ -338,7 +338,7 @@ const UsersPage: FunctionComponent<UsersPageInterface> = (
 
         const modifiedLimit : number = limit + TEMP_RESOURCE_LIST_ITEM_LIMIT_OFFSET;
 
-        getUsersList(modifiedLimit, offset, filter, attribute, domain)
+        getUsersList(modifiedLimit, offset, filter, attribute, domain, "groups,roles")
             .then((response: UserListInterface) => {
                 const data: UserListInterface = { ...response };
 
