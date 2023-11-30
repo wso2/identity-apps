e9a40eb896d5562e58cf7851983c06456ed14c26
Rashmini <rashmini.dkn@gmail.com>
2023-11-30 09:21:10 +0530

    Update translations

--- modules/i18n/src/translations/si-LK/portals/console.ts
+++ modules/i18n/src/translations/si-LK/portals/console.ts
@@ -8401,7 +8401,7 @@ export const console: ConsoleNS = {
                                 placeholder: "Enter {{type}} නම ඇතුළත් කරන්න",
                                 validations: {
                                     duplicate: "දී ඇති {{type}} නම සමඟ {{type}} දැනටමත් පවතී.",
-                                    duplicateInAudience: "",
+                                    duplicateInAudience: "තෝරාගත් ප්‍රේක්ෂකයින් තුළ මෙම නම සහිත භූමිකාවක් දැනටමත් පවතී.",
                                     empty: "ඉදිරියට යාමට {{type}} නම අවශ්‍ය වේ.",
                                     invalid: "{{type}} නමක අඩංගු විය හැක්කේ අක්ෂර සංඛ්‍යා, -, සහ _ පමණි. "
                                         + "අක්ෂර 3 ත් 30 ත් අතර දිගකින් යුක්ත විය යුතුය."
