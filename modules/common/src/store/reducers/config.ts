7cb96b24d3ef7f8c7517f6a1332f1f4bc36e43ca
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2023-11-30 18:51:27 +0530

    (fix) revert changes to the config.ts file

--- modules/common/src/store/reducers/config.ts
+++ modules/common/src/store/reducers/config.ts
@@ -1,71 +1,86 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 LLC. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
 /**
  * Initial state for the common config reducer.
  */
 export const commonConfigReducerInitialState = {
-        deployment: {
-            accountApp: {
-                basePath: "",
-                displayName: "",
-                path: "",
-                tenantQualifiedPath: ""
-            },
-            adminApp: {
-                basePath: "",
-                displayName: "",
-                path: "",
-                tenantQualifiedPath: ""
-            },
-            allowMultipleAppProtocols: undefined,
-            appBaseName: "",
-            appBaseNameWithoutTenant: "",
-            appHomePath: "",
-            appLoginPath: "",
-            appLogoutPath: "",
-            clientHost: "",
-            clientID: "",
-            clientOrigin: "",
-            customServerHost: "",
-            developerApp: {
-                basePath: "",
-                displayName: "",
-                path: "",
-                tenantQualifiedPath: ""
-            },
-            docSiteURL: "",
-            documentation: null,
-            extensions: null,
-            helpCenterURL: "",
-            idpConfigs: null,
-            loginCallbackUrl: "",
-            serverHost: "",
-            serverOrigin: "",
-            superTenant: "",
-            tenant: "",
-            tenantPath: "",
-            tenantPrefix: ""
+
+    deployment: {
+        accountApp: {
+            basePath: "",
+            displayName: "",
+            path: "",
+            tenantQualifiedPath: ""
         },
-        endpoints: {
-            CORSOrigins: "",
-            identityProviders: ""
+        adminApp: {
+            basePath: "",
+            displayName: "",
+            path: "",
+            tenantQualifiedPath: ""
         },
+        allowMultipleAppProtocols: undefined,
+        appBaseName: "",
+        appBaseNameWithoutTenant: "",
+        appHomePath: "",
+        appLoginPath: "",
+        appLogoutPath: "",
+        clientHost: "",
+        clientID: "",
+        clientOrigin: "",
+        customServerHost: "",
+        developerApp: {
+            basePath: "",
+            displayName: "",
+            path: "",
+            tenantQualifiedPath: ""
+        },
+        docSiteURL: "",
+        documentation: null,
+        extensions: null,
+        helpCenterURL: "",
+        idpConfigs: null,
+        loginCallbackUrl: "",
+        serverHost: "",
+        serverOrigin: "",
+        superTenant: "",
+        tenant: "",
+        tenantPath: "",
+        tenantPrefix: ""
+    },
+    endpoints: {
+        CORSOrigins: "",
+        identityProviders: ""
+    },
+    features: {
+        applications: null,
+        approvals: null,
+        attributeDialects: null,
+        certificates: null,
+        emailTemplates: null,
+        governanceConnectors: null,
+        groups: null,
+        guestUser: null,
+        identityProviders: null,
+        oidcScopes: null,
+        remoteFetchConfig: null,
+        roles: null,
+        secretsManagement: null,
+        userStores: null,
+        users: null
+    },
+    i18n: null,
+    ui: {
+        announcements: [],
+        appCopyright: "",
+        appName: "",
+        appTitle: "",
         features: {
             applications: null,
             approvals: null,
@@ -83,84 +98,61 @@ export const commonConfigReducerInitialState = {
             userStores: null,
             users: null
         },
-        i18n: null,
-        ui: {
-            announcements: [],
-            appCopyright: "",
-            appName: "",
-            appTitle: "",
-            features: {
-                applications: null,
-                approvals: null,
-                attributeDialects: null,
-                certificates: null,
-                emailTemplates: null,
-                governanceConnectors: null,
-                groups: null,
-                guestUser: null,
-                identityProviders: null,
-                oidcScopes: null,
-                remoteFetchConfig: null,
-                roles: null,
-                secretsManagement: null,
-                userStores: null,
-                users: null
+        googleOneTapEnabledTenants: [],
+        gravatarConfig: {
+            defaultImage: "",
+            fallback: null,
+            size: null
+        },
+        hiddenAuthenticators: [],
+        hiddenConnectionTemplates: [],
+        hiddenUserStores: [],
+        i18nConfigs: null,
+        identityProviderTemplates: {
+            apple: {
+                enabled: false
             },
-            googleOneTapEnabledTenants: [],
-            gravatarConfig: {
-                defaultImage: "",
-                fallback: null,
-                size: null
+            enterpriseOIDC: {
+                enabled: false
             },
-            hiddenAuthenticators: [],
-            hiddenConnectionTemplates: [],
-            hiddenUserStores: [],
-            i18nConfigs: null,
-            identityProviderTemplates: {
-                apple: {
-                    enabled: false
-                },
-                enterpriseOIDC: {
-                    enabled: false
-                },
-                enterpriseSAML: {
-                    enabled: false
-                },
-                facebook: {
-                    enabled: false
-                },
-                github: {
-                    enabled: false
-                },
-                google: {
-                    enabled: false
-                },
-                microsoft: {
-                    enabled: false
-                }
+            enterpriseSAML: {
+                enabled: false
             },
-            connectionResourcesUrl: "",
-            isClientSecretHashEnabled: undefined,
-            isCookieConsentBannerEnabled: undefined,
-            isDefaultDialectEditingEnabled: undefined,
-            isDialectAddingEnabled: undefined,
-            isGroupAndRoleSeparationEnabled: undefined,
-            isHeaderAvatarLabelAllowed: undefined,
-            isLeftNavigationCategorized: undefined,
-            isMarketingConsentBannerEnabled: undefined,
-            isRequestPathAuthenticationEnabled: undefined,
-            isSignatureValidationCertificateAliasEnabled: undefined,
-            listAllAttributeDialects: undefined,
-            privacyPolicyConfigs: null,
-            productName: "",
-            productVersionConfig: null,
-            selfAppIdentifier: "",
-            showAppSwitchButton: undefined,
-            systemAppsIdentifiers: [],
-            theme: {
-                name: "",
-                path: "",
-                styleSheets: null
+            facebook: {
+                enabled: false
+            },
+            github: {
+                enabled: false
+            },
+            google: {
+                enabled: false
+            },
+            microsoft: {
+                enabled: false
             }
+        },
+        connectionResourcesUrl: "",
+        isClientSecretHashEnabled: undefined,
+        isCookieConsentBannerEnabled: undefined,
+        isDefaultDialectEditingEnabled: undefined,
+        isDialectAddingEnabled: undefined,
+        isGroupAndRoleSeparationEnabled: undefined,
+        isHeaderAvatarLabelAllowed: undefined,
+        isLeftNavigationCategorized: undefined,
+        isMarketingConsentBannerEnabled: undefined,
+        isRequestPathAuthenticationEnabled: undefined,
+        isSignatureValidationCertificateAliasEnabled: undefined,
+        listAllAttributeDialects: undefined,
+        privacyPolicyConfigs: null,
+        productName: "",
+        productVersionConfig: null,
+        selfAppIdentifier: "",
+        showAppSwitchButton: undefined,
+        systemAppsIdentifiers: [],
+        theme: {
+            name: "",
+            path: "",
+            styleSheets: null
         }
-    };
+    }
+};
