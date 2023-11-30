4aa3a01c92fcf224a6e07a8526377c9b1a2fdccc
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2023-11-30 19:00:59 +0530

    (fix) Fix es lint issues

--- modules/core/src/models/config.ts
+++ modules/core/src/models/config.ts
@@ -352,12 +352,7 @@ export interface FeatureAccessConfigInterface {
     /**
      * Enable the tour option
      */
-    tryittourenabled?: boolean; 
-}
-
-interface DeprecatedFeature {
-    name: string;
-    deprecatedProperties: string[];
+    tryittourenabled?: boolean;
 }
 
 export interface DeprecatedFeatureInterface {
