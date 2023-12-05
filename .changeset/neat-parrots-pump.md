61b957e893b40f14b7da3849a6a0a49d91466160
DonOmalVindula <omalvindula@live.com>
2023-12-05 13:24:48 +0530

    Add changeset

---
+++ .changeset/neat-parrots-pump.md
@@ -0,0 +1,5 @@
+---
+"@wso2is/console": patch
+---
+
+Fix issues in the api resource dropdown
--- apps/console/src/features/api-resources/constants/api-resources-constants.ts
+++ apps/console/src/features/api-resources/constants/api-resources-constants.ts
@@ -73,7 +73,7 @@ export class APIResourcesConstants {
 export enum APIResourceType {
     MANAGEMENT = "management",
     ORGANIZATION = "organization",
-    CUSTOM = "custom"
+    BUSINESS = "business"
 }
 
 /*
