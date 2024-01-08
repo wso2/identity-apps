f27ea8a02ed8a10d64eee3bd3ced59e07c6484e7
Brion <info@brionmario.com>
2024-01-08 12:26:00 +0530

    Fix type issues

--- modules/common/src/configs/app.ts
+++ modules/common/src/configs/app.ts
@@ -1,10 +1,19 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
  *
- * This software is the property of WSO2 LLC. and its suppliers, if any.
- * Dissemination of any information or reproduction of any material contained
- * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
- * You may not alter or remove any copyright or other notice from copies of this content.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import { DocumentationConstants } from "@wso2is/core/constants";
@@ -30,7 +39,7 @@ export class AppConfig {
      *
      * @returns Server host.
      */
-    public static resolveServerHost(enforceOrgPath?: boolean): string {
+    public static resolveServerHost(_enforceOrgPath?: boolean): string {
         return window[ "AppUtils" ]?.getConfig()?.serverOriginWithTenant;
     }
 
@@ -76,6 +85,7 @@ export class AppConfig {
             extensions: window[ "AppUtils" ]?.getConfig()?.extensions,
             idpConfigs: window[ "AppUtils" ]?.getConfig()?.idpConfigs,
             loginCallbackUrl: window[ "AppUtils" ]?.getConfig()?.loginCallbackURL,
+            organizationPrefix: window["AppUtils"]?.getConfig()?.organizationPrefix,
             serverHost: window[ "AppUtils" ]?.getConfig()?.serverOriginWithTenant,
             serverOrigin: window[ "AppUtils" ]?.getConfig()?.serverOrigin,
             superTenant: window[ "AppUtils" ]?.getConfig()?.superTenant,
