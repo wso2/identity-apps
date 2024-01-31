3e304b1f4177c99ea05c223b9c7238b5d3d707d5
Dilshan Senarath <dilshan99news@gmail.com>
2024-01-31 10:35:25 +0530

    refactor the code

--- apps/console/src/features/console-settings/components/console-roles/console-roles-edit/console-roles-edit.tsx
+++ apps/console/src/features/console-settings/components/console-roles/console-roles-edit/console-roles-edit.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2023-2024, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -89,7 +89,7 @@ const ConsoleRolesEdit: FunctionComponent<ConsoleRolesEditPropsInterface> = (
     useEffect(() => {
         if(roleObject) {
             setIsAdminRole(roleObject.displayName === RoleConstants.ADMIN_ROLE ||
-                roleObject.displayName === RoleConstants.ADMIN_GROUP ||
+                roleObject?.displayName === RoleConstants.ADMIN_GROUP ||
                 roleObject?.displayName === administratorRoleDisplayName);
         }
     }, [ roleObject ]);
