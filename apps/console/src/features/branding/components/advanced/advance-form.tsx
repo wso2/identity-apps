72ff1d82a3026daad4df90f5686292b721900dbd
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2024-03-04 10:27:15 +0530

    [chore] address PR comments

--- apps/console/src/features/branding/components/advanced/advance-form.tsx
+++ apps/console/src/features/branding/components/advanced/advance-form.tsx
@@ -156,7 +156,7 @@ export const AdvanceForm: FunctionComponent<AdvanceFormPropsInterface> = forward
         // Use a regex to replace {{lang}}, {{country}}, and {{locale}} placeholders while preserving other characters
         moderatedValue = value?.trim().replace(new RegExp(placeholdersPattern, "g"), "");
 
-        if (URLUtils.isURLValid(moderatedValue) || !FormValidation.url(moderatedValue)) {
+        if (!URLUtils.isURLValid(moderatedValue, true) || !FormValidation.url(moderatedValue)) {
             errorMsg = t("extensions:develop.branding.forms.advance.links.fields.common.validations.invalid");
         }
 
