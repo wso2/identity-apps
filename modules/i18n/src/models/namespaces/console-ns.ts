f1d90f5f02d4f74a326bc385429c290694ca123e
jathushan-r <jathu292@gmail.com>
2024-03-06 22:35:58 +0530

    Modify idvp in i18n namespaces

--- modules/i18n/src/models/namespaces/console-ns.ts
+++ modules/i18n/src/models/namespaces/console-ns.ts
@@ -3004,24 +3004,150 @@ export interface ConsoleNS {
                     addIDVP: string;
                 };
                 placeholders: {
-                    emptyIDVPList: Placeholder;
-                    emptyIDVPTypeList: Placeholder;
+                    emptyIDVPList: {
+                        title: string;
+                        subtitles:
+                            | string
+                            | {
+                                  [key: number]: string;
+                              };
+                    };
+                    emptyIDVPTypeList: {
+                        title: string;
+                        subtitles:
+                            | string
+                            | {
+                                  [key: number]: string;
+                              };
+                    };
                 };
                 confirmations: {
-                    deleteIDVP: Confirmation;
+                    deleteIDVP: {
+                        assertionHint: string;
+                        header: string;
+                        message: string;
+                        content: string;
+                    };
                 };
                 notifications: {
-                  getIDVPList: Notification;
-                  deleteIDVP: Notification;
-                  updateIDVP: Notification;
-                  addIDVP: Notification;
-                  submitAttributeSettings: Notification;
-                  getAllLocalClaims: Notification;
-                  getIDVP: Notification;
-                  getUIMetadata: Notification;
-                  getIDVPTemplateTypes: Notification;
-                  getIDVPTemplateType: Notification;
-                  getIDVPTemplate: Notification;
+                    getIDVPList: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    deleteIDVP: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                        success: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    updateIDVP: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                        success: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    addIDVP: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                        success: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    submitAttributeSettings: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    getAllLocalClaims: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    getIDVP: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    getUIMetadata: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    getIDVPTemplateTypes: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    getIDVPTemplateType: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                    };
+                    getIDVPTemplate: {
+                        error: {
+                            message: string;
+                            description: string;
+                        };
+                        genericError: {
+                            message: string;
+                            description: string;
+                        };
+                    };
                 };
                 forms: {
                     attributeSettings: {
@@ -3051,21 +3177,45 @@ export interface ConsoleNS {
                             labels: {
                                 mappedValue: string;
                                 localClaim: string;
-                            }
+                            };
                         };
                         attributeSelectionModal: {
                             header: string;
                         };
                     };
                     generalDetails: {
-                        name: FormAttributes;
-                        description: FormAttributes;
+                        name: {
+                            hint: string;
+                            label: string | Record<string, unknown>;
+                            placeholder: string;
+                            validations: {
+                                empty: string;
+                                duplicate: string;
+                                invalid: string;
+                                required: string;
+                                maxLengthReached: string;
+                            };
+                        };
+                        description: {
+                            hint: string;
+                            label: string | Record<string, unknown>;
+                            placeholder: string;
+                        };
                     };
                 };
-                dangerZoneGroup?: {
+                dangerZoneGroup: {
                     header: string;
-                    disableIDVP: DangerZone;
-                    deleteIDVP: DangerZone;
+                    disableIDVP: {
+                        actionTitle: string;
+                        header: string;
+                        subheader: string;
+                        subheader2?: string;
+                    };
+                    deleteIDVP: {
+                        actionTitle: string;
+                        header: string;
+                        subheader: string;
+                    };
                 };
                 list: {
                     actions: string;
