aa53deae46c79a11686cce741664f14c843c97e5
jathushan-r <jathu292@gmail.com>
2024-03-25 10:01:30 +0530

    add consoleSettings in i18n constants

--- modules/i18n/src/translations/en-US/meta.ts
+++ modules/i18n/src/translations/en-US/meta.ts
@@ -28,6 +28,7 @@ export const meta: LocaleMeta = {
         I18nModuleConstants.CONSOLE_PORTAL_NAMESPACE,
         I18nModuleConstants.MY_ACCOUNT_NAMESPACE,
         I18nModuleConstants.EXTENSIONS_NAMESPACE,
-        "consoleSettings"
+        I18nModuleConstants.CONSOLE_SETTINGS_NAMESPACE,
+        I18nModuleConstants.FEATURE_GATE_NAMESPACE
     ]
 };
