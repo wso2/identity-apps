c333d9af24a6e3d1de532a7267d030bff44aca1f
DonOmalVindula <omalvindula@live.com>
2024-04-15 17:16:33 +0530

    Fix build failure

--- features/admin.api-resources.v2/components/api-resource-panes/general-api-resource.tsx
+++ features/admin.api-resources.v2/components/api-resource-panes/general-api-resource.tsx
@@ -33,7 +33,7 @@ import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "redux";
 import { Divider } from "semantic-ui-react";
-import { AppState, FeatureConfigInterface, history } from "../../../../../admin.core.v1";
+import { AppState, FeatureConfigInterface, history } from "../../../admin.core.v1";
 import { deleteAPIResource } from "../../api/api-resources";
 import { APIResourcesConstants } from "../../constants/api-resources-constants";
 import {
