ccd66f0bead4f15582a3e48f265bbc6e533312ab
asha15 <165079T@uom.lk>
2024-04-16 12:44:00 +0530

    fix import library path

--- features/admin.extensions.v1/application-templates/templates/m2m-application/__tests__/quick-start.test.tsx
+++ features/admin.extensions.v1/application-templates/templates/m2m-application/__tests__/quick-start.test.tsx
@@ -16,9 +16,8 @@
  * under the License.
  */
 
-import { render, screen } from "@testing-library/react";
 import React from "react";
-import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "../../../../../test-configs/utils";
 import M2MApplicationQuickStart, { M2MApplicationQuickStartPropsInterface }  from "../quick-start";
 
 describe("M2MApplicationQuickStart", () => {
