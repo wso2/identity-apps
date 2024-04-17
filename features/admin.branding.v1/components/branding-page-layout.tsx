8ddbe6ca414ae3115eb292f12e338ccfc96bc530
Chamath Samarawickrama <chamaths@wso2.com>
2024-04-17 12:37:17 +0530

    fix import order issue

--- features/admin.branding.v1/components/branding-page-layout.tsx
+++ features/admin.branding.v1/components/branding-page-layout.tsx
@@ -18,13 +18,13 @@
 
 import { IdentifiableComponentInterface } from "@wso2is/core/models";
 import { DocumentationLink, PageLayout, useDocumentation } from "@wso2is/react-components";
-import useAIBrandingPreference from "../../admin.ai.v1/hooks/use-ai-branding-preference";
 import React, { FunctionComponent, ReactElement, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import BrandingCore from "./branding-core";
 import { BrandingAIBanner } from "../../admin.ai.v1/components/branding-ai-banner";
 import { LoadingScreen } from "../../admin.ai.v1/components/branding-ai-loading-screen";
+import useAIBrandingPreference from "../../admin.ai.v1/hooks/use-ai-branding-preference";
 import { AppState } from "../../admin.core.v1/store";
 import { ExtendedFeatureConfigInterface } from "../../admin.extensions.v1/configs/models";
 import { AI_BRANDING_FEATURE_ID } from "../constants/ai-branding-constants";
