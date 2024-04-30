792516c04c404e71a86cb993ce8e39a19a8f196c
jathushan-r <jathu292@gmail.com>
2024-04-30 19:51:08 +0530

    Add theming for `Features`

---
+++ features/common.branding.v1/models/element-styles.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { CSSProperties } from "react";
+
+/**
+ * Interface for the Primary Button Style Attributes..
+ */
+export interface ButtonStyleAttributesInterface {
+    /**
+     * Button Text.
+     */
+    font: FontStyleAttributesInterface;
+    /**
+     * Button Background.
+     */
+    background: BackgroundStyleAttributesInterface;
+    /**
+     * Button Border.
+     */
+    border: Pick<BorderStyleAttributesInterface, "borderRadius">;
+}
+
+/**
+ * Color styles interface.
+ * @remarks Extend with contrast, alpha. whenever necessary.
+ */
+export type ColorStyleAttributesInterface = Pick<CSSProperties, "color">;
+
+/**
+ * Font styles interface.
+ * @remarks Extend with font size, weight. whenever necessary.
+ */
+export type FontStyleAttributesInterface = ColorStyleAttributesInterface;
+
+/**
+ * Border styles interface.
+ * @remarks Extend with borderStyle, etc. whenever necessary.
+ */
+export type BorderStyleAttributesInterface = Pick<CSSProperties, "borderColor">
+    & Pick<CSSProperties, "borderRadius">
+    & Pick<CSSProperties, "borderWidth">;
+
+/**
+ * Background styles interface.
+ * @remarks Extend with backgroundImage, backgroundSize, etc. whenever necessary.
+ */
+export type BackgroundStyleAttributesInterface = Pick<CSSProperties, "backgroundColor">;
+
+/**
+ * Generic interface for element states.
+ * @remarks Extend with hover, active & other possible element states.
+ */
+export interface ElementStateInterface<T> {
+    base: T;
+}
---
+++ features/common.branding.v1/providers/branding-preference-provider.tsx
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+import { ThemeProvider as OxygenThemeProvider } from "@oxygen-ui/react/theme";
+import React, { PropsWithChildren, ReactElement, useMemo } from "react";
+import { Helmet } from "react-helmet";
+import { generateAsgardeoTheme } from "../branding/theme";
+import { BrandingPreferenceContext, BrandingPreferenceContextProps } from "../contexts/branding-preference-context";
+import { BrandingPreferenceMeta } from "../meta";
+
+/**
+ * Props interface for the BrandingPreferenceProvider.
+ */
+export interface BrandingPreferenceProviderProps {
+
+    themePreference: any;
+}
+
+/**
+ * Branding preference provider.
+ *
+ * @param props - Props for the client.
+ * @returns Branding preference provider.
+ */
+export const ThemeProvider = (props: PropsWithChildren<BrandingPreferenceProviderProps>): ReactElement => {
+    const { children, themePreference: brandingPreference } = props;
+
+    const contextValues: BrandingPreferenceContextProps = useMemo(() => {
+        if (!brandingPreference?.preference?.configs?.isBrandingEnabled) {
+            return { brandingPreference: undefined };
+        }
+
+        return { brandingPreference };
+    }, [ brandingPreference ]);
+
+    const _theme: string = useMemo(
+        () => BrandingPreferenceMeta.getThemeSkeleton(brandingPreference?.preference?.theme),
+        [ brandingPreference?.preference?.theme ]
+    );
+
+    const injectBrandingCSSSkeleton = () => {
+        if (!brandingPreference?.preference?.theme || !brandingPreference?.preference?.configs?.isBrandingEnabled) {
+            return;
+        }
+
+        return <style type="text/css">{ _theme }</style>;
+    };
+
+    return (
+        <BrandingPreferenceContext.Provider value={ contextValues }>
+            <Helmet>
+                { injectBrandingCSSSkeleton() }
+            </Helmet>
+            <OxygenThemeProvider
+                theme={ generateAsgardeoTheme(contextValues) }
+                defaultMode="light"
+                modeStorageKey="console-oxygen-mode"
+            >
+                { children }
+            </OxygenThemeProvider>
+        </BrandingPreferenceContext.Provider>
+    );
+};
