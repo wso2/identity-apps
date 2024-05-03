196976b75927fda4766537759a9ff271ef5bc82f
thiva-k <kalyanasundaramthivaharan@gmail.com>
2024-05-03 23:44:51 +0530

    Fix lint issues

--- features/admin.remote-repository-configuration.v1/index.ts
+++ features/admin.remote-repository-configuration.v1/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -21,4 +21,4 @@ export * from "./components";
 export * from "./configs";
 export * from "./models";
 export * from "./pages";
-export * from "./api"
+export * from "./api";
--- features/rollup.config.cjs
+++ features/rollup.config.cjs
@@ -1,17 +1,34 @@
-const commonjs = require('@rollup/plugin-commonjs');
-const dynamicImportVars = require('@rollup/plugin-dynamic-import-vars');
-const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const typescript = require('@rollup/plugin-typescript');
-const dts = require('rollup-plugin-dts');
-const scss = require('rollup-plugin-scss');
-const svg = require('rollup-plugin-svg');
-const json = require('@rollup/plugin-json');
-const image = require('@rollup/plugin-image');
-const svgr = require('@svgr/rollup');
-const nodePolyfills = require('rollup-plugin-polyfill-node');
-const styles = require('rollup-plugin-styles'); // Import rollup-plugin-styles
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-const pkg = require('./package.json');
+const commonjs = require("@rollup/plugin-commonjs");
+const dynamicImportVars = require("@rollup/plugin-dynamic-import-vars");
+const image = require("@rollup/plugin-image");
+const json = require("@rollup/plugin-json");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const typescript = require("@rollup/plugin-typescript");
+const svgr = require("@svgr/rollup");
+const dts = require("rollup-plugin-dts");
+const nodePolyfills = require("rollup-plugin-polyfill-node");
+const scss = require("rollup-plugin-scss");
+const styles = require("rollup-plugin-styles");
+const svg = require("rollup-plugin-svg");
+const pkg = require("./package.json");
 
 // const onwarn = (warning, warn) => {
 //     if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
@@ -23,27 +40,27 @@ const pkg = require('./package.json');
 module.exports = [
     {
         cache: false,
-        input: './index.ts',
+        input: "./index.ts",
         output: [
             {
                 file: pkg.main,
-                format: 'cjs',
+                format: "cjs",
                 inlineDynamicImports: true,
-                sourcemap: true,
+                sourcemap: true
             },
             {
                 file: pkg.umd,
-                format: 'umd',
+                format: "umd",
                 inlineDynamicImports: true,
-                name: 'core',
-                sourcemap: true,
+                name: "core",
+                sourcemap: true
             },
             {
                 file: pkg.module,
-                format: 'esm',
+                format: "esm",
                 inlineDynamicImports: true,
-                sourcemap: true,
-            },
+                sourcemap: true
+            }
         ],
         plugins: [
             nodeResolve(),
@@ -55,22 +72,20 @@ module.exports = [
             nodePolyfills(),
             commonjs(),
             typescript({
-                tsconfig: './tsconfig.lib.json',
                 declaration: true,
-                declarationDir: 'dist',
+                declarationDir: "dist",
+                tsconfig: "./tsconfig.lib.json"
             }),
             dynamicImportVars(),
-            styles({ // Include rollup-plugin-styles
-                mode: 'inject'
-            }),
-        ],
+            styles({ mode: "inject" })
+        ]
         // onwarn,
     },
     {
         cache: false,
-        input: 'dist/esm/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [dts.default()],
-        external: [/\.(sass|scss|css)$/] 
-    },
+        external: [ /\.(sass|scss|css)$/ ],
+        input: "dist/esm/index.d.ts",
+        output: [ { file: "dist/index.d.ts", format: "esm" } ],
+        plugins: [ dts.default() ]
+    }
 ];
