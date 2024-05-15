5060d9240b2d63578123b4346fcedbe25bac9048
Pavindu Lakshan <pavindulakshan@gmail.com>
2024-05-15 10:21:55 +0530

    Mark invite parent user feature as new feature

--- features/admin.users.v1/pages/users.tsx
+++ features/admin.users.v1/pages/users.tsx
@@ -16,6 +16,7 @@
  * under the License.
  */
 
+import Chip from "@oxygen-ui/react/Chip";
 import { FeatureStatus, useCheckFeatureStatus } from "@wso2is/access-control";
 import { hasRequiredScopes } from "@wso2is/core/helpers";
 import {
@@ -65,6 +66,7 @@ import {
 } from "../../admin.core.v1";
 import { userstoresConfig } from "../../admin.extensions.v1";
 import { FeatureGateConstants } from "../../admin.extensions.v1/components/feature-gate/constants/feature-gate";
+import FeatureStatusLabel from "../../admin.extensions.v1/components/feature-gate/models/feature-gate";
 import { SCIMConfigs } from "../../admin.extensions.v1/configs/scim";
 import { useGetCurrentOrganizationType } from "../../admin.organizations.v1/hooks/use-get-organization-type";
 import {
@@ -129,6 +131,7 @@ const UsersPage: FunctionComponent<UsersPageInterface> = (
 
     const dispatch: Dispatch<any> = useDispatch();
     const { legacyAuthzRuntime }  = useAuthorization();
+    const showStatusLabel: boolean = window["AppUtils"]?.getConfig()?.ui?.showStatusLabel;
 
     const saasFeatureStatus : FeatureStatus = useCheckFeatureStatus(
         FeatureGateConstants.SAAS_FEATURES_IDENTIFIER);
@@ -760,7 +763,16 @@ const UsersPage: FunctionComponent<UsersPageInterface> = (
             dropDownOptions.push({
                 "data-componentid": `${componentId}-invite-parent-user`,
                 key: 3,
-                text: t("parentOrgInvitations:addUserWizard.heading"),
+                text: <>
+                    { t("parentOrgInvitations:addUserWizard.heading") }
+                    { showStatusLabel && !legacyAuthzRuntime && (
+                        <Chip
+                            size="small"
+                            label={ t(FeatureStatusLabel.NEW).toUpperCase() }
+                            className="oxygen-chip-new mb-1 ml-2"
+                        />
+                    ) }
+                </>,
                 value: UserAccountTypesMain.INTERNAL
             });
         }
