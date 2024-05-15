6a4cc71181d60088ec1608b51ff1d6221aac9b66
lashinie <lashinisharikaj@gmail.com>
2024-05-15 09:31:16 +0530

    update disabled feature

--- apps/myaccount/test-configs/__mocks__/window/app-utils.ts
+++ apps/myaccount/test-configs/__mocks__/window/app-utils.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -116,9 +116,7 @@ interface CustomWindow extends Window {
                         }
                     },
                     personalInfo: {
-                        disabledFeatures: [
-                            "profileInfo.mobileVerification"
-                        ],
+                        disabledFeatures: [],
                         enabled: true,
                         scopes: {
                             create: [],
