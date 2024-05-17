bb31e122b0cc25bac5f88e34119856ca08d3a389
jathushan-r <jathu292@gmail.com>
2024-05-17 09:05:06 +0530

    Fix RangeError in extensions.v1

--- features/admin.extensions.v1/extensions-manager.ts
+++ features/admin.extensions.v1/extensions-manager.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2023-2024, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -86,29 +86,29 @@ export class ExtensionsManager {
             };
         }
 
-        // If categories exists, try to resolve the category config by lazy loading the resource etc.
-        if (config.categories && Array.isArray(config.categories) && config.categories.length > 0) {
-            config.categories = config.categories
-                .map((category: TemplateConfigInterface<ApplicationTemplateCategoryInterface>) => {
-                    return ExtensionsManager.lazyLoadTemplateResources<ApplicationTemplateCategoryInterface>(category);
-                });
-        }
+        // // If categories exists, try to resolve the category config by lazy loading the resource etc.
+        // if (config.categories && Array.isArray(config.categories) && config.categories.length > 0) {
+        //     config.categories = config.categories
+        //         .map((category: TemplateConfigInterface<ApplicationTemplateCategoryInterface>) => {
+        //             return ExtensionsManager.lazyLoadTemplateResources<ApplicationTemplateCategoryInterface>(category);
+        //         });
+        // }
 
-        // If groups exists, try to resolve the group config by lazy loading the resource etc.
-        if (config.groups && Array.isArray(config.groups) && config.groups.length > 0) {
-            config.groups = config.groups
-                .map((group: TemplateConfigInterface<ApplicationTemplateGroupInterface>) => {
-                    return ExtensionsManager.lazyLoadTemplateResources<ApplicationTemplateGroupInterface>(group);
-                });
-        }
+        // // If groups exists, try to resolve the group config by lazy loading the resource etc.
+        // if (config.groups && Array.isArray(config.groups) && config.groups.length > 0) {
+        //     config.groups = config.groups
+        //         .map((group: TemplateConfigInterface<ApplicationTemplateGroupInterface>) => {
+        //             return ExtensionsManager.lazyLoadTemplateResources<ApplicationTemplateGroupInterface>(group);
+        //         });
+        // }
 
-        // If templates exists, try to resolve the group config by lazy loading the resource etc.
-        if (config.templates && Array.isArray(config.templates) && config.templates.length > 0) {
-            config.templates = config.templates
-                .map((template: TemplateConfigInterface<ApplicationTemplateInterface>) => {
-                    return ExtensionsManager.lazyLoadTemplateResources<ApplicationTemplateGroupInterface>(template);
-                });
-        }
+        // // If templates exists, try to resolve the group config by lazy loading the resource etc.
+        // if (config.templates && Array.isArray(config.templates) && config.templates.length > 0) {
+        //     config.templates = config.templates
+        //         .map((template: TemplateConfigInterface<ApplicationTemplateInterface>) => {
+        //             return ExtensionsManager.lazyLoadTemplateResources<ApplicationTemplateGroupInterface>(template);
+        //         });
+        // }
 
         return config;
     }
@@ -132,32 +132,32 @@ export class ExtensionsManager {
             };
         }
 
-        // If categories exists, try to resolve the category config by lazy loading the resource etc.
-        if (config.categories && Array.isArray(config.categories) && config.categories.length > 0) {
-            config.categories = config.categories
-                .map((category: TemplateConfigInterface<IdentityProviderTemplateCategoryInterface>) => {
-                    return ExtensionsManager
-                        .lazyLoadTemplateResources<IdentityProviderTemplateCategoryInterface>(category);
-                });
-        }
+        // // If categories exists, try to resolve the category config by lazy loading the resource etc.
+        // if (config.categories && Array.isArray(config.categories) && config.categories.length > 0) {
+        //     config.categories = config.categories
+        //         .map((category: TemplateConfigInterface<IdentityProviderTemplateCategoryInterface>) => {
+        //             return ExtensionsManager
+        //                 .lazyLoadTemplateResources<IdentityProviderTemplateCategoryInterface>(category);
+        //         });
+        // }
 
-        // If groups exists, try to resolve the group config by lazy loading the resource etc.
-        if (config.groups && Array.isArray(config.groups) && config.groups.length > 0) {
-            config.groups = config.groups
-                .map((group: TemplateConfigInterface<IdentityProviderTemplateGroupInterface>) => {
-                    return ExtensionsManager.
-                        lazyLoadTemplateResources<IdentityProviderTemplateListItemInterface>(group);
-                });
-        }
+        // // If groups exists, try to resolve the group config by lazy loading the resource etc.
+        // if (config.groups && Array.isArray(config.groups) && config.groups.length > 0) {
+        //     config.groups = config.groups
+        //         .map((group: TemplateConfigInterface<IdentityProviderTemplateGroupInterface>) => {
+        //             return ExtensionsManager.
+        //                 lazyLoadTemplateResources<IdentityProviderTemplateListItemInterface>(group);
+        //         });
+        // }
 
-        // If templates exists, try to resolve the group config by lazy loading the resource etc.
-        if (config.templates && Array.isArray(config.templates) && config.templates.length > 0) {
-            config.templates = config.templates
-                .map((template: TemplateConfigInterface<IdentityProviderTemplateListItemInterface>) => {
-                    return ExtensionsManager.
-                        lazyLoadTemplateResources<IdentityProviderTemplateListItemInterface>(template);
-                });
-        }
+        // // If templates exists, try to resolve the group config by lazy loading the resource etc.
+        // if (config.templates && Array.isArray(config.templates) && config.templates.length > 0) {
+        //     config.templates = config.templates
+        //         .map((template: TemplateConfigInterface<IdentityProviderTemplateListItemInterface>) => {
+        //             return ExtensionsManager.
+        //                 lazyLoadTemplateResources<IdentityProviderTemplateListItemInterface>(template);
+        //         });
+        // }
 
         return config;
     }
@@ -169,48 +169,37 @@ export class ExtensionsManager {
      * @returns the resolved template config.
      */
     // eslint-disable-next-line @typescript-eslint/ban-types
-    private static lazyLoadTemplateResources<T = {}>(
-        templateConfig: TemplateConfigInterface<T>): TemplateConfigInterface<T> {
+    // private static lazyLoadTemplateResources<T = {}>(
+    //     templateConfig: TemplateConfigInterface<T>): TemplateConfigInterface<T> {
 
-        // Dynamically lazy loads the content.
-        const loadContent = (content: TemplateContentInterface): TemplateContentInterface => {
+    //     // Dynamically lazy loads the content.
+    //     const loadContent = (content: TemplateContentInterface): TemplateContentInterface => {
 
-            if (!content && !isObject(content)) {
-                return null;
-            }
+    //         if (!content && !isObject(content)) {
+    //             return null;
+    //         }
 
-            for (const [ key, value ] of Object.entries(content)) {
+    //         for (const [ key, value ] of Object.entries(content)) {
+    //             content[ key ] = lazy(() => import(`${ value }`));
+    //         }
 
-                // Strip the prefix './application-templates/' and the '.tsx' extension to overcome rollup limitation
-                //https://www.npmjs.com/package/@rollup/plugin-dynamic-import-vars
+    //         return content;
+    //     };
 
-                const valueStripped: string= value.replace(/^\.\/application-templates\//, "").replace(/\.tsx$/, "");
+    //     // Lazy loads the resource.
+    //     const loadResource = (resource: any) => {
 
-                content[ key ] = lazy(() => import(`./application-templates/${ valueStripped }.tsx`));
-            }
+    //         if (typeof resource !== "string") {
+    //             return resource;
+    //         }
 
-            return content;
-        };
+    //         return import(`${ resource }`).then((module: any) => module.default);
+    //     };
 
-        // Lazy loads the resource.
-        const loadResource = (resource: any) => {
-
-            if (typeof resource !== "string") {
-                return resource;
-            }
-
-            // Strip the prefix './application-templates/' and the '.json' extension to overcome rollup limitation
-            //https://www.npmjs.com/package/@rollup/plugin-dynamic-import-vars
-
-            const resourceStripped: string = resource.replace(/^\.\/application-templates\//,"").replace(/\.json$/, "");
-
-            return import(`./application-templates/${ resourceStripped }.json`).then((module: any) => module.default);
-        };
-
-        return {
-            ...templateConfig,
-            content: loadContent(templateConfig?.content),
-            resource: loadResource(templateConfig?.resource)
-        };
-    }
+    //     return {
+    //         ...templateConfig,
+    //         content: loadContent(templateConfig?.content),
+    //         resource: loadResource(templateConfig?.resource)
+    //     };
+    // }
 }
