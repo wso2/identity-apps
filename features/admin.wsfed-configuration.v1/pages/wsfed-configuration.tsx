c14e0364a6559038515a402727fca721380296ab
jathushan-r <jathu292@gmail.com>
2024-05-17 12:33:06 +0530

    Refactor import paths in `admin.wsfed-configuration.v1`

--- features/admin.wsfed-configuration.v1/pages/wsfed-configuration.tsx
+++ features/admin.wsfed-configuration.v1/pages/wsfed-configuration.tsx
@@ -27,7 +27,7 @@ import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "redux";
 import { Divider, Grid, Placeholder, Ref } from "semantic-ui-react";
-import { AppConstants, AppState, FeatureConfigInterface, history } from "../../admin.core.v1";
+import { AppConstants, AppState, FeatureConfigInterface, history } from "@wso2is/admin.core.v1";
 import { updateWSFederationConfigurations, useWSFederationConfig } from "../api/wsfed-configuration";
 import {
     WSFederationConfigAPIResponseInterface,
