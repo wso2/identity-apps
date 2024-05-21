13ecf6e8e754759bf8aeb57aec992a007ec1e589
jathushan-r <jathu292@gmail.com>
2024-05-21 14:01:08 +0530

    Refactor import paths in `admin.tenants.v1`

--- features/admin.tenants.v1/api/tenants.ts
+++ features/admin.tenants.v1/api/tenants.ts
@@ -17,10 +17,10 @@
  */
 
 import { AsgardeoSPAClient, HttpClientInstance } from "@asgardeo/auth-react";
+import { store } from "@wso2is/admin.core.v1/store";
+import { OrganizationType } from "@wso2is/admin.organizations.v1/constants";
 import { HttpMethods } from "@wso2is/core/models";
 import { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
-import { store } from "../../admin.core.v1/store";
-import { OrganizationType } from "../../admin.organizations.v1/constants";
 import { TenantRequestResponse } from "../models";
 
 const getDomainQueryParam = (): string => {
