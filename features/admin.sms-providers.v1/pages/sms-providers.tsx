b6b60f62bb608ebd642ecc24f61e9c06f986649c
jathushan-r <jathu292@gmail.com>
2024-05-21 14:00:58 +0530

    Refactor import paths in `admin.sms-providers.v1`

--- features/admin.sms-providers.v1/pages/sms-providers.tsx
+++ features/admin.sms-providers.v1/pages/sms-providers.tsx
@@ -17,6 +17,17 @@
  */
 
 import { Show } from "@wso2is/access-control";
+import { AuthenticatorManagementConstants } from "@wso2is/admin.connections.v1/constants/autheticator-constants";
+import {
+    AppConstants,
+    AppState,
+    FeatureConfigInterface
+} from "@wso2is/admin.core.v1";
+import { history } from "@wso2is/admin.core.v1/helpers";
+import {
+    useSMSNotificationSenders
+}from "@wso2is/admin.extensions.v1/components/identity-providers/api/identity-provider";
+import smsProviderConfig from "@wso2is/admin.extensions.v1/configs/sms-provider";
 import { IdentityAppsApiException } from "@wso2is/core/exceptions";
 import { hasRequiredScopes } from "@wso2is/core/helpers";
 import { AlertLevels, IdentifiableComponentInterface } from "@wso2is/core/models";
@@ -39,17 +50,6 @@ import { Divider, Grid, Placeholder } from "semantic-ui-react";
 import CustomSMSProvider from "./custom-sms-provider";
 import TwilioSMSProvider from "./twilio-sms-provider";
 import VonageSMSProvider from "./vonage-sms-provider";
-import { AuthenticatorManagementConstants } from "../../admin.connections.v1/constants/autheticator-constants";
-import {
-    AppConstants,
-    AppState,
-    FeatureConfigInterface
-} from "../../admin.core.v1";
-import { history } from "../../admin.core.v1/helpers";
-import {
-    useSMSNotificationSenders
-}from "../../admin.extensions.v1/components/identity-providers/api/identity-provider";
-import smsProviderConfig from "../../admin.extensions.v1/configs/sms-provider";
 import { createSMSProvider, deleteSMSProviders, updateSMSProvider, useSMSProviders } from "../api";
 import { providerCards } from "../configs/provider-cards";
 import { SMSProviderConstants } from "../constants";
