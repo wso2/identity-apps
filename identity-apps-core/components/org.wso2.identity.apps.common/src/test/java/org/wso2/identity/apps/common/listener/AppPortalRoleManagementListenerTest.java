2db01844a9f38a5e685d01cbb2fd5cf083c42844
Hasini Samarathunga <hasinis@wso2.com>
2024-05-27 03:22:07 +0530

    Updated unit test code for preUpdatePermissionsForRole method

--- identity-apps-core/components/org.wso2.identity.apps.common/src/test/java/org/wso2/identity/apps/common/listener/AppPortalRoleManagementListenerTest.java
+++ identity-apps-core/components/org.wso2.identity.apps.common/src/test/java/org/wso2/identity/apps/common/listener/AppPortalRoleManagementListenerTest.java
@@ -1,117 +1,98 @@
 package org.wso2.identity.apps.common.listener;
 
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.testng.MockitoTestNGListener;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-
 import org.mockito.Mock;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.wso2.carbon.context.CarbonContext;
+import org.wso2.carbon.base.CarbonBaseConstants;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
-import org.wso2.carbon.context.internal.CarbonContextDataHolder;
 import org.wso2.carbon.identity.organization.management.service.util.OrganizationManagementUtil;
-import org.wso2.carbon.identity.role.v2.mgt.core.RoleManagementService;
 import org.wso2.carbon.identity.role.v2.mgt.core.exception.IdentityRoleManagementException;
+import org.wso2.carbon.identity.role.v2.mgt.core.RoleManagementService;
 import org.wso2.carbon.identity.role.v2.mgt.core.model.Role;
 import org.wso2.carbon.user.core.UserRealm;
 import org.wso2.carbon.user.core.common.AbstractUserStoreManager;
 import org.wso2.carbon.user.core.config.RealmConfiguration;
-import org.wso2.carbon.utils.CarbonUtils;
 import org.wso2.identity.apps.common.internal.AppsCommonDataHolder;
 import org.wso2.identity.apps.common.listner.AppPortalRoleManagementListener;
 
-import java.util.Arrays;
+import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
-@Listeners(MockitoTestNGListener.class)
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
+import static org.wso2.carbon.base.CarbonBaseConstants.CARBON_HOME;
+import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.ADMINISTRATOR;
+import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.APPLICATION;
+import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.ORGANIZATION;
+import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.RoleTableColumns.ROLE_NAME;
+import static org.wso2.identity.apps.common.util.AppPortalConstants.CONSOLE_APP;
+
 public class AppPortalRoleManagementListenerTest {
+
+    private AutoCloseable closeable;
+
+    private MockedStatic<PrivilegedCarbonContext> privilegedCarbonContext;
+    private MockedStatic<OrganizationManagementUtil> organizationManagementUtil;
+    private MockedStatic<AppsCommonDataHolder> appsCommonDataHolder;
 
     @Mock
     private UserRealm mockUserRealm;
 
     @Mock
-    private AbstractUserStoreManager mockAbstractUserStoreManager;
+    private RoleManagementService roleManagementService;
 
     @Mock
-    private RealmConfiguration mockRealmConfiguration;
-
-    @Mock
-    private RoleManagementService mockRoleManagementService;
-
-    @Mock
-    private Role mockRole;
-
-    private static final String ADMINISTRATOR = "Administrator";
-    private static final String APPLICATION = "application";
-    private static final String CONSOLE_APP = "Console";
-    private static final String carbonContext = "Console";
-    private static final String adminUsername = "admin";
+    private Role role;
 
     @BeforeMethod
-    public void setUp() throws Exception {
+    public void setUp() {
 
-        openMocks(this);
-
-        mockStatic(AppsCommonDataHolder.class);
-        AppsCommonDataHolder mockAppsCommonDataHolder = mock(AppsCommonDataHolder.class);
-        when(AppsCommonDataHolder.getInstance()).thenReturn(mockAppsCommonDataHolder);
-        when(mockAppsCommonDataHolder.getRoleManagementServiceV2()).thenReturn(mockRoleManagementService);
-
-        mockCarbonContextForTenant();
-
-        when(mockUserRealm.getRealmConfiguration()).thenReturn(mockRealmConfiguration);
-        when(mockUserRealm.getUserStoreManager()).thenReturn(mockAbstractUserStoreManager);
-
-        mockStatic(OrganizationManagementUtil.class);
-
+        setUpCarbonHome();
+        privilegedCarbonContext = mockStatic(PrivilegedCarbonContext.class);
+        organizationManagementUtil = mockStatic(OrganizationManagementUtil.class);
+        appsCommonDataHolder = mockStatic(AppsCommonDataHolder.class);
+        mockCarbonContext(privilegedCarbonContext);
+        closeable = MockitoAnnotations.openMocks(this);
     }
 
-    private void mockCarbonContextForTenant() {
+    @AfterMethod
+    public void tearDown() throws Exception {
 
-        try (MockedStatic<PrivilegedCarbonContext> privilegedCarbonContext = mockStatic(PrivilegedCarbonContext.class)) {
-
-            PrivilegedCarbonContext mockPrivilegedCarbonContext = mock(PrivilegedCarbonContext.class);
-            privilegedCarbonContext.when(PrivilegedCarbonContext::getThreadLocalCarbonContext)
-                .thenReturn(mockPrivilegedCarbonContext);
-            when(PrivilegedCarbonContext.getThreadLocalCarbonContext().getUserRealm()).thenReturn(mockUserRealm);
-        }
+        privilegedCarbonContext.close();
+        organizationManagementUtil.close();
+        appsCommonDataHolder.close();
+        closeable.close();
     }
 
     @DataProvider(name = "preUpdateUserListOfRoleDataProvider")
     public Object[][] preUpdateUserListOfRoleDataProvider() {
         return new Object[][]{
             // Test case where deletedUserIDList == null
-            {"roleId", Arrays.asList("newUser"), null, "tenantDomain", true, false, null},
+            {"roleId", Collections.emptyList(), null, CARBON_HOME, true, false, "adminUserId"},
 
-            // Test case where !isAdministratorRole(roleId, tenantDomain)
-            {"roleId", Arrays.asList("newUser"), Arrays.asList("deletedUser"), "tenantDomain", false, false, null},
-
-            // Test case where both deletedUserIDList == null and !isAdministratorRole(roleId, tenantDomain)
-            {"roleId", Arrays.asList("newUser"), null, "tenantDomain", false, false, null},
-
-            // Test case where both deletedUserIDList != null and isAdministratorRole(roleId, tenantDomain)
-            {"roleId", Arrays.asList("newUser"), Arrays.asList("deletedUser"), "tenantDomain", true, false, null},
+            // Test case where !isAdministratorRole
+            {"roleId", Collections.emptyList(), Collections.singletonList("deletedUser"), CARBON_HOME, false, false,
+                "adminUserId"},
 
             // Test case where isOrganization == true
-            {"roleId", Arrays.asList("newUser"), Arrays.asList("deletedUser"), "tenantDomain", true, true, null},
-
-            // Test case where isOrganization == false
-            {"roleId", Arrays.asList("newUser"), Arrays.asList("deletedUser"), "tenantDomain", true, false, null},
+            {"roleId", Collections.emptyList(), Collections.singletonList("deletedUser"), CARBON_HOME, true, true,
+                "adminUserId"},
 
             // Test case where deletedUserIDList contains adminUserId
-            {"roleId", Arrays.asList("newUser"), Arrays.asList("adminUserId"), "tenantDomain", true, false, "adminUserId"}
+            {"roleId", Collections.emptyList(), Collections.singletonList("adminUserId"), CARBON_HOME, true, false,
+                "adminUserId"}
         };
     }
 
@@ -120,48 +101,81 @@ public class AppPortalRoleManagementListenerTest {
                                 String tenantDomain, boolean isAdminRole, boolean isOrganization, String adminUserId)
         throws Exception {
 
-        //Create AppPortalRoleManagementListener Instance
-        AppPortalRoleManagementListener appPortalRoleManagementListener =
-            new AppPortalRoleManagementListener(true);
+        AppPortalRoleManagementListener appPortalRoleManagementListener = spy(
+            new AppPortalRoleManagementListener(true));
 
-        // Mock the behavior of isAdministratorRole
-        when(mockRoleManagementService.getRole(roleId, tenantDomain)).thenReturn(mockRole);
-        when(mockRole.getName()).thenReturn(ADMINISTRATOR);
-        when(mockRole.getAudience()).thenReturn(APPLICATION);
-        when(mockRole.getAudienceName()).thenReturn(CONSOLE_APP);
+        // Set up the mock behaviors
+        organizationManagementUtil.when(() ->
+            OrganizationManagementUtil.isOrganization(tenantDomain)).thenReturn(isOrganization);
+        AbstractUserStoreManager mockUserStoreManager = mock(AbstractUserStoreManager.class);
+        when(mockUserRealm.getRealmConfiguration()).thenReturn(mock(RealmConfiguration.class));
+        when(mockUserRealm.getRealmConfiguration().getAdminUserName()).thenReturn(ADMINISTRATOR);
+        when(mockUserRealm.getUserStoreManager()).thenReturn(mockUserStoreManager);
+        when(mockUserStoreManager.getUserIDFromUserName(anyString()))
+            .thenReturn(adminUserId);
+        mockAppsCommonDataHolder(appsCommonDataHolder);
+        when(roleManagementService.getRole(roleId, tenantDomain)).thenReturn(role);
+        setRoleAttributes(isAdminRole);
 
-        // Mock the behavior of OrganizationManagementUtil
-        when(OrganizationManagementUtil.isOrganization(tenantDomain)).thenReturn(isOrganization);
-
-        if (adminUserId != null) {
-            when(mockRealmConfiguration.getAdminUserName()).thenReturn(adminUsername);
-            when(mockAbstractUserStoreManager.getUserIDFromUserName(adminUsername)).thenReturn(adminUserId);
+        // Call the method
+        if (!isAdminRole || deletedUserIDList == null || isOrganization || !deletedUserIDList.contains(adminUserId)) {
+            appPortalRoleManagementListener.preUpdateUserListOfRole(roleId, newUserIDList, deletedUserIDList,
+                tenantDomain);
+        } else {
+            Exception exception = null;
+            try {
+                appPortalRoleManagementListener.preUpdateUserListOfRole(roleId, newUserIDList, deletedUserIDList,
+                    tenantDomain);
+                fail("Expected IdentityRoleManagementException was not thrown");
+            } catch (IdentityRoleManagementException e) {
+                exception = e;
+            }
+            assertNotNull(exception);
+            assertEquals(exception.getMessage(), "Deleting the tenant admin from 'Administrator' role " +
+                "belongs to the 'Console' application is not allowed.");
         }
 
-        if (adminUserId != null && deletedUserIDList != null && deletedUserIDList.contains(adminUserId)) {
-            IdentityRoleManagementException thrownException = null;
-            try {
-                appPortalRoleManagementListener.preUpdateUserListOfRole(
-                    roleId, newUserIDList, deletedUserIDList, tenantDomain);
-            } catch (IdentityRoleManagementException e) {
-                thrownException = e;
-            }
-            assertNotNull(thrownException);
-            assertEquals(thrownException.getMessage(), "Deleting the tenant admin from 'Administrator' " +
-                "role belongs to the 'Console' application is not allowed.");
-        } else {
-            appPortalRoleManagementListener.preUpdateUserListOfRole(
-                roleId, newUserIDList, deletedUserIDList, tenantDomain);
-
-            // Verify that the appPortalRoleManagementListener method is called when it should be
-            if (deletedUserIDList != null && isAdminRole && !isOrganization) {
-                verify(appPortalRoleManagementListener).preUpdateUserListOfRole(
-                    roleId, newUserIDList, deletedUserIDList, tenantDomain);
-            } else {
-                verify(appPortalRoleManagementListener, never()).preUpdateUserListOfRole(
-                    roleId, newUserIDList, deletedUserIDList, tenantDomain);
-            }
+        if (deletedUserIDList == null || !isAdminRole) {
+            organizationManagementUtil.verify(() -> OrganizationManagementUtil.isOrganization(tenantDomain), never());
+        } else if (isOrganization) {
+            organizationManagementUtil.verify(() -> OrganizationManagementUtil.isOrganization(tenantDomain));
         }
     }
-}
 
+    private void setRoleAttributes(boolean isAdminRole) {
+
+        if (isAdminRole) {
+            when(role.getName()).thenReturn(ADMINISTRATOR);
+            when(role.getAudience()).thenReturn(APPLICATION);
+            when(role.getAudienceName()).thenReturn(CONSOLE_APP);
+        } else {
+            when(role.getName()).thenReturn(ROLE_NAME);
+            when(role.getAudience()).thenReturn(ORGANIZATION);
+            when(role.getAudienceName()).thenReturn("App1");
+        }
+    }
+
+    private static void setUpCarbonHome() {
+
+        String carbonHome = Paths.get(System.getProperty("user.dir"), "target", "test-classes").toString();
+        System.setProperty(CARBON_HOME, carbonHome);
+        System.setProperty(CarbonBaseConstants.CARBON_CONFIG_DIR_PATH, Paths.get(carbonHome,
+            "repository/conf").toString());
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private void mockAppsCommonDataHolder(MockedStatic<AppsCommonDataHolder> appsCommonDataHolder) {
+
+        AppsCommonDataHolder mockAppsCommonDataHolder = mock(AppsCommonDataHolder.class);
+        appsCommonDataHolder.when(AppsCommonDataHolder::getInstance).thenReturn(mockAppsCommonDataHolder);
+        when(mockAppsCommonDataHolder.getRoleManagementServiceV2()).thenReturn(roleManagementService);
+    }
+
+    private void mockCarbonContext(MockedStatic<PrivilegedCarbonContext> privilegedCarbonContext) {
+
+        PrivilegedCarbonContext mockPrivilegedCarbonContext = mock(PrivilegedCarbonContext.class);
+        privilegedCarbonContext.when(
+            PrivilegedCarbonContext::getThreadLocalCarbonContext).thenReturn(mockPrivilegedCarbonContext);
+            when(PrivilegedCarbonContext.getThreadLocalCarbonContext().getUserRealm()).thenReturn(mockUserRealm);
+    }
+}
