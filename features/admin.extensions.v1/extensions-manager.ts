f7cd9bbb30a52434bfdf706f439dd0f109c7a631
Brion <info@brionmario.com>
2024-05-29 23:18:19 +0530

    Fix lazy loading issues in extensions manager

--- features/admin.extensions.v1/extensions-manager.ts
+++ features/admin.extensions.v1/extensions-manager.ts
@@ -182,12 +182,20 @@ export class ExtensionsManager {
 
             for (const [ key, value ] of Object.entries(content)) {
 
-                // Strip the prefix './application-templates/' and the '.tsx' extension to overcome rollup limitation
+                // Strip the prefix './application-templates/', './identity-provider-templates/`
+                // and the '.tsx' extension to overcome rollup limitation
                 //https://www.npmjs.com/package/@rollup/plugin-dynamic-import-vars
+                if (value.includes("application-templates")) {
+                    const valueStripped: string= value.replace(/^\.\/application-templates\//, "").replace(/\.tsx$/, "");
 
-                const valueStripped: string= value.replace(/^\.\/application-templates\//, "").replace(/\.tsx$/, "");
+                    content[ key ] = lazy(() => import(`./application-templates/${ valueStripped }.tsx`));
+                } else if (value.includes("identity-provider-templates")) {
+                    const valueStripped: string= value.replace(/^\.\/identity-provider-templates\//, "").replace(/\.tsx$/, "");
 
-                content[ key ] = lazy(() => import(`./application-templates/${ valueStripped }.tsx`));
+                    content[ key ] = lazy(() => import(`./identity-provider-templates/${ valueStripped }.tsx`));
+                } else {
+                    content[ key ] = lazy(() => import(`${ value }`));
+                }
             }
 
             return content;
@@ -200,12 +208,22 @@ export class ExtensionsManager {
                 return resource;
             }
 
-            // Strip the prefix './application-templates/' and the '.json' extension to overcome rollup limitation
+            // Strip the prefix './application-templates/', './identity-provider-templates/`
+            // and the '.json' extension to overcome rollup limitation
             //https://www.npmjs.com/package/@rollup/plugin-dynamic-import-vars
+            if (resource.includes("application-templates")) {
+                const resourceStripped: string = resource.replace(/^\.\/application-templates\//,"").replace(/\.json$/, "");
 
-            const resourceStripped: string = resource.replace(/^\.\/application-templates\//,"").replace(/\.json$/, "");
+                return import(`./application-templates/${ resourceStripped }.json`).then((module: any) => module.default);
+            }
 
-            return import(`./application-templates/${ resourceStripped }.json`).then((module: any) => module.default);
+            if (resource.includes("identity-provider-templates")) {
+                const resourceStripped: string = resource.replace(/^\.\/identity-provider-templates\//,"").replace(/\.json$/, "");
+
+                return import(`./identity-provider-templates/${ resourceStripped }.json`).then((module: any) => module.default);
+            }
+
+            return import(`${ resource }`).then((module: any) => module.default);
         };
 
         return {
