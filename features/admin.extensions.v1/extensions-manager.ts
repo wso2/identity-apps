389a26e682660767110b572200a3a65be7dcb733
Brion <info@brionmario.com>
2024-05-30 13:52:42 +0530

    Remove fallthrough cases

--- features/admin.extensions.v1/extensions-manager.ts
+++ features/admin.extensions.v1/extensions-manager.ts
@@ -196,8 +196,6 @@ export class ExtensionsManager {
                         .replace(/\.tsx$/, "");
 
                     content[ key ] = lazy(() => import(`./identity-provider-templates/${ valueStripped }.tsx`));
-                } else {
-                    content[ key ] = lazy(() => import(`${ value }`));
                 }
             }
 
@@ -231,8 +229,6 @@ export class ExtensionsManager {
                     (module: any) => module.default
                 );
             }
-
-            return import(`${ resource }`).then((module: any) => module.default);
         };
 
         return {
