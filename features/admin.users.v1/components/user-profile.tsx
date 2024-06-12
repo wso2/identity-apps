a7c4d14c68d2519a1b8bf88ec8bec0441925d2d3
Pasindu Yeshan <pasindua.18@cse.mrt.ac.lk>
2024-06-12 23:56:30 +0530

    refactor

--- features/admin.users.v1/components/user-profile.tsx
+++ features/admin.users.v1/components/user-profile.tsx
@@ -413,6 +413,15 @@ export const UserProfile: FunctionComponent<UserProfilePropsInterface> = (
 
                                 return;
                             }
+
+                            if (schema.extended && userInfo[ProfileConstants.SCIM2_WSO2_CUSTOM_SCHEMA]
+                                && userInfo[ProfileConstants.SCIM2_WSO2_CUSTOM_SCHEMA][schemaNames[0]]) {
+                                tempProfileInfo.set(
+                                    schema.name, userInfo[ProfileConstants.SCIM2_WSO2_CUSTOM_SCHEMA][schemaNames[0]]
+                                );
+
+                                return;
+                            }
                             tempProfileInfo.set(schema.name, userInfo[schemaName]);
                         }
                     } else {
