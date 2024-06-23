d67ab5c534f71194844a5c12970a94464594bf20
asha15 <165079T@uom.lk>
2024-06-23 17:16:41 +0530

    display the scopes field only when the API's are authorized

--- features/admin.extensions.v1/application-templates/shared/components/m2m-custom-configuration.tsx
+++ features/admin.extensions.v1/application-templates/shared/components/m2m-custom-configuration.tsx
@@ -168,6 +168,10 @@ export const M2MCustomConfiguration: FC<M2MCustomConfigurationPropsInterface> = 
         tokenRequest = tokenRequest.concat(` -d 'scope=${copyScopesValue}'`);
     }
 
+    const isScopeAvailable = (value: string) => {
+        return value != null && value.trim() !== "";
+    };
+
     const renderConfigurationFields = (): ReactElement => {
         return (
             <>
@@ -250,36 +254,38 @@ export const M2MCustomConfiguration: FC<M2MCustomConfigurationPropsInterface> = 
                                         className="mt-2"
                                     />
                                 </Form.Field>
-                                <Form.Field>
-                                    <label>
-                                        {
-                                            t("extensions:develop.applications.quickstart.mobileApp" +
-                                                ".configurations.scope.label")
-                                        }
-                                        <Hint className="mt-0 mb-0" popup>
-                                            <Trans
-                                                i18nKey={
-                                                    "extensions:develop.applications.quickstart" +
-                                                    ".m2m.configurations.scopes"
-                                                }
-                                            >
-                                                The list of authorized scopes.
-                                                Include required scopes by authorizing APIs through the
-                                                <a
-                                                    className="link pointing"
-                                                    onClick={ onAPIAuthorizationTabClick }
+                                { isScopeAvailable(copyScopesValue) && (
+                                    <Form.Field>
+                                        <label>
+                                            {
+                                                t("extensions:develop.applications.quickstart.mobileApp" +
+                                                    ".configurations.scope.label")
+                                            }
+                                            <Hint className="mt-0 mb-0" popup>
+                                                <Trans
+                                                    i18nKey={
+                                                        "extensions:develop.applications.quickstart" +
+                                                        ".m2m.configurations.scopes"
+                                                    }
                                                 >
-                                                    API Authorization
-                                                </a>
-                                                tab.
-                                            </Trans>
-                                        </Hint>
-                                    </label>
-                                    <CopyInputField
-                                        value={ copyScopesValue }
-                                        data-componentid={ `${ componentId }-scope-readonly-input` }
-                                    />
-                                </Form.Field>
+                                                    The list of authorized scopes.
+                                                    Include required scopes by authorizing APIs through the
+                                                    <a
+                                                        className="link pointing"
+                                                        onClick={ onAPIAuthorizationTabClick }
+                                                    >
+                                                        API Authorization
+                                                    </a>
+                                                    tab.
+                                                </Trans>
+                                            </Hint>
+                                        </label>
+                                        <CopyInputField
+                                            value={ copyScopesValue }
+                                            data-componentid={ `${ componentId }-scope-readonly-input` }
+                                        />
+                                    </Form.Field>
+                                )}
                                 <Form.Field>
                                     <label>
                                         {
