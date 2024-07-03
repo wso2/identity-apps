db61ad2bbf28b881257b756ae225a243485a1d24
JayaShakthi97 <jayashakthiperera@gmail.com>
2024-07-03 10:36:37 +0530

    Clean up identity-providers feature

--- features/admin.applications.v1/constants/application-management.ts
+++ features/admin.applications.v1/constants/application-management.ts
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import { ConnectionManagementConstants } from "@wso2is/admin.connections.v1";
+import { ConnectionManagementConstants } from "@wso2is/admin.connections.v1/constants/connection-constants";
 import { IdentityProviderManagementConstants } from "@wso2is/admin.identity-providers.v1/constants";
 import { SupportedAuthenticators } from "@wso2is/admin.identity-providers.v1/models";
 import { IdentityAppsError } from "@wso2is/core/errors";
