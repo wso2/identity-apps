c9753816f8b8dc8f7e17c2a29033f580fe778a02
JayaShakthi97 <jayashakthiperera@gmail.com>
2024-08-06 14:56:42 +0530

    Fix lint issues

--- features/admin.identity-verification-providers.v1/models/ui-metadata.ts
+++ features/admin.identity-verification-providers.v1/models/ui-metadata.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2023-2024, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import { FormFieldMessage } from "@wso2is/form";
 
 /**
@@ -53,8 +54,8 @@ export interface UIMetaDataForIDVP {
  * Model that represents a dropdown option
  */
 export interface DropdownOptionsInterface {
-     label: string;
-     value: string
+    label: string;
+    value: string
 }
 
 /**
