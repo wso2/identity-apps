82aa16fce3776f1e2b685a39c5f5862c948aeaa5
Brion <info@brionmario.com>
2024-08-09 17:57:52 +0530

    Remove Try It Tour

--- features/admin.extensions.v1/components/application/utils/try-it-utils.ts
+++ features/admin.extensions.v1/components/application/utils/try-it-utils.ts
@@ -16,49 +16,8 @@
  * under the License.
  */
 
-import { ApplicationManagementConstants } from "@wso2is/admin.applications.v1/constants";
-import { AppState, AppUtils } from "@wso2is/admin.core.v1";
-import { StorageIdentityAppsSettingsInterface } from "@wso2is/core/models";
-import cloneDeep from "lodash-es/cloneDeep";
-import get from "lodash-es/get";
-import isEmpty from "lodash-es/isEmpty";
-import set from "lodash-es/set";
-import { FunctionComponent } from "react";
-import { useSelector } from "react-redux";
 import LoginApplicationTemplate from
     "../../../application-templates/templates/single-page-application/login-playground-application.json";
-import { TryItApplicationConstants } from "../constants/try-it-constants";
-
-/**
- * Persist the Playground Tour seen status in Local Storage.
- *
- * @param {boolean} status - Status to set.
- */
-export const persistPlaygroundTourViewedStatus = (status: boolean): void => {
-    const userPreferences: StorageIdentityAppsSettingsInterface = AppUtils.getUserPreferences();
-    const newPref: StorageIdentityAppsSettingsInterface = cloneDeep(userPreferences);
-
-    set(newPref?.identityAppsSettings?.devPortal,
-        TryItApplicationConstants.TRY_IT_TOUR_STATUS_STORAGE_KEY, status);
-
-    AppUtils.setUserPreferences(newPref);
-};
-
-/**
- * Check if the Playground Tour has already been seen by the user.
- *
- * @return {boolean}
- */
-export const getPlaygroundTourViewedStatus = (): boolean => {
-    const userPreferences: StorageIdentityAppsSettingsInterface = AppUtils.getUserPreferences();
-
-    if (isEmpty(userPreferences)) {
-        return false;
-    }
-
-    return get(userPreferences?.identityAppsSettings?.devPortal,
-        TryItApplicationConstants.TRY_IT_TOUR_STATUS_STORAGE_KEY, false);
-};
 
 /**
  * Get the client Id of the Try it application.
