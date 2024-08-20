5f852ec8854435982bef3135ef9a287fe6f822e9
Dasuni Geeganage <dasunin30@gmail.com>
2024-08-20 23:34:09 +0530

    Fix ESLint issues

--- features/admin.users.v1/pages/users.tsx
+++ features/admin.users.v1/pages/users.tsx
@@ -977,18 +977,17 @@ const UsersPage: FunctionComponent<UsersPageInterface> = (
                     { t("users:confirmations.addMultipleUser.message") }
                 </ConfirmationModal.Message>
                 <ConfirmationModal.Content>
-                    <Trans i18nKey="users:confirmations.addMultipleUser.content">
-                        Invite User to Set Password should be enabled to add multiple users.
-                        Please enable email invitations for user password setup from 
-                            <Link
-                                onClick={ () => history.push(AppConstants.getPaths().get("GOVERNANCE_CONNECTOR_EDIT")
-                                    .replace(":categoryId", ServerConfigurationsConstants.USER_ONBOARDING_CONNECTOR_ID)
-                                    .replace(":connectorId", ServerConfigurationsConstants.ASK_PASSWORD_CONNECTOR_ID)) }
-                                external={ false }
-                            >
-                                Login & Registration settings
-                            </Link>
-                    </Trans>
+                <Trans i18nKey="users:confirmations.addMultipleUser.content">
+                    Invite User to Set Password should be enabled to add multiple users.
+                    Please enable email invitations for user password setup from
+                        <Link
+                        onClick={ () => history.push(AppConstants.getPaths().get("GOVERNANCE_CONNECTOR_EDIT")
+                        .replace(":categoryId", ServerConfigurationsConstants.USER_ONBOARDING_CONNECTOR_ID)
+                        .replace(":connectorId", ServerConfigurationsConstants.ASK_PASSWORD_CONNECTOR_ID)) }
+                        external={ false }>
+                            Login & Registration settings
+                        </Link>
+                </Trans>
                 </ConfirmationModal.Content>
             </ConfirmationModal>
         );
