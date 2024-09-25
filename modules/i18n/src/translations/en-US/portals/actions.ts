4169c32b0ae0af3578882dff59abc13fa4fffba3
Shenali <shenali@wso2.com>
2024-09-25 12:46:42 +0530

    Validate API key header name field

--- modules/i18n/src/translations/en-US/portals/actions.ts
+++ modules/i18n/src/translations/en-US/portals/actions.ts
@@ -57,10 +57,13 @@ export const actions: actionsNS = {
                     name: "API Key",
                     properties: {
                         header: {
+                            hint: "Must be a string containing only letters (a-z, A-Z), numbers (0-9), " +
+                            "period (.) and hyphen (-).",
                             label: "Header",
                             placeholder: "Header",
                             validations: {
-                                empty: "Header is a required field."
+                                empty: "Header is a required field.",
+                                invalid: "Please choose a valid name that adheres to the given guidelines."
                             }
                         },
                         value: {
