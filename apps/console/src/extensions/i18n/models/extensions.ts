42bc72b519fdc31569c0b81355f8aa3713d376fc
DonOmalVindula <omalvindula@live.com>
2024-10-23 14:53:18 +0530

    Add error handling for API resource create wizard

--- apps/console/src/extensions/i18n/models/extensions.ts
+++ apps/console/src/extensions/i18n/models/extensions.ts
@@ -618,6 +618,7 @@ export interface Extensions {
                                     identifier: {
                                         emptyValidate: string;
                                         alreadyExistsError: string;
+                                        errorOccurred: string;
                                         invalid: string;
                                         hint: string;
                                         label: string;
@@ -657,6 +658,7 @@ export interface Extensions {
                                     permission: {
                                         emptyValidate: string;
                                         uniqueValidate: string;
+                                        errorOccurred: string;
                                         invalid: string;
                                         label: string;
                                         placeholder: string;
