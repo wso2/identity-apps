372bd9fe824782e6bc8e454076c2c9c84bd65e02
JayaShakthi97 <jayashakthiperera@gmail.com>
2024-11-25 08:58:19 +0530

    [feat] introduce new remote user store impl - fix import issue

--- features/admin.extensions.v1/configs/common.tsx
+++ features/admin.extensions.v1/configs/common.tsx
@@ -18,7 +18,7 @@
 
 import { getSidePanelIcons } from "@wso2is/admin.core.v1/configs/ui";
 import { AppConstants } from "@wso2is/admin.core.v1/constants";
-import { RemoteUserStoreConstants } from "@wso2is/admin.remote-userstores.v1/constants";
+import { RemoteUserStoreConstants } from "@wso2is/admin.remote-userstores.v1/constants/remote-user-stores-constants";
 import { RouteInterface } from "@wso2is/core/models";
 import { lazy } from "react";
 import { CommonConfig } from "./models";
