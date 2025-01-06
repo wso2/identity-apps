6c25d993c4295277cdedd7aca90bfe39abbbddb5
Jerad Rutnam <jeradrutnam@outlook.com>
2025-01-06 14:09:38 +0530

    Fix rules data sample object key order

--- features/admin.rules.v1/data.tsx
+++ features/admin.rules.v1/data.tsx
@@ -18,134 +18,126 @@
 
 import { RuleComponentMetaDataInterface, RuleInterface } from "./models/rules";
 
-/* eslint-disable sort-keys */
-
 export const sampleRules: RuleInterface[] = [ {
-    "id": "e9a500d4-12dd-4b87-ad7c-9efdacf8a2a5",
-    "execution": "totp",
-    "conditions": [
+    conditions: [
         {
-            "id": "c90293c7-3fc1-465e-a946-adab6170e2fcdc",
-            "condition": "AND",
-            "order": 0,
-            "expressions": [
+            condition: "AND",
+            expressions: [
                 {
-                    "id": "8cbe103e-4c86-4021-91e4-83c37effe893a9267753",
-                    "field": "application",
-                    "operator": "equals",
-                    "value": "c90293c7-3fc1-465e-a946",
-                    "order": 0
+                    field: "application",
+                    id: "8cbe103e-4c86-4021-91e4-83c37effe893a9267753",
+                    operator: "equals",
+                    order: 0,
+                    value: "c90293c7-3fc1-465e-a946"
                 }
-            ]
+            ],
+            id: "c90293c7-3fc1-465e-a946-adab6170e2fcdc",
+            order: 0
         },
         {
-            "id": "c90293c7-3fc1-465e-a946-adab6170e2dc443",
-            "condition": "AND",
-            "order": 0,
-            "expressions": [
+            condition: "AND",
+            expressions: [
                 {
-                    "id": "8cbe103e-4c86-4021-91e4-83dsfc37fe89ee3a9",
-                    "field": "grantType",
-                    "operator": "equals",
-                    "value": "authorization_code",
-                    "order": 0
+                    field: "grantType",
+                    id: "8cbe103e-4c86-4021-91e4-83dsfc37fe89ee3a9",
+                    operator: "equals",
+                    order: 0,
+                    value: "authorization_code"
                 }
-            ]
+            ],
+            id: "c90293c7-3fc1-465e-a946-adab6170e2dc443",
+            order: 0
         },
         {
-            "id": "c90293c7-3fc1-465e-a946-adab6170e2dc",
-            "condition": "OR",
-            "order": 0,
-            "expressions": [
+            condition: "OR",
+            expressions: [
                 {
-                    "id": "8cbe103e-4c86-4021-91e4-83cdsf37fe893a9",
-                    "field": "grantType",
-                    "operator": "notEquals",
-                    "value": "client_credentials",
-                    "order": 0
+                    field: "grantType",
+                    id: "8cbe103e-4c86-4021-91e4-83cdsf37fe893a9",
+                    operator: "notEquals",
+                    order: 0,
+                    value: "client_credentials"
                 }
-            ]
+            ],
+            id: "c90293c7-3fc1-465e-a946-adab6170e2dc",
+            order: 0
         },
         {
-            "id": "c90293c7-3fc1-465e-a946-adab61e3270e2dc",
-            "condition": "OR",
-            "order": 0,
-            "expressions": [
+            condition: "OR",
+            expressions: [
                 {
-                    "id": "8cbe103e-4c86-4021-91e4-83c3fdsff437fe893a9",
-                    "field": "application",
-                    "operator": "equals",
-                    "value": "8cbe103e-4c86-4021",
-                    "order": 0
+                    field: "application",
+                    id: "8cbe103e-4c86-4021-91e4-83c3fdsff437fe893a9",
+                    operator: "equals",
+                    order: 0,
+                    value: "8cbe103e-4c86-4021"
                 }
-            ]
+            ],
+            id: "c90293c7-3fc1-465e-a946-adab61e3270e2dc",
+            order: 0
         }
-    ]
+    ],
+    execution: "totp",
+    id: "e9a500d4-12dd-4b87-ad7c-9efdacf8a2a5"
 } ];
 
 export const sampleApplicationList: any = {
-    "totalResults": 2,
-    "startIndex": 1,
-    "count": 2,
-    "applications": [
+    applications: [
         {
-            "id": "e028224b-51d1-4b26-a0f0-6e41f45472aa",
-            "name": "Console",
-            "description": "This is the console application.",
-            "applicationVersion": "v2.0.0",
-            "accessUrl": "https://localhost:9443/console",
-            "access": "READ",
-            "self": "/api/server/v1/applications/e028224b-51d1-4b26-a0f0-6e41f45472aa"
+            access: "READ",
+            accessUrl: "https://localhost:9443/console",
+            applicationVersion: "v2.0.0",
+            description: "This is the console application.",
+            id: "e028224b-51d1-4b26-a0f0-6e41f45472aa",
+            name: "Console",
+            self: "/api/server/v1/applications/e028224b-51d1-4b26-a0f0-6e41f45472aa"
         },
         {
-            "id": "dc2bf156-008e-4cd2-afb1-e31825277429",
-            "name": "My Account",
-            "description": "This is the my account application.",
-            "applicationVersion": "v2.0.0",
-            "accessUrl": "https://localhost:9443/myaccount",
-            "access": "WRITE",
-            "self": "/api/server/v1/applications/dc2bf156-008e-4cd2-afb1-e31825277429"
+            access: "WRITE",
+            accessUrl: "https://localhost:9443/myaccount",
+            applicationVersion: "v2.0.0",
+            description: "This is the my account application.",
+            id: "dc2bf156-008e-4cd2-afb1-e31825277429",
+            name: "My Account",
+            self: "/api/server/v1/applications/dc2bf156-008e-4cd2-afb1-e31825277429"
         }
     ],
-    "links": []
+    count: 2,
+    links: [],
+    startIndex: 1,
+    totalResults: 2
 };
 
+// TODO: Replace this with the actual data from the API.
 export const sampleExecutionsList: any = [
     {
-        "displayName": "TOTP",
-        "value": "totp"
+        displayName: "Prompt TOTP Page",
+        value: "dc2bf156-008e-4cdws2-afb1"
     },
     {
-        "displayName": "Basic",
-        "value": "basic"
-    },
-    {
-        "displayName": "Account Link",
-        "value": "account-link"
+        displayName: "Rule Conditions 3",
+        value: "dc2bf156-008e-4cd2-afb1sa"
     }
 ];
 
 export const sampleExpressionsMeta: RuleComponentMetaDataInterface = [
     {
         field: {
-            name: "application",
-            displayName: "application"
+            displayName: "application",
+            name: "application"
         },
         operators: [
             {
-                name: "equals",
-                displayName: "= Equals"
+                displayName: "= Equals",
+                name: "equals"
             },
             {
-                name: "notEquals",
-                displayName: "!= Not equals"
+                displayName: "!= Not equals",
+                name: "notEquals"
             }
         ],
         value: {
             inputType: "options",
-            valueType: "reference",
-            valueReferenceAttribute: "id",
-            valueDisplayAttribute: "name",
             links: [
                 {
                     href: "/applications?offset=0&limit=10",
@@ -157,22 +149,25 @@ export const sampleExpressionsMeta: RuleComponentMetaDataInterface = [
                     method: "GET",
                     rel: "filter"
                 }
-            ]
+            ],
+            valueDisplayAttribute: "name",
+            valueReferenceAttribute: "id",
+            valueType: "reference"
         }
     },
     {
         field: {
-            name: "grantType",
-            displayName: "grant type"
+            displayName: "grant type",
+            name: "grantType"
         },
         operators: [
             {
-                name: "equals",
-                displayName: "= Equals"
+                displayName: "= Equals",
+                name: "equals"
             },
             {
-                name: "notEquals",
-                displayName: "!= Not equals"
+                displayName: "!= Not equals",
+                name: "notEquals"
             }
         ],
         value: {
@@ -180,24 +175,24 @@ export const sampleExpressionsMeta: RuleComponentMetaDataInterface = [
             valueType: "string",
             values: [
                 {
-                    name: "authorization_code",
-                    displayName: "authorization code"
+                    displayName: "authorization code",
+                    name: "authorization_code"
                 },
                 {
-                    name: "password",
-                    displayName: "password"
+                    displayName: "password",
+                    name: "password"
                 },
                 {
-                    name: "refresh_token",
-                    displayName: "refresh token"
+                    displayName: "refresh token",
+                    name: "refresh_token"
                 },
                 {
-                    name: "client_credentials",
-                    displayName: "client credentials"
+                    displayName: "client credentials",
+                    name: "client_credentials"
                 },
                 {
-                    name: "urn:ietf:params:oauth:grant-type:token-exchange",
-                    displayName: "token exchange"
+                    displayName: "token exchange",
+                    name: "urn:ietf:params:oauth:grant-type:token-exchange"
                 }
             ]
         }
