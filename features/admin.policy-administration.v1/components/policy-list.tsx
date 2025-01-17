b08d38b185386905ff187142bfd07bcb0bf67a87
Jihan Jeeth <50905371+JeethJJ@users.noreply.github.com>
2025-01-17 13:19:16 +0530

    Update features/admin.policy-administration.v1/components/policy-list.tsx
    Co-authored-by: Pavindu Lakshan <pavindulakshan@gmail.com>

--- features/admin.policy-administration.v1/components/policy-list.tsx
+++ features/admin.policy-administration.v1/components/policy-list.tsx
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import { DroppableContainer, GetDragItemProps, useDnD  } from "@oxygen-ui/react/dnd";
 import {
     IdentifiableComponentInterface
