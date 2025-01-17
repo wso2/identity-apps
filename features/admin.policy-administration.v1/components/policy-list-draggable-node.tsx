e0d554132b795aed9ff37945ff1f038bbbaf6a26
Jihan Jeeth <50905371+JeethJJ@users.noreply.github.com>
2025-01-17 10:56:58 +0530

    Update features/admin.policy-administration.v1/components/policy-list-draggable-node.tsx
    Co-authored-by: Pavindu Lakshan <pavindulakshan@gmail.com>

--- features/admin.policy-administration.v1/components/policy-list-draggable-node.tsx
+++ features/admin.policy-administration.v1/components/policy-list-draggable-node.tsx
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import Card from "@oxygen-ui/react/Card";
 import CardContent from "@oxygen-ui/react/CardContent";
 import Stack from "@oxygen-ui/react/Stack";
