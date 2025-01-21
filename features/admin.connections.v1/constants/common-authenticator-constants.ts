4aff8193d270a2d8f17f2ef1907139f062cf827b
JayaShakthi97 <jayashakthiperera@gmail.com>
2025-01-21 10:39:16 +0530

    Fix eslint inconsistency

--- features/admin.connections.v1/constants/common-authenticator-constants.ts
+++ features/admin.connections.v1/constants/common-authenticator-constants.ts
@@ -44,6 +44,7 @@ export class CommonAuthenticatorConstants {
         ORGANIZATION_ENTERPRISE_IDP: string;
         SAML: string;
         SWE: string;
+        TEST_CONNECTOR: string;
         TRUSTED_TOKEN_ISSUER: string;
     } = {
             APPLE: "apple-idp",
@@ -60,6 +61,7 @@ export class CommonAuthenticatorConstants {
             ORGANIZATION_ENTERPRISE_IDP: "organization-enterprise-idp",
             SAML: "enterprise-saml-idp",
             SWE: "swe-idp",
+            TEST_CONNECTOR: "test-connector",
             TRUSTED_TOKEN_ISSUER: "trusted-token-issuer"
         };
 
