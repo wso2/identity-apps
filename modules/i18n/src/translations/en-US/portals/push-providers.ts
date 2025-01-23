6cf1ddd509eb5dc56df6635d3b8dfd60aa61b898
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-01-23 15:47:38 +0530

    Update modules/i18n/src/translations/en-US/portals/push-providers.ts

--- modules/i18n/src/translations/en-US/portals/push-providers.ts
+++ modules/i18n/src/translations/en-US/portals/push-providers.ts
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import { pushProvidersNS } from "../../../models";
 
 export const pushProviders: pushProvidersNS = {
