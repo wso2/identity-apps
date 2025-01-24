d4bb1ad391418bd2c3d8365080ccf50566425481
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-01-24 11:11:51 +0530

    Update features/admin.push-providers.v1/api/push-provider.ts

--- features/admin.push-providers.v1/api/push-provider.ts
+++ features/admin.push-providers.v1/api/push-provider.ts
@@ -17,7 +17,7 @@
  */
 
 import { AsgardeoSPAClient, HttpRequestConfig, HttpResponse } from "@asgardeo/auth-react";
-import { store } from "@wso2is/admin.core.v1";
+import { store } from "@wso2is/admin.core.v1/store";
 import { RequestConfigInterface } from "@wso2is/admin.core.v1/hooks/use-request";
 import { IdentityAppsApiException } from "@wso2is/core/exceptions";
 import { AcceptHeaderValues, ContentTypeHeaderValues, HttpMethods } from "@wso2is/core/models";
