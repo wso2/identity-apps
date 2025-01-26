af7ff78804c93be32330b5ffb9c3597246add67b
osandamaleesha <osandamaleesha@gmail.com>
2025-01-27 01:44:47 +0530

    Fix lint issue

--- features/admin.actions.v1/pages/actions.tsx
+++ features/admin.actions.v1/pages/actions.tsx
@@ -268,7 +268,7 @@ export const ActionTypesListingPage: FunctionComponent<ActionTypesListingPageInt
                 icon: <ProfileFlowIcon size="small" className="icon"/>,
                 identifier: ActionsConstants.PRE_UPDATE_PROFILE_URL_PATH,
                 route: isActionTypeDisabled(FeatureFlagConstants.FEATURE_FLAG_KEY_MAP
-                    .ACTIONS_TYPES_PRE_UPDATE_PASSWORD) ? undefined 
+                    .ACTIONS_TYPES_PRE_UPDATE_PASSWORD) ? undefined
                     : AppConstants.getPaths().get("PRE_UPDATE_PROFILE_EDIT")
             },
             {
