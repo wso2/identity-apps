ace4bcd350a030a5ce3c5b0f3ca94df2866b99b6
DonOmalVindula <omalvindula@live.com>
2025-02-07 12:18:15 +0530

    Remove whitespace

--- features/admin.server-configurations.v1/pages/account-disable-page.tsx
+++ features/admin.server-configurations.v1/pages/account-disable-page.tsx
@@ -77,8 +77,6 @@ export const AccountDisablePage: FC<AccountDisablePageInterface> = (
             return;
         }
 
-        console.log(accountDisableProperty);
-
         setIsAccountDisableEnabled(accountDisableProperty?.value === "true");
     }, [ accountDisableConnectorData ]);
 
