5a9faebe2832d457c3b645600ecb9a84b29d26a4
Pavindu Lakshan <pavindu@wso2.com>
2025-02-07 16:49:01 +0530

    Update modules/i18n/src/translations/en-US/portals/tenants.ts

--- modules/i18n/src/translations/en-US/portals/tenants.ts
+++ modules/i18n/src/translations/en-US/portals/tenants.ts
@@ -316,7 +316,7 @@ export const tenants: TenantsNS = {
         deActivate: "Disable",
         deActivated: "Disabled"
     },
-    subtitle: "Create and manage root organizations for enhanced separation and control.",
+    subtitle: "Create and manage your server by creating new root level organizations in your workspace.",
     systemSettings: {
         actions: {
             newTenant: {
