eaecbc9191312b4ff579d12d754dca477816d7bc
NipuniBhagya <nipunib@wso2.com>
2025-02-13 23:13:58 +0530

    Update password field

--- identity-apps-core/react-ui-core/src/components/adapters/password-field-adapter.js
+++ identity-apps-core/react-ui-core/src/components/adapters/password-field-adapter.js
@@ -21,15 +21,21 @@ import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
 import useFieldValidation from "../../hooks/use-field-validations";
 import { useTranslations } from "../../hooks/use-translations";
-import { getTranslationByKey } from "../../utils/i18n-utils";
-import ValidationCriteria from "../field-validation";
+import { resolveElementText } from "../../utils/i18n-utils";
+import ValidationCriteria from "../validation-criteria";
+import ValidationError from "../validation-error";
 
-const PasswordFieldAdapter = ({ component, formStateHandler, formErrorHandler }) => {
+const PasswordFieldAdapter = ({ component, formState, formStateHandler, formErrorHandler }) => {
 
     const { required, name, label, placeholder, rest, validation } = component.properties;
 
     const { translations } = useTranslations();
     const { fieldErrors, validate } = useFieldValidation(validation);
+
+    const fieldError = formState && formState.errors && formState.errors.length > 0 &&
+        formState.errors.filter(error => error.label === component.properties.name);
+    const error = fieldError ? fieldError[0] && fieldError[0].error :
+        fieldErrors && fieldErrors.length >= 0 && fieldErrors[0] && fieldErrors[0].error;
 
     const [ password, setPassword ] = useState("");
     const [ showPassword, setShowPassword ] = useState(false);
@@ -38,7 +44,9 @@ const PasswordFieldAdapter = ({ component, formStateHandler, formErrorHandler })
         formStateHandler(name, password);
     }, [ password ]);
 
-    const handleFieldValidation = (value) => {
+    const handlePasswordChange = (value) => {
+        setPassword(value);
+
         if (validation) {
             validate(value);
             formErrorHandler(component.properties.name, fieldErrors);
@@ -46,35 +54,43 @@ const PasswordFieldAdapter = ({ component, formStateHandler, formErrorHandler })
     };
 
     return (
-        <div
-            id="passwordField"
-            className={ classNames("field", required ? "required" : null) }
-        >
-            <label>{ getTranslationByKey(translations, label) }</label>
-            <div className="ui fluid left icon input addon-wrapper">
-                <input
-                    className="form-control"
-                    name={ name }
-                    type={ showPassword ? "text" : "password" }
-                    id="password"
-                    value={ password }
-                    placeholder={ getTranslationByKey(translations, placeholder) }
-                    onChange={ (e) => setPassword(e.target.value) }
-                    onBlur={ (e) => handleFieldValidation(e.target.value) }
-                    { ...rest }
-                />
-                <i aria-hidden="true" className="lock icon"></i>
-                <i
-                    id="password-eye"
-                    className={ classNames("eye icon right-align password-toggle", { "slash": !showPassword } ) }
-                    onClick={ () => setShowPassword(!showPassword) }
-                />
+        <>
+            <div
+                id="passwordField"
+                className={ classNames("field", required ? "required" : null) }
+            >
+                <label>{ resolveElementText(translations, label) }</label>
+                <div className="ui fluid left icon input addon-wrapper">
+                    <input
+                        className="form-control"
+                        name={ name }
+                        type={ showPassword ? "text" : "password" }
+                        id="password"
+                        value={ password }
+                        placeholder={ resolveElementText(translations, placeholder) }
+                        onChange={ (e) => handlePasswordChange(e.target.value) }
+                        { ...rest }
+                    />
+                    <i aria-hidden="true" className="lock icon"></i>
+                    <i
+                        id="password-eye"
+                        className={ classNames("eye icon right-align password-toggle", { "slash": !showPassword } ) }
+                        onClick={ () => setShowPassword(!showPassword) }
+                    />
+                </div>
+                {
+                    validation && validation.type === "CRITERIA" && validation.showValidationCriteria (
+                        <ValidationCriteria validationConfig={ validation } errors={ fieldErrors } value={ password } />
+                    )
+                }
+                {
+                    (fieldErrors.length > 0 || formState.errors.length > 0) &&  (
+                        <ValidationError error={ error } />
+                    )
+                }
+                <div className="ui divider hidden"></div>
             </div>
-            { validation && (
-                <ValidationCriteria validationConfig={ validation } errors={ fieldErrors } value={ password } />
-            ) }
-            <div className="ui divider hidden"></div>
-        </div>
+        </>
     );
 };
 
