ac0f070dd969369b75ec2b0b76b7fcb5d92a18bb
Jayashakthi Perera <jayashakthiperera@gmail.com>
2025-02-16 08:43:31 +0530

    Update change-action-status.ts

--- features/admin.actions.v1/api/change-action-status.ts
+++ features/admin.actions.v1/api/change-action-status.ts
@@ -16,6 +16,8 @@
  * under the License.
  */
 
+
+
 import { AsgardeoSPAClient, HttpClientInstance } from "@asgardeo/auth-react";
 import { RequestConfigInterface } from "@wso2is/admin.core.v1/hooks/use-request";
 import { store } from "@wso2is/admin.core.v1/store";
