9d901d1cd20ae88f4744bf621303e9ed0430cf3f
Brion <info@brionmario.com>
2025-03-04 17:38:14 +0530

    Add dnd components

---
+++ features/admin.flow-builder-core.v1/components/dnd/action.tsx
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import classNames from "classnames";
+import React, { CSSProperties, Ref, forwardRef } from "react";
+import "./action.scss";
+
+export interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+  variant?: "light" | "dark" | "destructive";
+  cursor?: CSSProperties["cursor"];
+}
+
+export const Action = forwardRef<HTMLButtonElement, Props>(
+    ({ className, cursor, style, variant = "light", ...props }, ref) => {
+        return (
+            <button
+                ref={ ref }
+                { ...props }
+                className={ classNames("dnd-action", className) }
+                style={
+          {
+              ...style,
+              cursor
+          } as CSSProperties
+                }
+            />
+        );
+    }
+);
---
+++ features/admin.flow-builder-core.v1/components/dnd/sortable.tsx
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { RestrictToVerticalAxis } from "@dnd-kit/abstract/modifiers";
+import { closestCorners } from "@dnd-kit/collision";
+import { UseSortableInput, useSortable } from "@dnd-kit/react/sortable";
+import Box from "@oxygen-ui/react/Box";
+import { IdentifiableComponentInterface } from "@wso2is/core/models";
+import classNames from "classnames";
+import React, { FC, PropsWithChildren, ReactElement, RefObject, useRef } from "react";
+
+/**
+ * Props interface of {@link Sortable}
+ */
+export interface SortableProps extends IdentifiableComponentInterface, UseSortableInput {
+    /**
+     * Handle reference.
+     */
+    handleRef?: RefObject<HTMLElement>;
+}
+
+/**
+ * Sortable component.
+ *
+ * @param props - Props injected to the component.
+ * @returns Sortable component.
+ */
+const Sortable: FC<PropsWithChildren<SortableProps>> = ({
+    "data-componentid": componentId = "sortable",
+    id,
+    index,
+    children,
+    handleRef,
+    collisionDetector = closestCorners,
+    ...rest
+}: PropsWithChildren<SortableProps>): ReactElement => {
+    const { ref, isDragging } = useSortable({
+        id,
+        index,
+        modifiers: [ RestrictToVerticalAxis ],
+        handle: handleRef,
+        collisionDetector,
+        feedback: "default",
+        ...rest
+    });
+
+    return (
+        <Box
+            ref={ ref }
+            data-componentid={ componentId }
+            sx={ { height: "100%", width: "100%" } }
+            className={ classNames("dnd-sortable", { "is-dragging": isDragging }) }
+        >
+            { children }
+        </Box>
+    );
+};
+
+export default Sortable;
