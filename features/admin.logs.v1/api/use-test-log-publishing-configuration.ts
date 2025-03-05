635d7203af50a867be91cc6228eb6f4fad8638d4
Avarjana <acpasavarjana@gmail.com>
2025-03-05 11:33:00 +0530

    Remote log publish UI APIs

---
+++ features/admin.logs.v1/api/use-test-log-publishing-configuration.ts
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import useRequest, {
+    RequestConfigInterface,
+    RequestErrorInterface,
+    RequestResultInterface
+} from "@wso2is/admin.core.v1/hooks/use-request";
+import { store } from "@wso2is/admin.core.v1/store";
+import { HttpMethods } from "@wso2is/core/models";
+import {LogType} from "../models/remote-log-publishing";
+
+/**
+ * Hook to get the remote log publishing configurations.
+ *
+ * @param shouldFetch - Should fetch the configurations.
+ * @param logType
+ * @returns remote log publishing configurations.
+ */
+const useTestRemoteLogPublishingConfiguration = <
+    Error = RequestErrorInterface
+>(
+        shouldFetch: boolean = true,
+        logType: LogType
+    ): RequestResultInterface<Error> => {
+    const requestConfig: RequestConfigInterface = {
+        headers: {
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Content-Type": "application/json",
+            "Expires": "0",
+            "Pragma": "no-cache"
+        },
+        method: HttpMethods.GET,
+        url: `${store.getState().config.endpoints.remoteLogging}/${logType}/test`
+    };
+
+    const { data, error, isValidating, mutate } = useRequest<Error>(shouldFetch ? requestConfig : null);
+
+    return {
+        data,
+        error: error,
+        isLoading: isValidating && !error,
+        isValidating,
+        mutate: mutate
+    };
+};
+
+export default useTestRemoteLogPublishingConfiguration;
