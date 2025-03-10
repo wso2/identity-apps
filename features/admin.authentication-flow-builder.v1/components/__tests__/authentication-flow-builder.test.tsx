4f563fa38b7c32423e6e3b3a08f407d70a4ff3c5
Brion <info@brionmario.com>
2025-03-10 18:12:05 +0530

    Fix unit tests

--- features/admin.authentication-flow-builder.v1/components/__tests__/authentication-flow-builder.test.tsx
+++ features/admin.authentication-flow-builder.v1/components/__tests__/authentication-flow-builder.test.tsx
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import UserPreferenceProvider from "@wso2is/admin.core.v1/providers/user-preferences-provider";
+import UserPreferenceProvider from "@wso2is/common.ui.v1/providers/user-preferences-provider";
 import { render, screen } from "@wso2is/unit-testing/utils";
 import React from "react";
 import "@testing-library/jest-dom";
--- features/admin.authentication-flow-builder.v1/components/__tests__/authentication-flow-option-add-modal.test.tsx
+++ features/admin.authentication-flow-builder.v1/components/__tests__/authentication-flow-option-add-modal.test.tsx
@@ -20,7 +20,7 @@
 
 import DeploymentConfigProvider from "@wso2is/admin.core.v1/providers/deployment-config-provider";
 import ResourceEndpointsProvider from "@wso2is/admin.core.v1/providers/resource-enpoints-provider";
-import UserPreferenceProvider from "@wso2is/admin.core.v1/providers/user-preferences-provider";
+import UserPreferenceProvider from "@wso2is/common.ui.v1/providers/user-preferences-provider";
 import { render, screen } from "@wso2is/unit-testing/utils";
 import React from "react";
 import "@testing-library/jest-dom";
