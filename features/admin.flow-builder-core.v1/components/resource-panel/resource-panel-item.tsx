1186df14b83a2b6eed3a731bef8e32242e84c8c6
NipuniBhagya <nipunib@wso2.com>
2025-03-13 02:18:23 +0530

    Remove unused imports

--- features/admin.flow-builder-core.v1/components/resource-panel/resource-panel-item.tsx
+++ features/admin.flow-builder-core.v1/components/resource-panel/resource-panel-item.tsx
@@ -25,10 +25,7 @@ import Stack from "@oxygen-ui/react/Stack";
 import Typography from "@oxygen-ui/react/Typography";
 import { PlusIcon } from "@oxygen-ui/react-icons";
 import { IdentifiableComponentInterface } from "@wso2is/core/models";
-import classNames from "classnames";
 import React, { FunctionComponent, HTMLAttributes, ReactElement } from "react";
-import { SupportedCanvasResources } from "../../models/visual-flow";
-import Draggable from "../dnd/draggable";
 import { Resource } from "../../models/resources";
 import "./resource-panel-item.scss";
 
