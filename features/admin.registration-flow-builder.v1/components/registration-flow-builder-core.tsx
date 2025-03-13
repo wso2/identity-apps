4ca5cc14f98c56032f13a02a1515f19c02e1f10a
Brion <info@brionmario.com>
2025-03-13 18:08:40 +0530

    Stop resetting the flow on AI template select

--- features/admin.registration-flow-builder.v1/components/registration-flow-builder-core.tsx
+++ features/admin.registration-flow-builder.v1/components/registration-flow-builder-core.tsx
@@ -851,6 +851,15 @@ const RegistrationFlowBuilderCore: FunctionComponent<RegistrationFlowBuilderCore
         return step;
     };
 
+    const handleResourceAdd = (resource: Resource): void => {
+        // If the template type is `GENERATE_WITH_AI`, show the AI generation modal.
+        if (resource.type === TemplateTypes.GeneratedWithAI) {
+            setShowAIGenerationModal(true);
+
+            return;
+        }
+    };
+
     if (isRegistrationFlowFetchRequestLoading || isRegistrationFlowFetchRequestValidating) {
         return null;
     }
@@ -866,6 +875,7 @@ const RegistrationFlowBuilderCore: FunctionComponent<RegistrationFlowBuilderCore
                 onTemplateLoad={ handleTemplateLoad }
                 onWidgetLoad={ handleWidgetLoad }
                 onStepLoad={ handleStepLoad }
+                onResourceAdd={ handleResourceAdd }
                 nodes={ nodes }
                 edges={ edges }
                 setNodes={ setNodes }
