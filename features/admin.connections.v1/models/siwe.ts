b8b35188ff4fde5f95ec27a067e1558d9375bb7a
Brion <info@brionmario.com>
2025-04-16 23:39:35 +0530

    Refactor `SIWEConstants`

--- features/admin.connections.v1/models/siwe.ts
+++ features/admin.connections.v1/models/siwe.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2022-2025, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -16,4 +16,16 @@
  * under the License.
  */
 
-export * from "./sms-otp-constants";
+/**
+ * Interface for the Sign In With Ethereum registration API response.
+ */
+export interface SIWERegistrationAPIResponseInterface {
+    /**
+     * Client ID of the created OIDC application.
+     */
+    client_id: string;
+    /**
+     * Client secret of the created OIDC application.
+     */
+    client_secret: string;
+}
