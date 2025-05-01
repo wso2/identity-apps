4dfaa76dbfa9e4f3e6fa9e59297c09060f97797c
Shenali <shenali@wso2.com>
2025-05-02 02:16:04 +0530

    Update clientID of portals at new tenant creation

--- identity-apps-core/components/org.wso2.identity.apps.common/src/main/java/org/wso2/identity/apps/common/util/AppPortalUtils.java
+++ identity-apps-core/components/org.wso2.identity.apps.common/src/main/java/org/wso2/identity/apps/common/util/AppPortalUtils.java
@@ -38,6 +38,7 @@ import org.wso2.carbon.identity.application.mgt.ApplicationManagementService;
 import org.wso2.carbon.identity.application.mgt.ApplicationMgtUtil;
 import org.wso2.carbon.identity.core.URLBuilderException;
 import org.wso2.carbon.identity.core.util.IdentityUtil;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.oauth.IdentityOAuthAdminException;
 import org.wso2.carbon.identity.oauth.OAuthUtil;
 import org.wso2.carbon.identity.oauth.dto.OAuthConsumerAppDTO;
@@ -310,7 +311,7 @@ public class AppPortalUtils {
                 List<String> allowedGrantTypes = Arrays.asList(AppsCommonDataHolder.getInstance()
                     .getOAuthAdminService().getAllowedGrantTypes());
                 grantTypes = grantTypes.stream().filter(allowedGrantTypes::contains).collect(Collectors.toList());
-                String consumerKey = appPortal.getConsumerKey();
+                String consumerKey = resolveClientID(appPortal.getConsumerKey(), tenantInfoBean.getTenantDomain());
                 try {
                     AppPortalUtils.createOAuth2Application(appPortal.getName(), appPortal.getPath(), consumerKey,
                         consumerSecret, tenantInfoBean.getAdmin(), tenantInfoBean.getTenantId(),
@@ -419,6 +420,22 @@ public class AppPortalUtils {
         } finally {
             IdentityApplicationManagementUtil.removeAllowUpdateSystemApplicationThreadLocal();
             PrivilegedCarbonContext.endTenantFlow();
+        }
+    }
+
+    /**
+     * Resolve tenant qualified client ID.
+     *
+     * @param consumerKey consumer key.
+     * @param tenantDomain tenant domain.
+     * @return client ID.
+     */
+    public static String resolveClientID(String consumerKey, String tenantDomain) {
+
+        if (IdentityTenantUtil.isTenantQualifiedUrlsEnabled() || SUPER_TENANT_DOMAIN_NAME.equals(tenantDomain)) {
+            return consumerKey;
+        } else {
+            return consumerKey + "_" + tenantDomain;
         }
     }
 
