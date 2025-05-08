58967838352b45cd35733dbbd15e2f7c727bbd29
samudrauduwaka <samudrauduwaka@gmail.com>
2025-05-08 13:20:29 +0530

    Styling and Formatting checks

--- features/admin.branding.v1/components/custom-page-editor-page-layout.tsx
+++ features/admin.branding.v1/components/custom-page-editor-page-layout.tsx
@@ -16,17 +16,17 @@
  * under the License.
  */
 
+import { MenuItem } from "@mui/material";
 import Button from "@oxygen-ui/react/Button";
+import Divider from "@oxygen-ui/react/Divider";
+import Select from "@oxygen-ui/react/Select";
+import { ArrowUpRightFromSquareIcon } from "@oxygen-ui/react-icons";
 import { TestableComponentInterface } from "@wso2is/core/models";
 import React, { FunctionComponent, ReactElement, useState } from "react";
 import { RouteComponentProps } from "react-router";
+import { Segment } from "semantic-ui-react";
 import { EditorViewTabs } from "./custom-page-editor/editor-view";
 import useCustomPageEditor from "../hooks/use-custom-page-editor";
-import { Segment } from "semantic-ui-react";
-import Select from '@oxygen-ui/react/Select';
-import { MenuItem } from "@mui/material";
-import Divider from '@oxygen-ui/react/Divider';
-import { ArrowUpRightFromSquareIcon } from "@oxygen-ui/react-icons";
 
 type CustomPageEditorPageLayoutPropsInterface = TestableComponentInterface;
 
@@ -35,33 +35,45 @@ interface RouteParams {
     templateId: string;
 }
 
-const templateOptions = [
-    { key: 'centered', value: 'centered', text: 'Centred' },
-    { key: 'left', value: 'left', text: 'Left-aligned' },
-    { key: 'right', value: 'right', text: 'Right-aligned' },
-    { key: 'left-image', value: 'left-image', text: 'Left-image' },
-    { key: 'right-image', value: 'right-image', text: 'Right-image' },
+interface TemplateOptions {
+    key: string;
+    value: string;
+    text: string;
+}
+
+interface UpdatedContent {
+    html: string;
+    css: string;
+    js: string;
+}
+
+const templateOptions: TemplateOptions[] = [
+    { key: "centered", text: "Centered", value: "centered" },
+    { key: "left", text: "Left-aligned", value: "left" },
+    { key: "right", text: "Right-aligned", value: "right" },
+    { key: "left-image", text: "Left-image", value: "left-image" },
+    { key: "right-image", text: "Right-image", value: "right-image" }
 ];
 
 const CustomPageEditorPageLayout: FunctionComponent<CustomPageEditorPageLayoutPropsInterface> = (
     props: CustomPageEditorPageLayoutPropsInterface & RouteComponentProps<RouteParams>
 ): ReactElement => {
 
-    const [template, setTemplate] = useState('centered');
+    const [ , setTemplate ] = useState("centered");
 
     const {
         [ "data-testid" ]: testId
     } = props;
 
-    const { customLayoutMode , setCustomLayoutMode } = useCustomPageEditor();
+    const { setCustomLayoutMode } = useCustomPageEditor();
 
     const handleBackButtonClick = (): void => {
         setCustomLayoutMode(false);
     };
 
-    const [html, setHtml] = useState("<div>Hello</div>");
-    const [css, setCss] = useState("body { background: white; }");
-    const [js, setJs] = useState("console.log('Hi');");
+    const [ html, setHtml ] = useState("<div>Hello</div>");
+    const [ css, setCss ] = useState("body { background: white; }");
+    const [ js, setJs ] = useState("console.log('Hi');");
 
     const handleReset = () => {
         setHtml("<div>Hello</div>");
@@ -72,61 +84,67 @@ const CustomPageEditorPageLayout: FunctionComponent<CustomPageEditorPageLayoutPr
     return (
         <div>
             <div
-                className="back-button"
-                data-testid="user-mgt-edit-user-back-button"
-                onClick={ handleBackButtonClick }
-                style={ {
+                className = "back-button"
+                data-testid = "user-mgt-edit-user-back-button"
+                onClick = { handleBackButtonClick }
+                style = { {
                     cursor: "pointer"
                 } }
             >
                 <i aria-hidden="true" className="arrow left icon"></i>
                     Go back
             </div>
-            <div style={{ position: 'relative'}}>
-                <div style={{ position: 'absolute', top: '0', right: '0', padding: '6px',zIndex: '5' }}>
-                    <span style={{ marginRight: '1em' }}>Start With</span>
+            <div style = { { position: "relative" } }>
+                <div style = { { padding: "6px", position: "absolute", right: "0", top: "0",zIndex: "5" } }>
+                    <span style = { { marginRight: "1em" } }> Start With </span>
                     <Select
-                        defaultValue={templateOptions[0].value}
-                        variant="outlined"
-                        onChange={ (event) => {
+                        defaultValue = { templateOptions[0].value }
+                        variant = "outlined"
+                        onChange = { ( event: React.ChangeEvent<HTMLInputElement> ) => {
                             setTemplate(event.target.value as string);
                         } }
-                        data-testid={ `${ testId }-template-select` }
-                        style={{ marginRight: '1em' ,width: '200px', height: '40px'}}
+                        data-testid = { `${ testId }-template-select` }
+                        style = { { height: "40px", marginRight: "1em" ,width: "200px" } }
                     >
-                        {templateOptions.map((option) => (
-                            <MenuItem key={option.key} value={option.value}>
-                                {option.text}
+                        { templateOptions.map(( option: TemplateOptions ) => (
+                            <MenuItem key = { option.key } value = { option.value }>
+                                { option.text }
                             </MenuItem>
-                        ))}
+                        ))
+                        }
                     </Select>
-                    <Button variant="outlined">Preview <span className="ml-2"><ArrowUpRightFromSquareIcon /></span></Button>
+                    <Button variant = "outlined">
+                        Preview
+                        <span className = "ml-2">
+                            <ArrowUpRightFromSquareIcon />
+                        </span>
+                    </Button>
                 </div>
-                <Segment style={{ paddingLeft: '0',paddingRight: '0'}}>
+                <Segment style = { { paddingLeft: "0", paddingRight: "0" } }>
                     <EditorViewTabs
-                        html={ html }
-                        css={ css }
-                        js={ js }
-                        onContentUpdate={ (updated) => {
+                        html = { html }
+                        css = { css }
+                        js = { js }
+                        onContentUpdate = { ( updated: UpdatedContent ) => {
                             console.log("Updated content:", updated);
                         } }
                     />
                     <Divider/>
-                    <div style={{ paddingLeft: '6px', paddingTop:'10px' }}>
+                    <div style = { { paddingLeft: "6px", paddingTop:"10px" } }>
                         <Button
-                            variant="contained"
-                            data-testid={ `${ testId }-save-button` }
-                            onClick={ () => {
+                            variant = "contained"
+                            data-testid = { `${ testId }-save-button` }
+                            onClick = { () => {
                                 console.log("Save and publish clicked");
                             } }>
                             Save and Publish
                         </Button>
                         <Button
-                            style={ { marginLeft: "4px" } }
-                            color="secondary"
-                            variant="contained"
-                            onClick={handleReset}
-                            data-testid={ `${ testId }-reset-button` }>
+                            style = { { marginLeft: "4px" } }
+                            color = "secondary"
+                            variant = "contained"
+                            onClick = { handleReset }
+                            data-testid = { `${ testId }-reset-button` }>
                             Reset to Default
                         </Button>
                     </div>
