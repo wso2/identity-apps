e3a76b1db8f3bc1f8b7fdc807d1c2f3dbe728b2d
JayaShakthi97 <jayashakthiperera@gmail.com>
2025-07-03 15:34:00 +0530

    Add doc comments

--- apps/myaccount/src/models/profile-ui.ts
+++ apps/myaccount/src/models/profile-ui.ts
@@ -20,20 +20,70 @@ import { IdentifiableComponentInterface, PatchOperationRequest, ProfileSchemaInt
 import { FormValue, Validation } from "@wso2is/forms";
 import { ProfilePatchOperationValue } from "./profile";
 
+/**
+ * Props interface of the profile field form renderer.
+ */
 export interface ProfileFieldFormRendererPropsInterface extends IdentifiableComponentInterface {
+    /**
+     * The schema of the attribute.
+     */
     fieldSchema: ProfileSchemaInterface;
+    /**
+     * The flattened profile schema. This is used in the email, mobile number fields.
+     */
     flattenedProfileSchema: ProfileSchemaInterface[];
+    /**
+     * The initial value of the attribute.
+     */
     initialValue: string;
+    /**
+     * The label to be displayed.
+     */
     fieldLabel: string;
+    /**
+     * Whether the field is in edit mode or not.
+     */
     isActive: boolean;
+    /**
+     * Whether the field is editable or not.
+     */
     isEditable: boolean;
+    /**
+     * Whether the attribute value is required or not.
+     */
     isRequired: boolean;
+    /**
+     * Callback to set the profile updating state.
+     *
+     * @param value - Profile updating state.
+     */
     setIsProfileUpdating: (value: boolean) => void;
+    /**
+     * Callback to trigger the profile update.
+     *
+     * @param data - Patch operation data.
+     */
     triggerUpdate: (data: PatchOperationRequest<ProfilePatchOperationValue>) => void;
+    /**
+     * Mapped user profile data. This is used in the email, mobile number fields.
+     * Ex: Retrieve the primary email address.
+     */
     profileInfo: Map<string, string>;
+    /**
+     * Whether email verification is enabled or not.
+     */
     isEmailVerificationEnabled: boolean;
+    /**
+     * Whether mobile number verification is enabled or not.
+     */
     isMobileVerificationEnabled: boolean;
+    /**
+     * Whether the profile is loading or not.
+     */
     isLoading: boolean;
+    /**
+     * Whether the profile is updating or not.
+     */
     isUpdating: boolean;
 }
 
@@ -46,30 +96,83 @@ export interface ProfileFieldFormPropsInterface
         | "isMobileVerificationEnabled"
         | "flattenedProfileSchema"
     > {
+    /**
+     * Callback to trigger the edit mode.
+     */
     onEditClicked: () => void;
+    /**
+     * Callback to close the edit mode.
+     */
     onEditCancelClicked: () => void;
+    /**
+     * Callback to submit the data.
+     *
+     * @param schemaName - Schema name.
+     * @param value - Value to be submitted.
+     */
     handleSubmit: (schemaName: string, value: FormValue) => void;
 }
 
 export interface TextFieldFormPropsInterface extends ProfileFieldFormPropsInterface {
+    /**
+     * Callback to validate the field. If not provided, the default validation will be used.
+     * In the default validation, the value will be validated against the schema regex.
+     *
+     * @param value - Value to be validated.
+     * @param validation - Validation object.
+     */
     onValidate?: (value: string, validation: Validation) => void;
+    /**
+     * Placeholder text to be displayed. If not provided, the default placeholder text will be used.
+     */
     placeholderText?: string;
+    /**
+     * Type of the input field. If not provided, "text" will be used.
+     */
     type?: "text" | "number";
+    /**
+     * Step value of the input field. Only used in the number field.
+     */
     step?: number | "any";
 }
 
 export interface CountryFieldFormPropsInterface extends ProfileFieldFormPropsInterface {}
 
 export interface EmailFieldFormPropsInterface extends Omit<ProfileFieldFormPropsInterface, "handleSubmit"> {
+    /**
+     * Mapped user profile data.
+     */
     profileInfo: Map<string, string>;
+    /**
+     * Whether the verification is enabled or not.
+     */
     isVerificationEnabled: boolean;
+    /**
+     * Callback to trigger the update.
+     *
+     * @param data - Patch operation data.
+     */
     triggerUpdate: (data: PatchOperationRequest<ProfilePatchOperationValue>) => void;
 }
 
 export interface MobileFieldFormPropsInterface extends Omit<ProfileFieldFormPropsInterface, "handleSubmit"> {
+    /**
+     * Mapped user profile data.
+     */
     profileInfo: Map<string, string>;
+    /**
+     * Whether the verification is enabled or not.
+     */
     isVerificationEnabled: boolean;
+    /**
+     * Callback to trigger the update.
+     *
+     * @param data - Patch operation data.
+     */
     triggerUpdate: (data: PatchOperationRequest<ProfilePatchOperationValue>) => void;
+    /**
+     * Flattened profile schema.
+     */
     flattenedProfileSchema: ProfileSchemaInterface[];
 }
 
@@ -85,6 +188,12 @@ export interface DropdownFieldFormPropsInterface extends ProfileFieldFormPropsIn
 
 export interface MultiValueFieldFormPropsInterface<T extends string | number>
     extends Omit<ProfileFieldFormPropsInterface, "initialValue"> {
+    /**
+     * Initial value of the field.
+     */
     initialValue: T[];
+    /**
+     * Type of the field. If not provided, "text" will be used.
+     */
     type?: string;
 }
