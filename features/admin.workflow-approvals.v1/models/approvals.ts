3828a3e4d042ce7c133f1816dd788ad8f1cf4d49
Malith-19 <malithdilshan91@gmail.com>
2025-07-04 17:13:52 +0530

    Fix lint issues.

--- features/admin.workflow-approvals.v1/models/approvals.ts
+++ features/admin.workflow-approvals.v1/models/approvals.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2019-2025, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -126,7 +126,6 @@ export interface ApprovalAction {
  * Enum for approval statuses.
  *
  * @readonly
- * @enum {string}
  */
 export enum ApprovalStatus {
     READY = "READY",
