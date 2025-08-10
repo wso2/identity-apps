4086e448d8745708696e933054c4b36dd1d97b5b
Hasini Samarathunga <hasinis@wso2.com>
2025-08-10 08:36:25 +0530

    Fix discovery config PUT call to include defaultParam

--- features/admin.organization-discovery.v1/constants/organization-discovery-config-constants.ts
+++ features/admin.organization-discovery.v1/constants/organization-discovery-config-constants.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2023-2025, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -27,6 +27,7 @@ export class OrganizationDiscoveryConfigConstants {
 
     public static readonly EMAIL_DOMAIN_DISCOVERY_PROPERTY_KEY: string = "emailDomain.enable";
     public static readonly EMAIL_DOMAIN_DISCOVERY_SELF_REG_PROPERTY_KEY: string = "emailDomainBasedSelfSignup.enable";
+    public static readonly DEFAULT_DISCOVERY_PARAM: string = "defaultParam";
 
     public static readonly ORGANIZATION_DISCOVERY_DOMAINS_NOT_CONFIGURED_ERROR_CODE: string = "OCM-60002";
 }
