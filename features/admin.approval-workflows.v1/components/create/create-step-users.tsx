ce9801463c203bd8c2869c5ebc4f23dcfc3a6bf2
Malith-19 <malith.19@cse.mrt.ac.lk>
2025-08-19 11:21:07 +0530

    Remove user store domain value for user list in workflow.

--- features/admin.approval-workflows.v1/components/create/create-step-users.tsx
+++ features/admin.approval-workflows.v1/components/create/create-step-users.tsx
@@ -22,7 +22,6 @@ import Autocomplete, {
 } from "@oxygen-ui/react/Autocomplete";
 import Grid from "@oxygen-ui/react/Grid";
 import TextField from "@oxygen-ui/react/TextField";
-import { AppState } from "@wso2is/admin.core.v1/store";
 import { GroupsInterface } from "@wso2is/admin.groups.v1/models/groups";
 import { AutoCompleteRenderOption } from
     "@wso2is/admin.roles.v2/components/edit-role/edit-role-common/auto-complete-render-option";
@@ -31,17 +30,8 @@ import { RoleConstants } from "@wso2is/admin.roles.v2/constants";
 import { RoleManagementUtils } from "@wso2is/admin.roles.v2/utils/role-management-utils";
 import { useUsersList } from "@wso2is/admin.users.v1/api";
 import { UserBasicInterface } from "@wso2is/admin.users.v1/models/user";
-import { getAUserStore } from "@wso2is/admin.userstores.v1/api/";
-import { useGetUserStores } from "@wso2is/admin.userstores.v1/api/use-get-user-stores";
-import {
-    UserStoreDropdownItem,
-    UserStoreListItem,
-    UserStorePostData,
-    UserStoreProperty
-} from "@wso2is/admin.userstores.v1/models/user-stores";
 import { AlertLevels, IdentifiableComponentInterface } from "@wso2is/core/models";
 import { addAlert } from "@wso2is/core/store";
-import { StringUtils } from "@wso2is/core/utils";
 import debounce, { DebouncedFunc } from "lodash-es/debounce";
 import isEmpty from "lodash-es/isEmpty";
 import React, {
@@ -54,9 +44,8 @@ import React, {
     useState
 } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "redux";
-import { userstoresConfig } from "../../../admin.extensions.v1";
 import { StepEditSectionsInterface } from "../../models/ui";
 
 type StepUsersPropsInterface = IdentifiableComponentInterface & StepEditSectionsInterface;
@@ -70,30 +59,19 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
         activeUserStore,
         onUsersChange,
         showValidationError,
-        ["data-componentid"]: componentId
-        = "workflow-model-approval-step-users"
+        ["data-componentid"]: componentId = "workflow-model-approval-step-users"
     } = props;
 
     const { t } = useTranslation();
     const dispatch: Dispatch = useDispatch();
 
-    const primaryUserStoreDomainName: string = useSelector(
-        (state: AppState) => state?.config?.ui?.primaryUserStoreDomainName
-    );
-
     const [ userSearchValue, setUserSearchValue ] = useState<string>(undefined);
     const [ isUserSearchLoading, setUserSearchLoading ] = useState<boolean>(false);
     const [ users, setUsers ] = useState<UserBasicInterface[]>([]);
     const [ activeOption, setActiveOption ] = useState<GroupsInterface | UserBasicInterface>(undefined);
-    const [ availableUserStores, setAvailableUserStores ] = useState<UserStoreDropdownItem[]>([]);
-    const [ selectedUserStoreDomainName, setSelectedUserStoreDomainName ] = useState<string>(
-        userstoresConfig.primaryUserstoreName
-    );
-    const [ selectedUsersFromUserStore, setSelectedUsersFromUserStore ] = useState<UserBasicInterface[]>([]);
+    const [ selectedUsers, setSelectedUsers ] = useState<UserBasicInterface[]>([]);
     const [ validationError, setValidationError ] = useState<boolean>(false);
     const [ hasInitialized, setHasInitialized ] = useState<boolean>(false);
-
-    const { data: userStores, isLoading: isUserStoresLoading } = useGetUserStores(null);
 
     const {
         data: userResponse,
@@ -104,82 +82,17 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
         null,
         userSearchValue ? `userName co ${userSearchValue}` : null,
         null,
-        selectedUserStoreDomainName,
         null,
-        !!selectedUserStoreDomainName || !!userSearchValue
+        null
     );
-
-    const isUserBelongToSelectedUserStore = (user: UserBasicInterface, userStoreName: string) => {
-        const userNameChunks: string[] = user.userName.split("/");
-
-        return (
-            (userNameChunks.length === 1 &&
-                StringUtils.isEqualCaseInsensitive(userStoreName, primaryUserStoreDomainName)) ||
-            (userNameChunks.length === 2 && StringUtils.isEqualCaseInsensitive(userNameChunks[0], userStoreName))
-        );
-    };
 
     useEffect(() => {
         setValidationError(showValidationError);
     },[ showValidationError ]);
 
-    /**
-     * Populates the available user stores, excluding disabled ones
-     */
     useEffect(() => {
-        if (userStores && !isUserStoresLoading) {
-            const storeOptions: UserStoreDropdownItem[] = [
-                {
-                    key: -1,
-                    text: userstoresConfig?.primaryUserstoreName,
-                    value: userstoresConfig?.primaryUserstoreName
-                }
-            ];
-
-            let storeOption: UserStoreDropdownItem = {
-                key: null,
-                text: "",
-                value: ""
-            };
-
-            userStores?.forEach((store: UserStoreListItem, index: number) => {
-                if (store?.name?.toUpperCase() !== userstoresConfig?.primaryUserstoreName) {
-                    getAUserStore(store.id).then((response: UserStorePostData) => {
-                        const isDisabled: boolean =
-                            response.properties.find((property: UserStoreProperty) => property.name === "Disabled")
-                                ?.value === "true";
-
-                        if (!isDisabled) {
-                            storeOption = {
-                                key: index,
-                                text: store.name,
-                                value: store.name
-                            };
-                            storeOptions.push(storeOption);
-                        }
-                    });
-                }
-            });
-
-            setAvailableUserStores(storeOptions);
-        }
-    }, [ userStores, isUserStoresLoading ]);
-
-    useEffect(() => {
-        if (activeUserStore) {
-            setSelectedUserStoreDomainName(activeUserStore);
-        } else {
-            setSelectedUserStoreDomainName(userstoresConfig.primaryUserstoreName);
-        }
-    }, [ activeUserStore ]);
-
-    useEffect(() => {
-        setSelectedUserStoreDomainName(userstoresConfig.primaryUserstoreName);
-    }, [ selectedUserStoreDomainName ]);
-
-    useEffect(() => {
-        onUsersChange(selectedUsersFromUserStore);
-    }, [ selectedUsersFromUserStore, onUsersChange ]);
+        onUsersChange(selectedUsers);
+    }, [ selectedUsers, onUsersChange ]);
 
     /**
      * Pre-selects users based on initial form values once user data is available.
@@ -195,7 +108,7 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
                 stepUsers.includes(user.id)
             );
 
-            setSelectedUsersFromUserStore(matchedUsers);
+            setSelectedUsers(matchedUsers);
         }
 
         setHasInitialized(true);
@@ -206,22 +119,11 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
      */
     useEffect(() => {
         if (!isReadOnly && userResponse?.totalResults > 0 && Array.isArray(userResponse?.Resources)) {
-            const usersAvailableToSelect: UserBasicInterface[] =
-                userResponse?.Resources?.filter((user: UserBasicInterface) => {
-                    const isUserInSelectedUserStore: boolean = isUserBelongToSelectedUserStore(
-                        user,
-                        selectedUserStoreDomainName
-                    );
-
-                    return isUserInSelectedUserStore;
-
-                }) ?? [];
-
-            setUsers(usersAvailableToSelect);
+            setUsers(userResponse.Resources);
         } else {
             setUsers([]);
         }
-    }, [ userResponse, selectedUsersFromUserStore ]);
+    }, [ userResponse, selectedUsers ]);
 
     /**
      * Show error if user list fetch request failed
@@ -238,12 +140,6 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
         }
     }, [ userListFetchRequestError ]);
 
-    // useEffect(() => {
-    //     if (selectedUsersFromUserStore.length > 0 && validationError) {
-    //         setValidationError(false);
-    //     }
-    // }, [ selectedUsersFromUserStore ]);
-
     /**
      * Handles the search query for the users list.
      */
@@ -257,7 +153,7 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
 
     return (
         <>
-            { users && availableUserStores && !isReadOnly && (
+            { users && !isReadOnly && (
                 <Grid
                     container
                     spacing={ 2 }
@@ -286,7 +182,7 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
                             disableCloseOnSelect
                             loading={ isUserListFetchRequestLoading || isUserSearchLoading }
                             options={ users }
-                            value={ selectedUsersFromUserStore }
+                            value={ selectedUsers }
                             disabled={ isReadOnly }
                             getOptionLabel={ (user: UserBasicInterface) => RoleManagementUtils.getUserUsername(user) }
                             renderInput={ (params: AutocompleteRenderInputParams) => (
@@ -303,7 +199,7 @@ const StepUsersList: FunctionComponent<StepUsersPropsInterface> = (
                                 />
                             ) }
                             onChange={ (event: SyntheticEvent, users: UserBasicInterface[]) => {
-                                setSelectedUsersFromUserStore(users);
+                                setSelectedUsers(users);
                             } }
                             filterOptions={ (users: UserBasicInterface[]) => users }
                             onInputChange={ (_event: SyntheticEvent, searchTerm: string) => {
