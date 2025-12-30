20b71a57ea92bc17b3a3c9211696f9a1bd790f6b
Lashen1227 <lashenmartino@gmail.com>
2025-12-30 15:54:54 +0530

    Disallow token exchange in pre-issue access token action v1

--- features/admin.actions.v1/components/pre-issue-access-token-action-config-form.tsx
+++ features/admin.actions.v1/components/pre-issue-access-token-action-config-form.tsx
@@ -87,6 +87,7 @@ const PreIssueAccessTokenActionConfigForm: FunctionComponent<PreIssueAccessToken
     isReadOnly,
     actionTypeApiPath,
     isCreateFormState,
+    versionInfo,
     [ "data-componentid" ]: _componentId = "action-config-form"
 }: PreIssueAccessTokenActionConfigFormInterface): ReactElement => {
 
@@ -304,6 +305,12 @@ const PreIssueAccessTokenActionConfigForm: FunctionComponent<PreIssueAccessToken
                 <RulesProvider
                     conditionExpressionsMetaData={ RuleExpressionsMetaData }
                     initialData={ actionData?.rule }
+                    hidden={ {
+                        values:
+                            ActionsConstants.__TEMPORARY__DISALLOWED_RULES[
+                                ActionsConstants.PRE_ISSUE_ACCESS_TOKEN_URL_PATH
+                            ]?.[actionData?.version || versionInfo?.latestVersion]
+                    } }
                 >
                     <FinalForm
                         onSubmit={ (values: ActionConfigFormPropertyInterface, form: any) => {
