a076903e535c20e0f655de27f98426fabfa7a27e
DilshanSenarath <74205483+DilshanSenarath@users.noreply.github.com>
2026-02-18 18:55:38 +0530

    add session mangement into feature gate

--- features/admin.feature-gate.v1/constants/feature-flag-constants.ts
+++ features/admin.feature-gate.v1/constants/feature-flag-constants.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2025-2026, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -60,6 +60,7 @@ class FeatureFlagConstants {
         LOGIN_AND_REGISTRATION_ORGANIZATION_IMPERSONATION: "loginAndRegistration.organizationSettings.impersonation",
         LOGIN_AND_REGISTRATION_SELF_REGISTRATION_FLOW_BUILDER:
             "loginAndRegistration.userOnboarding.registrationFlowBuilder",
+        LOGIN_AND_REGISTRATION_SESSION_MANAGEMENT: "console.loginAndRegistration.sessionManagement",
         MCP_SERVERS: "mcpServers",
         ORGANIZATION_BRANDING_TEXT: "branding.stylesAndText.organization.text",
         PUSH_PROVIDERS: "pushProviders",
