6666cdf93b95f9c666bc5c408404b7bcd140310c
Pavindu Lakshan <pavindulakshan@gmail.com>
2026-02-23 13:13:54 +0530

    Update tab text

--- modules/i18n/src/translations/en-US/portals/users.ts
+++ modules/i18n/src/translations/en-US/portals/users.ts
@@ -134,7 +134,7 @@ export const users: usersNS = {
                 1: "Groups",
                 2: "Roles",
                 3: "Active Sessions",
-                4: "Share"
+                4: "Shared Access"
             }
         }
     },
