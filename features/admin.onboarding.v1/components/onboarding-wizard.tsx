04e7b18398d4df7c6b9423df62203983bb804a13
KrishmalX <krishmaldinidu5466@gmail.com>
2026-06-11 03:58:52 +0530

    fix the line 348

--- features/admin.onboarding.v1/components/onboarding-wizard.tsx
+++ features/admin.onboarding.v1/components/onboarding-wizard.tsx
@@ -345,10 +345,11 @@ const OnboardingWizard: FunctionComponent<OnboardingWizardPropsInterface> = (
         totalSteps: progressTotalSteps
     } = useStepProgress(visibleStep, onboardingData);
 
-    const isM2M: boolean = useMemo(
+    /*const isM2M: boolean = useMemo(
         () => onboardingData.templateId === ApplicationTemplateIdTypes.M2M_APPLICATION,
         [ onboardingData.templateId ]
-    );
+    );*/
+    const isM2M: boolean = onboardingData.templateId === ApplicationTemplateIdTypes.M2M_APPLICATION;
 
     const { data: templateListing } = useGetExtensionTemplates(ResourceTypes.APPLICATIONS);
 
