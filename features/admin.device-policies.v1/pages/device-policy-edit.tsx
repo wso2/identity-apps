09c0cbc82235886cb46908d66862facd0415dc1f
kaviska <kaviska525@gmail.com>
2026-07-27 10:15:56 +0530

    Update licenses header

--- features/admin.device-policies.v1/pages/device-policy-edit.tsx
+++ features/admin.device-policies.v1/pages/device-policy-edit.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -326,8 +326,10 @@ const DevicePolicyEditPage: FunctionComponent<DevicePolicyEditPagePropsInterface
                         onClick={ (): void => setShowEditWizard(true) }
                         data-componentid={ `${ componentId }-edit-button` }
                     >
-                        <PenToSquareIcon />
-                        { t("devices:assurancePolicies.edit.editButton") }
+                        <Box sx={ { alignItems: "center", display: "flex", gap: 1 } }>
+                            <PenToSquareIcon />
+                            { t("devices:assurancePolicies.edit.editButton") }
+                        </Box>
                     </PrimaryButton>
                 ) }
                 data-componentid={ `${ componentId }-layout` }
